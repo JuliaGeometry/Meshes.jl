e1fd1bc02ce41c963bb8ba713244010c282081ed
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-10-23 14:55:10 -0300

    Refactor TupleView

--- src/viewtypes.jl
+++ src/viewtypes.jl
@@ -3,12 +3,11 @@
 # ------------------------------------------------------------------
 
 """
-    TupleView{T, N, Skip, A}
+    TupleView{T,N,M,A}
 
 TupleView, groups elements of an array as tuples.
 N is the dimension of the tuple, M tells how many elements to skip to the next tuple.
 By default TupleView{N} defaults to skip N items.
-# a few examples:
 
 ```julia
 
@@ -35,35 +34,25 @@ y = reinterpret(Point{2, Int}, TupleView{2, 1}(x))
 ```
 
 """
-struct TupleView{T,N,Skip,A} <: AbstractVector{T}
+struct TupleView{T,N,M,A} <: AbstractVector{T}
     data::A
     connect::Bool
 end
 
-coordinates(tw::TupleView) = coordinates(tw.data)
+TupleView{N,M}(x::AbstractVector{T}; connect=false) where {T,N,M} =
+    TupleView{NTuple{N,T},N,M,typeof(x)}(x, connect)
+
+TupleView{N}(x::AbstractVector; connect=false) where {N} =
+    TupleView{N,N}(x, connect=connect)
 
 function Base.size(x::TupleView{T,N,M}) where {T,N,M}
     nitems = length(x.data) ÷ (N - (N - M))
     nitems = nitems - max(N - M, 0)
-    return (nitems + x.connect,) # plus one item if we connect
+    (nitems + x.connect,) # plus one item if we connect
 end
 
-function Base.getindex(x::TupleView{T,N,M}, index::Integer) where {T,N,M}
-    return ntuple(i -> x.data[mod1(((index - 1) * M) + i, length(x.data))], N)
-end
-
-function TupleView{N}(x::AbstractVector; connect=false) where {N}
-    return TupleView{N,N}(x, connect=connect)
-end
-
-function TupleView{N,M}(x::AbstractVector{T}; connect=false) where {T,N,M}
-    return TupleView{NTuple{N,T},N,M,typeof(x)}(x, connect)
-end
-
-@inline function connected_line(points::AbstractVector{<:Point{N}},
-                                skip=N) where {N}
-    return connect(points, Line, skip)
-end
+Base.getindex(x::TupleView{T,N,M}, index::Integer) where {T,N,M} =
+    ntuple(i -> x.data[mod1(((index - 1) * M) + i, length(x.data))], N)
 
 """
     connect(points::AbstractVector{<:Point}, ::Type{<:Polytope}, skip::Int)
