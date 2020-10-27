9a5bcccef9cc022b727fe4b3e9db44fc94b89c69
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-10-27 17:15:49 -0300

    Add connect

--- src/connectivities.jl
+++ src/connectivities.jl
@@ -3,22 +3,33 @@
 # ------------------------------------------------------------------
 
 """
-    Connectivity(P, list)
+    Connectivity{PL,N}
 
-A connectivity `list` for the vertices of a polytope
-with type `P`. Indices are taken from a global vector
-of [`Point`](@ref) items.
+A connectivity list of `N` indices representing a polytope
+of type `PL`. Indices are taken from a global vector of
+[`Point`](@ref) items.
 
-## Example
-
-```julia
-# points 1, 2 and 3 make up a triangle
-points = [Point(0,0), Point(1,0), Point(0,1), ...]
-Δ = Connectivity(Triangle, (1,2,3))
-```
+Connectivity objects are constructed with the [`connect`](@ref)
+helper function.
 """
-struct Connectivity{N,P<:Polytope}
-    list::NTuple{N,Int}
+struct Connectivity{PL<:Polytope,N}
+  list::NTuple{N,Int}
 end
 
-connect(list::NTuple{N,Int}, P::Type{<:Polytope}) where {N} = Connectivity{N,P}(list)
+"""
+    connect(list, PL)
+
+Connect a `list` of indices from a global vector of points
+into a polytope of type `PL`.
+
+## Example
+```
+Δ = connect((1,2,3), Triangle)
+```
+"""
+connect(list::NTuple{N,Int}, PL::Type{<:Polytope}) where {N} = Connectivity{PL,N}(list)
+
+function materialize(connec::Connectivity{PL},
+                     points::AbstractVector{P}) where {PL<:Polytope,P<:Point}
+  PL(view(points, collect(connec.list)))
+end
