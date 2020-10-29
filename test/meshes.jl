48ba788bebee6d7f119c17fd4510e3957bba5b0d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-10-29 12:13:24 -0300

    Add more tests for UnstructredMesh

--- test/meshes.jl
+++ test/meshes.jl
@@ -42,5 +42,19 @@
     bytes = @allocated collect(cells)
     @test bytes < 800
     @test collect(cells) == triangles
+    cells = elements(mesh)
+    @test collect(cells) == triangles
+
+    points = Point2[(0,0), (1,0), (0,1), (1,1), (0.25,0.5), (0.75,0.5)]
+    Δs = connect.([(3,1,5),(4,6,2)], Triangle)
+    □s = connect.([(1,2,5,6),(5,6,3,4)], Quadrangle)
+    mesh = UnstructuredMesh(points, [Δs; □s])
+    elms = [
+      Triangle((0.0,1.0), (0.0,0.0), (0.25,0.5)),
+      Triangle((1.0,1.0), (0.75,0.5), (1.0,0.0)),
+      Quadrangle((0.0,0.0), (1.0,0.0), (0.25,0.5), (0.75,0.5)),
+      Quadrangle((0.25,0.5), (0.75,0.5), (0.0,1.0), (1.0,1.0))
+    ]
+    @test collect(elements(mesh)) == elms
   end
 end
