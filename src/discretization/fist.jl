4acd6ee2320abba124e06338be95b4e3b97dc91f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-11-12 10:35:41 -0300

    Refactor FIST

--- src/discretization/fist.jl
+++ src/discretization/fist.jl
@@ -24,46 +24,5 @@ to heuristics implemented in the algorithm.
 struct FIST <: DiscretizationMethod end
 
 function discretize(polysurface::PolySurface, method::FIST)
-  verts, perms = _fist_remove_duplicates(polysurface)
-end
-
-function _fist_remove_duplicates(polysurface)
-  outer, inners = chains(polysurface)
-
-  # retrieve vertices from chains
-  verts = [@view vertices(r)[begin:end-1] for r in [outer; inners]]
-
-  # sort vertices lexicographically
-  perms = [sortperm(coordinates.(v)) for v in verts]
-
-  # remove true duplicates
-  vertsperms = map(1:length(verts)) do k
-    vert = verts[k]
-    perm = perms[k]
-
-    keep = Int[]
-    newperm = deepcopy(perm)
-    sorted = @view vert[perm]
-    for i in 1:length(sorted)-1
-      if sorted[i] != sorted[i+1]
-        # save index in the original vector
-        push!(keep, perm[i])
-      else
-        # pop index from permutation vector
-        # by assigning the value 0 and then
-        # decrease all other indices that
-        # are greater than the index by 1
-        newperm[newperm .> perm[i]] .-= 1
-        newperm[i] = 0
-      end
-    end
-    push!(keep, last(perm))
-
-    sort!(keep)
-    filter!(!iszero, newperm)
-
-    view(vert, keep), newperm
-  end
-
-  first.(vertsperms), last.(vertsperms)
+  p = unique(polysurface)
 end
