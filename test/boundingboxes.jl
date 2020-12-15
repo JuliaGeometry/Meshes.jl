3a21dfbe9d3abe566ad1c2b6df91e835399828ef
Shreya Gupta <shreyagupta3011@gmail.com>
2020-12-16 01:15:28 +0530

    add boundingbox test (#20)
    * add boundingbox test
    
    * add boundingbox test
    
    * add missing parenthesis
    
    * add test
    
    * omit expression

--- test/boundingboxes.jl
+++ test/boundingboxes.jl
@@ -1,3 +1,4 @@
 @testset "Bounding boxes" begin
-  # TODO
+  @test boundingbox(Sphere((0,0),1)) == Box(Point(-1,-1),Point(1,1))
+  @test boundingbox(Sphere((1,1),1)) == Box(Point(0,0),Point(2,2))
 end
