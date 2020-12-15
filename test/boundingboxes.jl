a2b0640ce8636a4214420abd2e120ec636180e4e
shreyagupta30 <shreyagupta3011@gmail.com>
2020-12-15 11:05:42 +0530

    omit expression

--- test/boundingboxes.jl
+++ test/boundingboxes.jl
@@ -1,4 +1,4 @@
 @testset "Bounding boxes" begin
-  @test boundingbox(Sphere((0,0),1)) == Box{2,Int}(Point(-1,-1),Point(1,1))
-  @test boundingbox(Sphere((1,1),1)) == Box{2,Int}(Point(0,0),Point(2,2))
+  @test boundingbox(Sphere((0,0),1)) == Box(Point(-1,-1),Point(1,1))
+  @test boundingbox(Sphere((1,1),1)) == Box(Point(0,0),Point(2,2))
 end
