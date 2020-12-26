832074ebd415c3ac2651bd8f36a2a48207f9634c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-12-26 08:51:14 -0300

    Update test/boundingboxes.jl

--- test/boundingboxes.jl
+++ test/boundingboxes.jl
@@ -1,4 +1,4 @@
 @testset "Bounding boxes" begin
-  @test boundingbox(Sphere((0,0),1)) == Box(Point(-1,-1),Point(1,1))
-  @test boundingbox(Sphere((1,1),1)) == Box(Point(0,0),Point(2,2))
+  @test boundingbox(Sphere(P2(0,0),T(1))) == Box(P2(-1,-1),P2(1,1))
+  @test boundingbox(Sphere(P2(1,1),T(1))) == Box(P2(0,0),P2(2,2))
 end
