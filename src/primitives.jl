85a893715e1c6c6e600002ee26dbb0dcf6d15bc1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-01-19 11:20:28 -0300

    Refactor geometries.jl

---
+++ src/primitives.jl
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
+"""
+    Primitive{Dim,T}
+
+We say that a geometry is a primitive when it can be expressed as a single
+entity with no parts (a.k.a. atomic). For example, a sphere is a primitive
+described in terms of a mathematical expression involving a metric and a radius.
+See https://en.wikipedia.org/wiki/Geometric_primitive.
+"""
+abstract type Primitive{Dim,T} <: Geometry{Dim,T} end
+
+include("primitives/box.jl")
+include("primitives/ball.jl")
+include("primitives/sphere.jl")
+include("primitives/cylinder.jl")
