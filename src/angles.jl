cec64d3c2eccb28414f0c463ffbc768fbab0daaf
serenity4 <cedric.bel@hotmail.fr>
2021-01-28 11:03:22 +0100

    Use atan for angle computations

--- src/angles.jl
+++ src/angles.jl
@@ -13,12 +13,6 @@ See https://en.wikipedia.org/wiki/Angle.
 ```julia
 ∠(Point(0,1), Point(0,0), Point(1,0)) == π/2
 ```
-
-## References
-
-* Balbes, R. and Siegel, J. 1990. [A robust method for calculating
-  the simplicity and orientation of planar polygons]
-  (https://www.sciencedirect.com/science/article/abs/pii/0167839691900198)
 """
 function ∠(A::P, B::P, C::P) where {P<:Point{2}}
   BA = A - B
@@ -27,16 +21,5 @@ function ∠(A::P, B::P, C::P) where {P<:Point{2}}
   cross = BA × BC
   inner = BA ⋅ BC
 
-  R = norm(cross) / (norm(BA) * norm(BC))
-
-  # Table 1 from Balbes, R. and Siegel, J. 1990.
-  if cross ≥ 0 && inner ≥ 0
-    asin(R)
-  elseif cross ≥ 0 && inner < 0
-    π - asin(R)
-  elseif cross < 0 && inner ≥ 0
-    -asin(R)
-  elseif cross < 0 && inner < 0
-    asin(R) - π
-  end
+  atan(cross, inner)
 end
