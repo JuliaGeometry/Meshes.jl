475780979cd41f15663d3e14b6a365036faa185b
Hongyang Zhou <hyzhou@umich.edu>
2021-01-31 16:16:51 +0200

    Add ref link.

--- src/polytopes/segment.jl
+++ src/polytopes/segment.jl
@@ -142,9 +142,11 @@ function intersectpoint(s1::Segment{Dim,T}, s2::Segment{Dim,T}) where {Dim,T}
   v2 = y1 - y2
   v12 = x1 - y1
 
+  # Ref: https://en.wikipedia.org/wiki/Line-line_intersection#Formulas
   # find scalar along first segment
-  s = (v12[1] * v2[2] - v12[2] * v2[1]) / (v1[1] * v2[2] - v1[2] * v2[1])
-  
+  s = (v12[1] * v2[2] - v12[2] * v2[1]) /
+      (v1[1] * v2[2] - v1[2] * v2[1])
+
   x1 - s*v1
 end
 
