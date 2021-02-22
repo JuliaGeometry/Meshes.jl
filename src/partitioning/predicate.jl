a7d04a908a356481185d6340cc563a348cd11922
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-02-22 10:00:08 -0300

    Fix docstring of PredicatePartition

--- src/partitioning/predicate.jl
+++ src/partitioning/predicate.jl
@@ -3,11 +3,11 @@
 # ------------------------------------------------------------------
 
 """
-    PredicatePartition(pred)
+    PredicatePartition(predicate)
 
-A method for partitioning spatial objects with a given `predicate`.
-Two locations `i` and `j` are part of the same subset whenever
-`predicate(i, j) == true`
+A method for partitioning spatial objects with a given `predicate`
+function. Two locations `i` and `j` are part of the same subset
+whenever `predicate(i, j) == true`
 """
 struct PredicatePartition <: PredicatePartitionMethod
   pred::Function
