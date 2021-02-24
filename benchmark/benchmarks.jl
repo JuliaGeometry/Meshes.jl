4c2e9ebd989ebf33967444c0c478c3d6c143f464
serenity4 <cedric.bel@hotmail.fr>
2021-02-24 18:51:56 +0100

    Add benchmark folder

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,17 @@
+using BenchmarkTools
+using BenchmarkTools: tune!, allocs,
+      prettytime, time, prettymemory, memory, generate_benchmark_definition,
+      Parameters, benchmarkable_parts, collectvars, warmup, Trial
+
+include("utils.jl")
+
+SUITE = BenchmarkGroup()
+
+include("points.jl")
+
+t = @elapsed using Meshes
+println("Julia version is $VERSION")
+println(string("Meshes.jl loading time: \e[33;1;1m$t\e[m seconds"))
+println()
+println("Benchmarking Meshes.jl...")
+println()
