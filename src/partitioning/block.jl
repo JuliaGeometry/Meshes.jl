b0f1d48c2c8f5e24e582dd67e8a1d4faa46fa383
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-03-11 13:31:55 -0300

    Update src/partitioning/block.jl

--- src/partitioning/block.jl
+++ src/partitioning/block.jl
@@ -7,7 +7,7 @@
     BlockPartition(side₁, side₂, ...)
 
 A method for partitioning spatial objects into blocks of given `sides`.
-Metadata calculation is disabled by default. Enable it by calling BlockPartition with neighbors = true.
+Optionally, compute the `neighbors` of a block as the metadata.
 """
 struct BlockPartition{Dim,T} <: PartitionMethod
   sides::SVector{Dim,T}
