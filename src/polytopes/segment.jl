2c7113d54b08a9049200b001949dccc01f1b6cf2
Cédric Belmant <cedric.bel@hotmail.fr>
2021-03-19 22:38:31 +0100

    Make 1D parametric types callable (#85)
    * Make Segment callable
    
    * Add test for callable
    
    * Make Ray callable
    
    * Add test for Segment on negative input

--- src/polytopes/segment.jl
+++ src/polytopes/segment.jl
@@ -6,11 +6,21 @@
     Segment(p1, p2)
 
 An oriented line segment with end points `p1`, `p2`.
+The segment can be called as `s(t)` with `t` between
+`0` and `1` to interpolate linearly between its endpoints.
 
 See also [`Line`](@ref).
 """
 struct Segment{Dim,T,V<:AbstractVector{Point{Dim,T}}} <: Polytope{1,Dim,T}
   vertices::V
+end
+
+function (s::Segment)(t)
+  if t < 0 || t > 1
+    throw(DomainError(t, "s(t) is not defined for t outside [0, 1]."))
+  end
+  p1, p2 = s.vertices
+  p1 + t * (p2 - p1)
 end
 
 measure(s::Segment) = norm(s.vertices[2] - s.vertices[1])
