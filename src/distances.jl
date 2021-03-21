8a9347ba1395238cc326b27606afac0055f9bd6a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-03-21 13:25:41 -0300

    Add distance between points

--- src/distances.jl
+++ src/distances.jl
@@ -11,9 +11,17 @@ evaluate(d::PreMetric, g::Geometry, p::Point) = evaluate(d, p, g)
 Evaluate the Euclidean distance between `point` and `line`.
 """
 function evaluate(::Euclidean, p::Point, l::Line)
-  a, b = points(l)
   u = coordinates(p)
+  a, b = points(l)
   v = b - a
   α = (u ⋅ v) / (v ⋅ v)
   norm(u - α*v)
 end
+
+"""
+    evaluate(::PreMetric, point1, point2)
+
+Evaluate pre-metric between coordinates of `point2` and `point2`.
+"""
+evaluate(d::PreMetric, p1::Point, p2::Point) =
+  evaluate(d, coordinates(p1), coordinates(p2))
--- test/distances.jl
+++ test/distances.jl
@@ -3,4 +3,7 @@
   l = Line(P2(0,0), P2(1,0))
   @test evaluate(Euclidean(), p, l) == T(1)
   @test evaluate(Euclidean(), l, p) == T(1)
+
+  p1, p2 = P2(1,0), P2(0,1)
+  @test evaluate(Chebyshev(), p1, p2) == T(1)
 end
