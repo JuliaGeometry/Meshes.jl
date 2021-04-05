8cacbfd634918ce3f46298a533ebc350762dcd61
lassepe <lasse.peters@mailbox.org>
2021-04-06 00:30:57 +0200

    Add tests

--- test/distances.jl
+++ test/distances.jl
@@ -1,16 +1,18 @@
 @testset "Distances" begin
-  p = P2(0, 1)
-  l = Line(P2(0, 0), P2(1, 0))
-  @test (@test_deprecated evaluate(Euclidean(), p, l)) == T(1)
-  @test (@test_deprecated evaluate(Euclidean(), l, p)) == T(1)
-  @test mindistance(Euclidean(), l, p) == mindistance(Euclidean(), p, l) == T(1)
+    p = P2(0, 1)
+    l = Line(P2(0, 0), P2(1, 0))
+    for metric in (Euclidean(), SqEuclidean())
+        @test (@test_deprecated evaluate(metric, p, l)) == T(1)
+        @test (@test_deprecated evaluate(metric, l, p)) == T(1)
+        @test mindistance(metric, l, p) == mindistance(metric, p, l) == T(1)
+    end
 
-  p1, p2 = P2(1, 0), P2(0, 1)
-  @test (@test_deprecated evaluate(Chebyshev(), p1, p2)) == T(1)
-  @test mindistance(Chebyshev(), p1, p2) == T(1)
+    p1, p2 = P2(1, 0), P2(0, 1)
+    @test (@test_deprecated evaluate(Chebyshev(), p1, p2)) == T(1)
+    @test mindistance(Chebyshev(), p1, p2) == T(1)
 
-  p = P2(68, 259)
-  l = Line(P2(68, 260), P2(69, 261))
-  @test (@test_deprecated evaluate(Euclidean(), p, l)) ≤ T(0.8)
-  @test mindistance(Euclidean(), l, p) == mindistance(Euclidean(), p, l) ≤ T(0.8)
+    p = P2(68, 259)
+    l = Line(P2(68, 260), P2(69, 261))
+    @test (@test_deprecated evaluate(Euclidean(), p, l)) ≤ T(0.8)
+    @test mindistance(Euclidean(), l, p) == mindistance(Euclidean(), p, l) ≤ T(0.8)
 end
