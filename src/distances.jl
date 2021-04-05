07fc4967441a7adf318e25066f01f24281b90622
lassepe <lasse.peters@mailbox.org>
2021-04-06 01:18:39 +0200

    closest_point for Segment and Chain

--- src/distances.jl
+++ src/distances.jl
@@ -2,21 +2,48 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
+function _projected_α(l::Union{Line,Segment}, p::Point)
+    a, b = l(0), l(1)
+    u = p - a
+    v = b - a
+    (u ⋅ v) / (v ⋅ v)
+end
+
 """
     closest_point(m::PreMetric, g::Union{Geometry,Point}, p::Point)
 
 Returns the closest point in `g` to the point `p` as measured by the metric `m`.
+
+There may be multiple points in `g` that are closest to `p`. Any of such points may be returned by
+this function.
 """
 function closest_point end
 
 closest_point(::PreMetric, p::Point, ::Point) = p
+closest_point(::Union{Euclidean,SqEuclidean}, l::Line, p::Point) = l(_projected_α(l, p))
 
-function closest_point(::Union{Euclidean,SqEuclidean}, l::Line, p::Point)
-    a, b = l(0), l(1)
-    u = p - a
-    v = b - a
-    α = (u ⋅ v) / (v ⋅ v)
-    l(α)
+function closest_point(::Union{Euclidean,SqEuclidean}, s::Segment, p::Point)
+    p1, p2 = vertices(s)
+    if p1 == p2
+        p1
+    else
+        α = _projected_α(s, p)
+        if α <= 0
+            p1
+        elseif α >= 1
+            p2
+        else
+            s(α)
+        end
+    end
+end
+
+function closest_point(metric::Union{Euclidean,SqEuclidean}, c::Chain, p::Point)
+    reduce(segments(c); init = (; distance = Inf, point = first(vertices(c)))) do best, s
+        point = closest_point(metric, s, p)
+        distance = mindistance(metric, point, p)
+        distance < best.distance ? (; distance, point) : best
+    end.point
 end
 
 """
--- test/distances.jl
+++ test/distances.jl
@@ -16,6 +16,25 @@
         l = Line(P2(68, 260), P2(69, 261))
         @test (@test_deprecated evaluate(Euclidean(), p, l)) ≤ T(0.8)
         @test mindistance(Euclidean(), l, p) == mindistance(Euclidean(), p, l) ≤ T(0.8)
+
+        s = Segment(P2(0, 0), P2(1, 0))
+        for metric in (Euclidean(), SqEuclidean())
+            @test mindistance(metric, s, P2(0, 1)) == mindistance(metric, P2(0, 1), s) == T(1)
+            @test mindistance(metric, s, P2(2, 0)) == mindistance(metric, P2(2, 0), s) == T(1)
+            @test mindistance(metric, s, first(vertices(s))) ==
+                  mindistance(metric, first(vertices(s)), s) ==
+                  T(0)
+        end
+
+        c = Chain(P2(0, 0), P2(1, 0), P2(1, 1))
+        for metric in (Euclidean(), SqEuclidean())
+            @test mindistance(metric, c, P2(0, 1)) == mindistance(metric, P2(0, 1), c) == T(1)
+            @test mindistance(metric, c, P2(2, 0)) == mindistance(metric, P2(2, 0), c) == T(1)
+            @test mindistance(metric, c, first(vertices(c))) ==
+                  mindistance(metric, first(vertices(c)), c) ==
+                  T(0)
+        end
+
     end
 
     @testset "closest_point" begin
@@ -27,6 +46,22 @@
             @test closest_point(metric, l, l.a) == l.a
             @test closest_point(metric, l, l.b) == l.b
             @test_throws MethodError closest_point(metric, p, l)
+        end
+
+        s = Segment(P2(0, 0), P2(1, 0))
+        for metric in (Euclidean(), SqEuclidean())
+            p_center = P2(((coordinates(s.vertices[1]) + coordinates(s.vertices[2])) / 2)...)
+            @test closest_point(metric, s, P2(0, 1)) == s.vertices[1]
+            @test closest_point(metric, s, P2(2, 0)) == s.vertices[2]
+            @test closest_point(metric, s, s.vertices[1]) == s.vertices[1]
+            @test closest_point(metric, s, p_center) == p_center
+        end
+
+        c = Chain(P2(0, 0), P2(1, 0), P2(1, 1))
+        for metric in (Euclidean(), SqEuclidean())
+            @test closest_point(metric, c, P2(0, 1)) in (c.vertices[1], c.vertices[3])
+            @test closest_point(metric, c, P2(2, 0)) == c.vertices[2]
+            @test closest_point(metric, c, c.vertices[1]) == c.vertices[1]
         end
     end
 end
