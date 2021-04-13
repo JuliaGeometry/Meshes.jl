f02448f49367898c6489ec311103bb4627c5a00e
hyrodium <hyrodium@gmail.com>
2021-04-13 22:07:54 +0900

    add method Base.==(::Line, ::Line)

--- src/primitives/line.jl
+++ src/primitives/line.jl
@@ -29,3 +29,7 @@ function Base.in(p::Point, l::Line)
   # d ≈ 0.0 will be too precise, and d < atol{T} can't scale.
   d+w ≈ w
 end
+
+function Base.:(==)(l1::Line, l2::Line)
+  l1.a in l2 && l1.b in l2 && l2.a in l1 && l2.b in l1
+end
