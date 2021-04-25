019889e2a1dbfd94425f85fe2e3c183854164ebb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-04-25 14:40:45 -0300

    Add paramdim for Multi

--- src/geometries.jl
+++ src/geometries.jl
@@ -103,6 +103,8 @@ Base.lastindex(multi::Multi) = lastindex(multi.items)
 Base.iterate(multi::Multi, state=1) =
   state > length(multi) ? nothing : (multi[state], state+1)
 
+paramdim(multi::Multi) = maximum(paramdim, multi.items)
+
 function centroid(multi::Multi)
   cs = coordinates.(centroid.(multi.items))
   Point(sum(cs) / length(cs))
