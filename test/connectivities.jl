af0b3a1fd0698615a2f3874c9fa69104e02d897a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-04-30 15:11:22 -0300

    Add more tests for Connectivity

--- test/connectivities.jl
+++ test/connectivities.jl
@@ -9,4 +9,8 @@
   @test paramdim(c) == 2
   @test indices(c) == (1,2,3)
   @test materialize(c, P2[(0,0),(1,0),(0,1)]) == Triangle(P2(0,0), P2(1,0), P2(0,1))
+
+  # incorrect number of vertices for polytope
+  @test_throws AssertionError connect((1,2,3,4), Triangle)
+  @test_throws AssertionError connect((1,2,3), Quadrangle)
 end
