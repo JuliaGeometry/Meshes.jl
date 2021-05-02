b5bcb86532a4655badd24aa6a3aded612c6ecd25
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-05-02 11:42:53 -0300

    More tests for topological structures

--- test/topostructures.jl
+++ test/topostructures.jl
@@ -10,6 +10,9 @@
       @test he.head ∈ inds
       @test boundary(connec[e], 0, s2) == inds
       @test boundary(connec[e], 1, s2) == segs
+      for seg in segs
+        @test boundary(seg, 0, structure) == collect(indices(seg))
+      end
     end
   end
 
@@ -17,12 +20,6 @@
   s1 = ElementListStructure(connec)
   s2 = convert(HalfEdgeStructure, s1)
   test_halfedge(connec, s2)
-  @test boundary(connect((1,2), Segment), 0, s2) == [1,2]
-  @test boundary(connect((2,3), Segment), 0, s2) == [2,3]
-  @test boundary(connect((3,1), Segment), 0, s2) == [3,1]
-  @test boundary(connect((4,3), Segment), 0, s2) == [4,3]
-  @test boundary(connect((3,2), Segment), 0, s2) == [3,2]
-  @test boundary(connect((2,4), Segment), 0, s2) == [2,4]
   @test adjacency(1, s2) == [2,3]
   @test adjacency(2, s2) == [4,3,1]
   @test adjacency(3, s2) == [1,2,4]
