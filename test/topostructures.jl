7a7ba19583db2ad88cfdb854311ac2457624a115
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-05-02 11:39:28 -0300

    Refactor test/topostructures.jl

--- test/topostructures.jl
+++ test/topostructures.jl
@@ -1,18 +1,22 @@
 @testset "Topostructures" begin
+  function test_halfedge(connec, structure)
+    @test nelements(structure) == length(connec)
+    for e in 1:nelements(structure)
+      inds = collect(indices(connec[e]))
+      cvec = CircularVector(inds)
+      segs = [connect((cvec[i], cvec[i+1]), Segment) for i in 1:length(cvec)]
+      he = edgeonelem(structure, e)
+      @test he.elem == e
+      @test he.head ∈ inds
+      @test boundary(connec[e], 0, s2) == inds
+      @test boundary(connec[e], 1, s2) == segs
+    end
+  end
+
   connec = connect.([(1,2,3),(4,3,2)], Triangle)
   s1 = ElementListStructure(connec)
   s2 = convert(HalfEdgeStructure, s1)
-  @test nelements(s2) == 2
-  for e in 1:nelements(s2)
-    inds = collect(indices(connec[e]))
-    cvec = CircularVector(inds)
-    segs = [connect((cvec[i], cvec[i+1]), Segment) for i in 1:length(cvec)]
-    he = edgeonelem(s2, e)
-    @test he.elem == e
-    @test he.head ∈ inds
-    @test boundary(connec[e], 0, s2) == inds
-    @test boundary(connec[e], 1, s2) == segs
-  end
+  test_halfedge(connec, s2)
   @test boundary(connect((1,2), Segment), 0, s2) == [1,2]
   @test boundary(connect((2,3), Segment), 0, s2) == [2,3]
   @test boundary(connect((3,1), Segment), 0, s2) == [3,1]
@@ -29,17 +33,7 @@
   connec = connect.([(1,2,6,5),(2,4,6),(4,3,5,6),(1,5,3)], Ngon)
   s1 = ElementListStructure(connec)
   s2 = convert(HalfEdgeStructure, s1)
-  @test nelements(s2) == 4
-  for e in 1:nelements(s2)
-    inds = collect(indices(connec[e]))
-    cvec = CircularVector(inds)
-    segs = [connect((cvec[i], cvec[i+1]), Segment) for i in 1:length(cvec)]
-    he = edgeonelem(s2, e)
-    @test he.elem == e
-    @test he.head ∈ inds
-    @test boundary(connec[e], 0, s2) == inds
-    @test boundary(connec[e], 1, s2) == segs
-  end
+  test_halfedge(connec, s2)
   s3 = convert(ElementListStructure, s2)
   @test s3 == s1
 end
