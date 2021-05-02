2a907548ec6527a4c1319ae181b5c6ee147cc7c3
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-05-02 09:29:54 -0300

    More tests for topological structures

--- test/topostructures.jl
+++ test/topostructures.jl
@@ -1,13 +1,23 @@
 @testset "Topostructures" begin
-  s1 = ElementListStructure(connect.([(1,2,3),(4,3,2)], Triangle))
-  @test connectivities(s1) == connect.([(1,2,3),(4,3,2)], Triangle) 
-
+  connec = connect.([(1,2,3),(4,3,2)], Triangle)
+  s1 = ElementListStructure(connec)
   s2 = convert(HalfEdgeStructure, s1)
   @test nelements(s2) == 2
   @test all(edgeonelem(s2, e).elem == e for e in 1:nelements(s2))
   @test edgeonelem(s2, 1).head ∈ 1:3
   @test edgeonelem(s2, 2).head ∈ 4:6
-
+  @test boundary(connec[1], 1, s2) == connect.([(1,2),(2,3),(3,1)], Segment)
+  @test boundary(connec[2], 1, s2) == connect.([(4,3),(3,2),(2,4)], Segment)
+  @test boundary(connec[1], 0, s2) == [1,2,3]
+  @test boundary(connec[2], 0, s2) == [4,3,2]
+  @test boundary(connect((1,2), Segment), 0, s2) == [1,2]
+  @test boundary(connect((2,3), Segment), 0, s2) == [2,3]
+  @test boundary(connect((3,1), Segment), 0, s2) == [3,1]
+  @test boundary(connect((4,3), Segment), 0, s2) == [4,3]
+  @test adjacency(1, s2) == [2,3]
+  @test adjacency(2, s2) == [4,3,1]
+  @test adjacency(3, s2) == [1,2,4]
+  @test adjacency(4, s2) == [3,2]
   s3 = convert(ElementListStructure, s2)
   @test s3 == s1
 end
