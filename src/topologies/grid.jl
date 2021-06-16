6929195b66d353f6bd87b39750fcb71850052868
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-06-16 16:01:11 -0300

    Refactor GridTopology

--- src/topologies/grid.jl
+++ src/topologies/grid.jl
@@ -4,15 +4,15 @@ using Base: StatusConnecting
 # ------------------------------------------------------------------
 
 """
-    GridTopology(size)
+    GridTopology(dims)
 
-A data structure for grid topologies of given `size`.
+A data structure for grid topologies with `dims` elements.
 """
 struct GridTopology{D} <: Topology
-  size::NTuple{D,Int}
+  dims::Dims{D}
 end
 
-GridTopology(size::Vararg{Int,D}) where {D} = GridTopology{D}(size)
+GridTopology(dims::Vararg{Int,D}) where {D} = GridTopology{D}(dims)
 
 """
     size(t)
@@ -20,13 +20,13 @@ GridTopology(size::Vararg{Int,D}) where {D} = GridTopology{D}(size)
 Return the size of the grid topology `t`, i.e. the
 number of elements along each dimension of the grid.
 """
-Base.size(t::GridTopology) = t.size
+Base.size(t::GridTopology) = t.dims
 
 # ---------------------
 # HIGH-LEVEL INTERFACE
 # ---------------------
 
-nvertices(t::GridTopology) = prod(t.size .+ 1)
+nvertices(t::GridTopology) = prod(t.dims .+ 1)
 
 function faces(t::GridTopology{D}, rank) where {D}
   if rank == D
@@ -39,8 +39,8 @@ function faces(t::GridTopology{D}, rank) where {D}
 end
 
 function element(t::GridTopology{D}, ind) where {D}
-  l2c(ind) = CartesianIndices(t.size)[ind].I
-  c2l(ind...) = LinearIndices(t.size .+ 1)[ind...]
+  l2c(ind) = CartesianIndices(t.dims)[ind].I
+  c2l(ind...) = LinearIndices(t.dims .+ 1)[ind...]
   if D == 1
     i1 = ind
     i2 = ind+1
@@ -68,7 +68,7 @@ function element(t::GridTopology{D}, ind) where {D}
   end
 end
 
-nelements(t::GridTopology) = prod(t.size)
+nelements(t::GridTopology) = prod(t.dims)
 
 function facet(t::GridTopology{D}, ind) where {D}
   if D == 1
@@ -84,16 +84,16 @@ end
 
 function nfacets(t::GridTopology{D}) where {D}
   if D == 1
-    t.size[1] + 1
+    t.dims[1] + 1
   elseif D == 2
-    2prod(t.size) +
-    t.size[1] +
-    t.size[2]
+    2prod(t.dims) +
+    t.dims[1] +
+    t.dims[2]
   elseif D == 3
-    3prod(t.size) +
-    prod(t.size[[1,2]]) +
-    prod(t.size[[1,3]]) +
-    prod(t.size[[2,3]])
+    3prod(t.dims) +
+    prod(t.dims[[1,2]]) +
+    prod(t.dims[[1,3]]) +
+    prod(t.dims[[2,3]])
   else
     throw(ErrorException("not implemented"))
   end
