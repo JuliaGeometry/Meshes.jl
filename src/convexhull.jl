0d0b0c89c2e1102d73a9982f5028b032081ae974
Andre <andreconsidera@id.uff.br>
2021-08-17 19:26:38 -0300

    shortened the stopping criterion, formatted docstring

--- src/convexhull.jl
+++ src/convexhull.jl
@@ -1,30 +1,36 @@
 """
-Compute the upper part of the convex hull of a given set of 2D points 'pset' using the "Graham Scan" algorithm.
+    upperhull(pset)
 
-*) Sort the points lexicographic
-
-*) Assess the last 3 points in 'upperHull'. If the the clockwise angle is <180° remove the second to last point.
-
+Upper part of the convex hull of a given set of 2D points `pset`. 
+The upperhull is computed by the "Graham Scan" algorithm.
 """
-
-function upperhull(pset::PointSet) 
+function upperhull(pset::PointSet)
 
   xs=sort(coordinates.(pset), by=first)
-
+  
   upperHull=[xs[1],xs[2]] 
   
   for i in 3:length(xs)
     push!(upperHull,xs[i])
     
+    A=Point(upperHull[end-2])
+    B=Point(upperHull[end-1])
+    C=Point(upperHull[end])
+
     # remove the second to last point in upperHull while the last 3 points make a "left turn"
-    while length(upperHull)>2 && ∠(Point(upperHull[lastindex(upperHull)-2]),Point(upperHull[lastindex(upperHull)-1]),Point(upperHull[lastindex(upperHull)]))<0 
+    while length(upperHull)>2 && ∠(A,B,C)<0 
       
       splice!(upperHull,lastindex(upperHull)-1)
+      
+      if length(upperHull)>2 
+
+        A=Point(upperHull[end-2])
+        B=Point(upperHull[end-1])
+        C=Point(upperHull[end])
+      end
     
-    end 
+    end
 
-  end
-
+  end 
   upperHull
-
-end
+end 
