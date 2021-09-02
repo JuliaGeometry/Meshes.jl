c2ac641752194ecb5595c99c96166929be651b43
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-09-02 19:11:51 -0300

    Update test/convexhull.jl

--- test/convexhull.jl
+++ test/convexhull.jl
@@ -1,10 +1,10 @@
 @testset "Convex Hull" begin
   
   # Upper part of the Convex Hull
-  pset1=PointSet(P2(0,0),P2(1,1),P2(1.5,1.0))
-  pset2=PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.))
-  pset3=PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.),P2(2,3))
-  pset4=PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.),P2(2,3),P2(2,4))
+  pset1 = PointSet(P2(0,0),P2(1,1),P2(1.5,1.0))
+  pset2 = PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.))
+  pset3 = PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.),P2(2,3))
+  pset4 = PointSet(P2(0,0),P2(1,1),P2(1.5,1.0),P2(1.5,3.),P2(2,3),P2(2,4))
 
   @test upperhull(pset1) == [[0.0, 0.0],[1.0, 1.0],[1.5, 1.0]]
   @test upperhull(pset2) == [[0.0, 0.0],[1.5, 3.0]]
