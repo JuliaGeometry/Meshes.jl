f952302242ace8d764358a2e2155d915ef27c7c1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-10-18 08:28:54 -0300

    Fix boundary order in Box an Hexahedron

--- src/polytopes/hexahedron.jl
+++ src/polytopes/hexahedron.jl
@@ -41,6 +41,6 @@ end
 
 function boundary(h::Hexahedron)
   indices = [(1,2,3,4),(2,1,5,6),(2,6,7,3),
-             (7,3,8,4),(4,8,5,1),(5,8,7,6)]
+             (3,7,8,4),(4,8,5,1),(5,8,7,6)]
   SimpleMesh(h.vertices, connect.(indices))
 end
