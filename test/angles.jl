8d95675212a0250d51f6f159f1435ba51185117d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-10-27 05:06:22 -0400

    Should `angles(Triangle(Point(0,0), Point(1,0), Point(0,1))` be negatives?

--- test/angles.jl
+++ test/angles.jl
@@ -19,6 +19,6 @@
   
   # Ngon
   t = Triangle(Point2(0,0), Point2(1,0), Point2(0,1))
-  @test all(isapprox.(rad2deg.(angles(t)), [90.0, 45.0, 45.0], atol=8*eps(45.0)))
+  @test all(isapprox.(rad2deg.(angles(t)), [-90.0, -45.0, -45.0], atol=8*eps(45.0)))
   @test all(isapprox.(rad2deg.(innerangles(t)), [90.0, 45.0, 45.0], atol=8*eps(45.0)))     
 end
