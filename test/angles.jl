2307670d3c5520201712d6ccb5f61c4293168214
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-10-27 08:12:54 -0400

    remove angles tests from here

--- test/angles.jl
+++ test/angles.jl
@@ -15,10 +15,5 @@
   @test ∠(P3(0,1,0),P3(0,0,0),P3(1,0,0)) ≈ T(π/2)
   @test ∠(P3(0,1,0),P3(0,0,0),P3(0,0,1)) ≈ T(π/2)
   @test ∠(P3(0,0,1),P3(0,0,0),P3(1,0,0)) ≈ T(π/2)
-  @test ∠(P3(0,0,1),P3(0,0,0),P3(0,1,0)) ≈ T(π/2)
-  
-  # Ngon
-  t = Triangle(P2(0,0), P2(1,0), P2(0,1))
-  @test all(isapprox.(rad2deg.(angles(t)), T[-90, -45, -45], atol=atol(T)))
-  @test all(isapprox.(rad2deg.(innerangles(t)), T[90, 45, 45], atol=atol(T)))     
+  @test ∠(P3(0,0,1),P3(0,0,0),P3(0,1,0)) ≈ T(π/2)   
 end
