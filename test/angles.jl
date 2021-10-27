a501fd75180e6014fd3d06beba32ade7da504555
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-10-27 00:01:53 -0400

    add tests for `angles` `innerangles`

--- test/angles.jl
+++ test/angles.jl
@@ -16,4 +16,9 @@
   @test ∠(P3(0,1,0),P3(0,0,0),P3(0,0,1)) ≈ T(π/2)
   @test ∠(P3(0,0,1),P3(0,0,0),P3(1,0,0)) ≈ T(π/2)
   @test ∠(P3(0,0,1),P3(0,0,0),P3(0,1,0)) ≈ T(π/2)
+  
+  # Ngon
+  t = Triangle(Point2(0,0), Point2(1,0), Point2(0,1))
+  @test all(isapprox.(rad2deg.(angles(t)), [90.0, 45.0, 45.0], atol=8*eps(45.0)))
+  @test all(isapprox.(rad2deg.(innerangles(t)), [90.0, 45.0, 45.0], atol=8*eps(45.0)))     
 end
