9ec3d49317fad8fa5193fc6df01ac688a34165c3
Jack Beagley <jack.beagley@outlook.com>
2021-11-04 23:20:11 +1300

    Commenting

--- src/intersections/planes.jl
+++ src/intersections/planes.jl
@@ -12,13 +12,18 @@ Compute the intersection of a segment `s` and a plane `p`
 * https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection
 """
 function intersecttype(s::Segment{3,T}, p::Plane{3,T}) where {T}
-    sᵥ = vertices(s)
-    p₀ = vertices(p)[1]
+    # Convert the positions of the segment vertices and the plane origin to coordinates
+    sᵥ = coordinates.(vertices(s))
+    pₒ = coordinates(origin(p))
+
+    # Get the normal of the plane
+    n = normal(p)
     
-    n = m.v × m.w
+    # Calculate components
     ln = (sᵥ[2] - sᵥ[1]) ⋅ n
-    pₒn = p₀ ⋅ n
+    pₒn = pₒ ⋅ n
   
+
     if isapprox(ln, zero(T))
         if isapprox(pₒn, ln)
             return ContainedSegmentPlane()
@@ -26,14 +31,17 @@ function intersecttype(s::Segment{3,T}, p::Plane{3,T}) where {T}
             return NoIntersection()
         end
     else
-        λ = ((sᵥ[1] - p₀) ⋅ n) / ln
+        # Calculate the segment parameter
+        λ = ((sᵥ[1] - pₒ) ⋅ n) / ln
 
+        # If λ is approximately 0 or 1, set as so to prevent any domain errors
         λ = isapprox(λ, zero(T), atol=atol(T)) ? zero(T) : (isapprox(λ, one(T), atol=atol(T)) ? one(T) : λ)
 
+        # If λ is out of bounds for the segment, then there is no intersection
         if (λ < zero(T)) || (λ > one(T))
-            return IntersectingSegmentPlane(s(λ), λ)
-        else
             return NoIntersection()
+        else
+            return IntersectingSegmentPlane(s(λ), λ)
         end
     end
   end
