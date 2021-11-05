2417e5b099120d390e5dc8538fa7da4ddcda522c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-11-05 09:37:49 -0300

    Update test/polytopes.jl

--- test/polytopes.jl
+++ test/polytopes.jl
@@ -142,6 +142,7 @@
     h = Hexahedron(P3[(0,0,0),(2,0,0),(2,2,0),(0,2,0),
                       (0,0,2),(2,0,2),(2,2,2),(0,2,2)])
     @test volume(h) ≈ T(2*2*2)
+
     # volume formula of a frustum of a prism is V = 1/3*H*(S₁+S₂+sqrt(S₁*S₂))
     # here we build a hexahedron which is a frustum of a prism with
     # bottom area S₁= 4, top area S₂= 1, height H = 2
