2a8569053e6465bd9323df8c76a6e6b64bbb53e8
Haoxuan Guo <kuohaoxuan@outlook.com>
2021-11-08 23:56:47 +0800

    force Float64 Point

--- src/points.jl
+++ src/points.jl
@@ -29,6 +29,7 @@ Point{Dim,T}(coords...) where {Dim,T} = Point{Dim,T}(SVector{Dim,T}(coords...))
 Point(coords::SVector{Dim,T}) where {Dim,T} = Point{Dim,T}(coords)
 Point(coords::AbstractVector{T}) where {T} = Point{length(coords),T}(coords)
 Point(coords...) = Point(SVector(coords...))
+Point(coords::SVector{Dim,T}) where {Dim,T<:Integer} = Point(SVector{Dim, Float64}(coords))
 
 # coordinate type conversions
 Base.convert(::Type{Point{Dim,T}}, coords) where {Dim,T} = Point{Dim,T}(coords)
