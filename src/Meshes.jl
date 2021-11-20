77aab749be4011fcc5fb1d719548321904ba007f
Haoxuan Guo <kuohaoxuan@outlook.com>
2021-11-20 20:47:47 +0800

    Add public API for Rays (#217)
    * Add public API for Rays
    
    * Update src/primitives/ray.jl
    
    * Update src/Meshes.jl
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/Meshes.jl
+++ src/Meshes.jl
@@ -155,7 +155,8 @@ export
   Box, Ball, Sphere, Cylinder,
   ncontrols, degree, Horner, DeCasteljau,
   radius, height, sides,
-  measure, diagonal, origin,
+  measure, diagonal,
+  origin, direction,
 
   # polytopes
   Polytope, Polygon, Polyhedron,
