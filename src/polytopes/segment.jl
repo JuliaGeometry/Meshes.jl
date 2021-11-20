67a6a7f2957ab55a27a0d0b91d08ae01ab571486
Haoxuan Guo <kuohaoxuan@outlook.com>
2021-11-20 20:04:32 +0800

    Make Segment oriented

--- src/polytopes/segment.jl
+++ src/polytopes/segment.jl
@@ -48,9 +48,5 @@ end
 Tells whether or not the coordinates of segments `s1` and `s2 are approximately equal.
 """
 function Base.isapprox(s1::Segment{Dim,T}, s2::Segment{Dim,T}) where {Dim,T}
-  function reverse(s::Segment{Dim,T})
-    Segment(s.vertices[2], s.vertices[1])
-  end
-  return all(isapprox.(s1.vertices, s2.vertices)) ||
-    all(isapprox.(reverse(s1).vertices, s2.vertices))
+  return all(isapprox.(s1.vertices, s2.vertices))
 end
