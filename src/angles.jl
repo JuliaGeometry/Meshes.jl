c7df1b3086de23a29921a43ed3ad9cfab90b81b5
Kyle Beggs <beggskw@gmail.com>
2021-12-16 17:32:23 -0500

    added methods for vectors for ∠ (#235)
    * added methods for vectors for ∠
    
    * incorporated suggestion on PR #235
    
    * Update src/angles.jl
    
    * change angle method for vectors to 2 arg atan
    
    * Update src/angles.jl
    
    * Update src/angles.jl
    
    * express methods for points in terms of vectors
    
    * Update src/angles.jl
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/angles.jl
+++ src/angles.jl
@@ -8,7 +8,7 @@
 Angle ∠ABC between rays BA and BC.
 See https://en.wikipedia.org/wiki/Angle.
 
-Uses the two-argument form of `atan`.
+Uses the two-argument form of `atan` returning value in range [-π, π].
 See https://en.wikipedia.org/wiki/Atan2.
 
 ## Examples
@@ -17,12 +17,23 @@ See https://en.wikipedia.org/wiki/Atan2.
 ∠(Point(1,0), Point(0,0), Point(0,1)) == π/2
 ```
 """
-function ∠(A::P, B::P, C::P) where {P<:Point{2}}
-  BA, BC = A - B, C - B
-  atan(BA × BC, BA ⋅ BC) # preserve sign
-end
+∠(A::P, B::P, C::P) where {P<:Point{2}} = ∠(A-B, C-B)
+∠(A::P, B::P, C::P) where {P<:Point{3}} = ∠(A-B, C-B)
 
-function ∠(A::P, B::P, C::P) where {P<:Point{3}}
-  BA, BC = A - B, C - B
-  atan(norm(BA × BC), BA ⋅ BC) # discard sign
-end
+"""
+    ∠(u, v)
+
+Angle between vectors u and v.
+See https://en.wikipedia.org/wiki/Angle.
+
+Uses the two-argument form of `atan` returning value in range [-π, π].
+See https://en.wikipedia.org/wiki/Atan2.
+
+## Examples
+
+```julia
+∠(Vec(1,0), Vec(0,1)) == π/2
+```
+"""
+∠(u::V, v::V) where {V<:Vec{2}} = atan(u × v, u ⋅ v)  # preserve sign
+∠(u::V, v::V) where {V<:Vec{3}} = atan(norm(u × v), u ⋅ v)  # discard sign
