a5b020cd9d7070a0e123a15ede64752b521ef375
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-02-11 18:57:47 -0300

    Fix docstring of FanTriangulation

--- src/discretization/fan.jl
+++ src/discretization/fan.jl
@@ -5,7 +5,7 @@
 """
     FanTriangulation()
 
-The fan triangulation algorithm fox convex polygons.
+The fan triangulation algorithm for convex polygons.
 See [https://en.wikipedia.org/wiki/Fan_triangulation]
 (https://en.wikipedia.org/wiki/Fan_triangulation).
 """
