812ba1c2120c618b8b55d891a958ff742ac5c568
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-05-05 14:54:05 -0300

    Improve docstring of CartesianGrid

--- src/mesh/cartesiangrid.jl
+++ src/mesh/cartesiangrid.jl
@@ -32,19 +32,19 @@ In this case, the origin and spacing default to (0,0,...) and (1,1,...).
 
 ## Examples
 
-Create a 3D grid with 100x100x50 locations:
+Create a 3D grid with 100x100x50 hexahedrons:
 
 ```julia
 julia> CartesianGrid(100,100,50)
 ```
 
-Create a 2D grid with 100x100 locations and origin at (10.,20.) units:
+Create a 2D grid with 100x100 quadrangles and origin at (10.,20.) units:
 
 ```julia
 julia> CartesianGrid((100,100),(10.,20.),(1.,1.))
 ```
 
-Create a 1D grid from -1 to 1 with 100 locations:
+Create a 1D grid from -1 to 1 with 100 segments:
 
 ```julia
 julia> CartesianGrid((-1.,),(1.,), dims=(100,))
