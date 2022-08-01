e8126343cd60e63ffa432b3937e6769f59ae629f
Gerhard <quantum.quentin@mailbox.org>
2022-08-01 09:19:51 +0200

    type stability test for non exported function

--- test/intersections.jl
+++ test/intersections.jl
@@ -538,15 +538,15 @@
 
     p1, p2 = P2(0,0), P2(1,1)
     p3, p4 = P2(1,0), P2(0,1)
-    @inferred intersectparameters(p1, p2, p3, p4)
-    @inferred intersectparameters(p1, p3, p2, p4)
-    @inferred intersectparameters(p1, p2, p1, p2)
+    @inferred Meshes.intersectparameters(p1, p2, p3, p4)
+    @inferred Meshes.intersectparameters(p1, p3, p2, p4)
+    @inferred Meshes.intersectparameters(p1, p2, p1, p2)
 
     p1, p2 = P3(0,0,0), P3(1,1,1)
     p3, p4 = P3(1,0,0), P3(0,1,1)
-    @inferred intersectparameters(p1, p2, p3, p4)
-    @inferred intersectparameters(p1, p3, p2, p4)
-    @inferred intersectparameters(p1, p2, p1, p2)
+    @inferred Meshes.intersectparameters(p1, p2, p3, p4)
+    @inferred Meshes.intersectparameters(p1, p3, p2, p4)
+    @inferred Meshes.intersectparameters(p1, p2, p1, p2)
   end
 
   @testset "missing" begin
