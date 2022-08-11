0e9240ad4d14c16dd9b35aaf14c849dbb0499c96
Elias Carvalho <eliascarvdev@gmail.com>
2022-08-11 09:54:17 -0300

    Add Vec struct, constructors and alias

--- src/vectors.jl
+++ src/vectors.jl
@@ -15,14 +15,39 @@ A vector can be obtained by subtracting two [`Point`](@ref) objects.
 A = Point(0.0, 0.0)
 B = Point(1.0, 0.0)
 v = B - A
+
+# 2D vectors
+A = Vec(0.0, 1.0) # double precision as expected
+B = Vec(0f0, 1f0) # single precision as expected
+C = Vec(0, 0) # Integer is converted to Float64 by design
+D = Vec2(0, 1) # explicitly ask for double precision
+E = Vec2f(0, 1) # explicitly ask for single precision
+
+# 3D vectors
+F = Vec(1.0, 2.0, 3.0) # double precision as expected
+G = Vec(1f0, 2f0, 3f0) # single precision as expected
+H = Vec(1, 2, 3) # Integer is converted to Float64 by design
+I = Vec3(1, 2, 3) # explicitly ask for double precision
+J = Vec3f(1, 2, 3) # explicitly ask for single precision
 ```
 
 ### Notes
 
-- A `Vec` is a type of `FieldVector` from StaticArrays.jl
+- A `Vec` is a subtype of `StaticVector` from StaticArrays.jl
 - Type aliases are `Vec1`, `Vec2`, `Vec3`, `Vec1f`, `Vec2f`, `Vec3f`
 """
-const Vec = SVector
+struct Vec{Dim,T} <: StaticVector{Dim,T}
+  coords::SVector{Dim,T}
+  Vec{Dim,T}(coords::SVector{Dim,T}) where {Dim,T} = new{Dim,T}(coords)
+  Vec{Dim,T}(coords::SVector{Dim,T}) where {Dim,T<:Integer} = new{Dim,Float64}(coords)
+end
+
+# convenience constructors
+Vec{Dim,T}(coords...) where {Dim,T} = Vec{Dim,T}(SVector{Dim,T}(coords...))
+Vec{Dim}(coords::T...) where {Dim,T} = Vec{Dim,T}(SVector{Dim,T}(coords...))
+Vec(coords::SVector{Dim,T}) where {Dim,T} = Vec{Dim,T}(coords)
+Vec(coords::AbstractVector{T}) where {T} = Vec{length(coords),T}(coords)
+Vec(coords...) = Vec(SVector(coords...))
 
 # type aliases for convenience
 const Vec1  = Vec{1,Float64}
@@ -31,3 +56,7 @@ const Vec3  = Vec{3,Float64}
 const Vec1f = Vec{1,Float32}
 const Vec2f = Vec{2,Float32}
 const Vec3f = Vec{3,Float32}
+
+# StaticVector interface
+Base.getindex(v::Vec, i::Int) = getindex(v.coords, i)
+Base.size(v::Vec, i) = size(v.coords, i)
