efca0a73d5a9f0b0c2516c838e2596ec574df773
Elias Carvalho <eliascarvdev@gmail.com>
2022-09-07 13:34:14 -0300

    Experiment: Vec

--- src/vectors.jl
+++ src/vectors.jl
@@ -40,26 +40,23 @@ Vec3f(1, 2, 3) # explicitly ask for single precision
 
 ### Notes
 
-- A `Vec` is a subtype of `StaticVector` from StaticArrays.jl
 - Type aliases are `Vec1`, `Vec2`, `Vec3`, `Vec1f`, `Vec2f`, `Vec3f`
 """
-struct Vec{Dim,T} <: StaticVector{Dim,T}
+struct Vec{Dim,T}
   coords::SVector{Dim,T}
   Vec{Dim,T}(coords::SVector{Dim,T}) where {Dim,T} = new{Dim,T}(coords)
   Vec{Dim,T}(coords::SVector{Dim,T}) where {Dim,T<:Integer} = new{Dim,Float64}(coords)
 end
 
 # convenience constructors
-Vec{Dim,T}(coords...) where {Dim,T} = Vec{Dim,T}(SVector{Dim,T}(coords))
+Vec{Dim,T}(coords...) where {Dim,T} = Vec{Dim,T}(coords)
 Vec{Dim,T}(coords::Tuple) where {Dim,T} = Vec{Dim,T}(SVector{Dim,T}(coords))
 Vec{Dim,T}(coords::AbstractVector) where {Dim,T} = Vec{Dim,T}(SVector{Dim,T}(coords))
 
-Vec(coords::NTuple{Dim,T}) where {Dim,T} = Vec{Dim,T}(coords)
+Vec(coords...) = Vec(coords)
+Vec(coords::Tuple) = Vec(SVector(coords))
 Vec(coords::SVector{Dim,T}) where {Dim,T} = Vec{Dim,T}(coords)
 Vec(coords::AbstractVector{T}) where {T} = Vec{length(coords),T}(coords)
-
-Vec(coords...) = Vec(SVector(coords))
-Vec(coords::Tuple) = Vec(SVector(coords))
 
 # type aliases for convenience
 const Vec1  = Vec{1,Float64}
@@ -69,11 +66,31 @@ const Vec1f = Vec{1,Float32}
 const Vec2f = Vec{2,Float32}
 const Vec3f = Vec{3,Float32}
 
-# StaticVector interface
-Base.Tuple(v::Vec) = Tuple(v.coords)
-Base.getindex(v::Vec, i::Int) = getindex(v.coords, i)
-function StaticArrays.similar_type(::Type{<:Vec}, ::Type{T}, ::Size{S}) where {T,S}
-  L = prod(S)
-  N = length(S)
-  N == 1 ? Vec{L,T} : SArray{Tuple{S...}, T, N, L}
+"""
+    coordtype(vector)
+
+Return the machine type of each coordinate used to describe the `vector`.
+"""
+coordtype(::Type{Vec{Dim,T}}) where {Dim,T}  = T
+coordtype(v::Vec) = coordtype(typeof(v))
+
+"""
+    coordinates(vector)
+
+Return the coordinates of the vector with respect to the
+canonical Euclidean basis.
+"""
+coordinates(v::Vec) = v.coords
+
++(a::Vec, b::Vec) = Vec(a.coords + b.coords)
+
+-(a::Vec, b::Vec) = Vec(a.coords - b.coords)
+
+*(k::Number, a::Vec) = Vec(k * a.coords)
+*(a::Vec, k::Number) = k * a
+
+==(a::Vec, b::Vec) = a.coords == b.coords
+
+function Base.show(io::IO, v::Vec)
+  print(io, "Vec$(Tuple(v.coords))")
 end
