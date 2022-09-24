ee05791e613d1d48eb6769d278f4b91a073ee904
dorn-gerhard <67096719+dorn-gerhard@users.noreply.github.com>
2022-09-24 14:08:48 +0200

    use access functions origin & direction (rays.jl) (#300)

--- src/intersections/rays.jl
+++ src/intersections/rays.jl
@@ -22,19 +22,19 @@ function intersection(f, r1::Ray{N,T}, r2::Ray{N,T}) where {N,T}
     return @IT NoIntersection nothing f #CASE 6
   # collinear
   elseif r == rₐ == 1 
-    if r1.v ⋅ r2.v ≥ 0 # rays aligned in same direction
-      if (r1.p - r2.p) ⋅ r1.v ≥ 0 # origin of r1 ∈ r2
+    if direction(r1) ⋅ direction(r2) ≥ 0 # rays aligned in same direction
+      if (origin(r1) - origin(r2)) ⋅ direction(r1) ≥ 0 # origin of r1 ∈ r2
         return @IT OverlappingAgreeingRays r1 f # CASE 4: r1
       else
         return @IT OverlappingAgreeingRays r2 f # CASE 4: r2
       end
     else # colliding rays
-      if r1.p ∉ r2
+      if origin(r1) ∉ r2
         return @IT NoIntersection nothing f # CASE 6
-      elseif r1.p == r2.p
+      elseif origin(r1) == origin(r2)
         return @IT CornerTouchingRays a f # CASE 3
       else
-        return @IT OverlappingOpposingRays Segment(r1.p, r2.p) f # CASE 5
+        return @IT OverlappingOpposingRays Segment(origin(r1), origin(r2)) f # CASE 5
       end
     end
   # in same plane, not parallel
@@ -53,11 +53,11 @@ function intersection(f, r1::Ray{N,T}, r2::Ray{N,T}) where {N,T}
       if λ₂ == 0
         return @IT MidTouchingRays c f # CASE 2: origin of r2
       else
-        return @IT CrossingRays (a+λ₁*r1.v) f # CASE 1: equal to c + λ₂ * r2.v
+        return @IT CrossingRays r1(λ₁) f # CASE 1: equal to r2(λ₂)
       end
     end
   end
 end
 
 # compute the intersection of two rays assuming that it is a point
-intersectpoint(r1::Ray, r2::Ray) = intersectpoint(Line(r1.p, r1.p + r1.v), Line(r2.p, r2.p + r2.v))
+intersectpoint(r1::Ray, r2::Ray) = intersectpoint(Line(r1(0), r1(1)), Line(r2(0), r2(1)))
