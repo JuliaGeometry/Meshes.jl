5f72e3039916c08e60a6956e534ead406c6a94a0
dorn-gerhard <quantum.quentin@mailbox.org>
2022-10-18 08:59:54 +0200

    introduce approx as helper function

--- src/intersections/segments.jl
+++ src/intersections/segments.jl
@@ -1,7 +1,6 @@
 # ------------------------------------------------------------------
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
-
 #=
 The intersection type can be one of six types:
 1. intersect at one inner point (CrossingSegments -> Point)
@@ -15,17 +14,19 @@ function intersection(f, s1::Segment{N,T}, s2::Segment{N,T}) where {N,T}
   c, d = s2(0), s2(1)
 
   λ₁, λ₂, r, rₐ = intersectparameters(a, b, c, d)
+  # helper function to round values close to x (one or zero)
+  approx(λ, x) = isapprox(λ, x, atol=atol(T)) ? x : λ
 
   if r ≠ rₐ # not in same plane or parallel
     return @IT NoIntersection nothing f #CASE 5
   elseif r == rₐ == 1 # collinear
     # find parameters λc and λd for points c and d in s1
     # use dimension with largest vector component to avoid division by zero
-    ix = argmax(abs.(b-a))
-    v₁ = (b-a)[ix]
-    λc, λd = ((c-a)[ix], (d-a)[ix]) ./ v₁
-    λc = isapprox(λc, zero(T), atol=atol(T)) ? zero(T) : (isapprox(λc, one(T), atol=atol(T)) ? one(T) : λc)
-    λd = isapprox(λd, zero(T), atol=atol(T)) ? zero(T) : (isapprox(λd, one(T), atol=atol(T)) ? one(T) : λd)
+    v₁ = b-a
+    i = argmax(abs.(v₁))
+    λc, λd = ((c-a)[i], (d-a)[i]) ./ v₁[i]
+    λc = approx(approx(λc, 0), 1)
+    λd = approx(approx(λd, 0), 1)
     if (λc > 1 && λd > 1) || (λc < 0 && λd < 0)
       return @IT NoIntersection nothing f # CASE 5
     elseif (λc == 0 && λd < 0) ||  (λd == 0 && λc < 0)
@@ -33,13 +34,12 @@ function intersection(f, s1::Segment{N,T}, s2::Segment{N,T}) where {N,T}
     elseif (λc == 1 && λd > 1) || (λd == 1 && λc > 1)
       return @IT CornerTouchingSegments b f # CASE 3
     else
-      p = sort([0,1,λc,λd])
-      return @IT OverlappingSegments Segment(s1(maximum(p[1:2])), s1(minimum(p[3:4]))) f # CASE 4
+      parameters = sort([0,1,λc,λd])
+      return @IT OverlappingSegments Segment(s1(parameters[2]), s1(parameters[3])) f # CASE 4
     end
-  # in same plane, not parallel
-  else
-    λ₁ = isapprox(λ₁, zero(T), atol=atol(T)) ? zero(T) : (isapprox(λ₁, one(T), atol=atol(T)) ? one(T) : λ₁)
-    λ₂ = isapprox(λ₂, zero(T), atol=atol(T)) ? zero(T) : (isapprox(λ₂, one(T), atol=atol(T)) ? one(T) : λ₂)
+  else # in same plane, not parallel
+    λ₁ = approx(approx(λ₁, 0), 1)
+    λ₂ = approx(approx(λ₂, 0), 1)
     if λ₁ < 0 || λ₂ < 0 || λ₁ > 1 || λ₂ > 1
       return @IT NoIntersection nothing f # CASE 5
     # 8 cases remain
