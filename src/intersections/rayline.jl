61e7b17b8a2f9549485a13280e781f9ffee79d5f
dorn-gerhard <quantum.quentin@mailbox.org>
2022-11-02 08:06:42 +0100

    shorter variables

--- src/intersections/rayline.jl
+++ src/intersections/rayline.jl
@@ -10,22 +10,22 @@ The intersection type can be one of four types:
 3. overlap of line and ray (OverlappingRayLine -> Ray)
 4. do not overlap nor intersect (NoIntersection)
 =#
-function intersection(f, r1::Ray{N,T}, l1::Line{N,T}) where {N,T}
-  a, b = r1(0), r1(1)
-  c, d = l1(0), l1(1)
+function intersection(f, r::Ray{N,T}, l::Line{N,T}) where {N,T}
+  a, b = r(0), r(1)
+  c, d = l(0), l(1)
 
   λ₁, λ₂, r, rₐ = intersectparameters(a, b, c, d)
 
   if r ≠ rₐ # not in same plane or parallel
     return @IT NoIntersection nothing f # CASE 4
   elseif r == rₐ == 1 # collinear
-    return @IT OverlappingRayLine r1 f # CASE 3
+    return @IT OverlappingRayLine r f # CASE 3
   else # in same plane, not parallel
     λ₁ = isapprox(λ₁, 0, atol=atol(T)) ? 0 : λ₁
     if λ₁ > 0
-      return @IT CrossingRayLine r1(λ₁) f # CASE 1
+      return @IT CrossingRayLine r(λ₁) f # CASE 1
     elseif λ₁ == 0
-      return @IT TouchingRayLine origin(r1) f # CASE 2
+      return @IT TouchingRayLine origin(r) f # CASE 2
     else
       return @IT NoIntersection nothing f # CASE 4
     end
