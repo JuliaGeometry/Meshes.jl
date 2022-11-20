d51106441a075faeadbb99dd901261ee496ccc64
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-11-20 18:27:11 -0300

    Add more options for DouglasPeucker simplification

--- src/simplification/douglaspeucker.jl
+++ src/simplification/douglaspeucker.jl
@@ -3,10 +3,14 @@
 # ------------------------------------------------------------------
 
 """
-    DouglasPeucker(ϵ)
+    DouglasPeucker([ϵ]; min=3, max=typemax(Int), maxiter=10)
 
-The Douglas-Peucker algorithm for simplifying polygonal chains or
-polygonal areas given a deviation tolerance `ϵ`.
+Simplify geometries with Douglas-Peucker algorithm. The higher
+is the tolerance `ϵ`, the more agressive is the simplification.
+
+If the tolerance `ϵ` is not provided, perform binary search until
+the number of vertices is between `min` and `max` or until the
+number of iterations reaches a maximum `maxiter`.
 
 ## References
 
@@ -16,19 +20,71 @@ polygonal areas given a deviation tolerance `ϵ`.
 """
 struct DouglasPeucker{T} <: SimplificationMethod
   ϵ::T
+  min::Int
+  max::Int
+  maxiter::Int
 end
 
+DouglasPeucker(ϵ=nothing; min=3, max=typemax(Int), maxiter=10) =
+  DouglasPeucker(ϵ, min, max, maxiter)
+
 function simplify(chain::Chain, method::DouglasPeucker)
-  v = _simplify(vertices(chain), method.ϵ)
+  v = if isnothing(method.ϵ)
+    # perform binary search with other parameters
+    βsimplify(vertices(chain), method.min, method.max, method.maxiter)
+  else
+    # perform Douglas-Peucker ϵ-simplification
+    ϵsimplify(vertices(chain), method.ϵ)
+  end |> collect
   isclosed(chain) ? Chain([v; first(v)]) : Chain(v)
 end
 
+# simplification by means of binary search
+function βsimplify(v::AbstractVector{Point{Dim,T}},
+                   min, max, maxiter) where {Dim,T}
+  i = 0
+  u = v
+  n = length(u)
+  a = zero(T)
+  b = initeps(u)
+  while !(min ≤ n ≤ max) && i < maxiter
+    # midpoint candidate
+    ϵ = (a + b) / 2
+
+    # evaluate at midpoint
+    u = ϵsimplify(v, ϵ)
+    n = length(u)
+
+    # binary search
+    n < min && (b = ϵ)
+    n > max && (a = ϵ)
+
+    i += 1
+  end
+
+  u
+end
+
+# initial ϵ guess for a given chain
+function initeps(v::AbstractVector{Point{Dim,T}}) where {Dim,T}
+  n = length(v)
+  ϵ = typemax(T)
+  l = Line(first(v), last(v))
+  for i in 2:n-1
+    d = evaluate(Euclidean(), v[i], l)
+    d < ϵ && (ϵ = d)
+  end
+  2ϵ
+end
+
 # simplify chain assuming it is open
-function _simplify(v::AbstractVector{Point{Dim,T}}, ϵ) where {Dim,T}
+function ϵsimplify(v::AbstractVector{Point{Dim,T}}, ϵ) where {Dim,T}
   # find vertex with maximum distance
+  # to reference line
+  l = Line(first(v), last(v))
   imax, dmax = 0, zero(T)
   for i in 2:length(v)-1
-    d = evaluate(Euclidean(), v[i], Line(first(v), last(v)))
+    d = evaluate(Euclidean(), v[i], l)
     if d > dmax
       imax = i
       dmax = d
@@ -38,8 +94,8 @@ function _simplify(v::AbstractVector{Point{Dim,T}}, ϵ) where {Dim,T}
   if dmax < ϵ
     [first(v), last(v)]
   else
-    v₁ = _simplify(v[begin:imax], ϵ)
-    v₂ = _simplify(v[imax:end],   ϵ)
+    v₁ = ϵsimplify(v[begin:imax], ϵ)
+    v₂ = ϵsimplify(v[imax:end],   ϵ)
     [v₁[begin:end-1]; v₂]
   end
 end
--- test/simplification.jl
+++ test/simplification.jl
@@ -1,31 +1,44 @@
 @testset "Simplification" begin
-  c = Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
-  s1 = simplify(c, DouglasPeucker(T(0.1)))
-  s2 = simplify(c, DouglasPeucker(T(0.5)))
-  @test s1 == Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
-  @test s2 == Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)])
+  @testset "DouglasPeucker" begin
+    c = Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
+    s1 = simplify(c, DouglasPeucker(T(0.1)))
+    s2 = simplify(c, DouglasPeucker(T(0.5)))
+    @test s1 == Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
+    @test s2 == Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)])
 
-  p = PolyArea(Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)]))
-  s1 = simplify(p, DouglasPeucker(T(0.5)))
-  @test s1 == PolyArea(Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)]))
-  m = Multi([p, p])
-  s2 = simplify(m, DouglasPeucker(T(0.5)))
-  @test s2 == Multi([s1, s1])
-  d = GeometrySet([p, p])
-  s3 = simplify(d, DouglasPeucker(T(0.5)))
-  @test s3 == GeometrySet([s1, s1])
+    p = PolyArea(Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)]))
+    s1 = simplify(p, DouglasPeucker(T(0.5)))
+    @test s1 == PolyArea(Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)]))
+    m = Multi([p, p])
+    s2 = simplify(m, DouglasPeucker(T(0.5)))
+    @test s2 == Multi([s1, s1])
+    d = GeometrySet([p, p])
+    s3 = simplify(d, DouglasPeucker(T(0.5)))
+    @test s3 == GeometrySet([s1, s1])
 
-  # decimate is a helper function to simplify
-  # geometries with an appropriate method
-  b = Box(P2(0,0), P2(1,1))
-  s = decimate(b, 1.0)
-  @test s isa Polygon
-  @test nvertices(s) == 3
-  @test boundary(s) == Chain(P2[(0,0),(1,0),(0,1),(0,0)])
+    # perform binary search for ϵ tolerance
+    c = Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
+    s1 = simplify(c, DouglasPeucker(T(0.1)))
+    s2 = simplify(c, DouglasPeucker(max=6))
+    @test s1 == s2
+    s1 = simplify(c, DouglasPeucker(T(0.5)))
+    s2 = simplify(c, DouglasPeucker(max=4))
+    @test s1 == s2
+  end
 
-  c = Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
-  s1 = decimate(c, T(0.1))
-  s2 = decimate(c, T(0.5))
-  @test s1 == Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
-  @test s2 == Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)])
+  @testset "Utilities" begin
+    # decimate is a helper function to simplify
+    # geometries with an appropriate method
+    b = Box(P2(0,0), P2(1,1))
+    s = decimate(b, 1.0)
+    @test s isa Polygon
+    @test nvertices(s) == 3
+    @test boundary(s) == Chain(P2[(0,0),(1,0),(0,1),(0,0)])
+
+    c = Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
+    s1 = decimate(c, T(0.1))
+    s2 = decimate(c, T(0.5))
+    @test s1 == Chain(P2[(0,0),(1,0),(1.5,0.5),(1,1),(0,1),(0,0)])
+    @test s2 == Chain(P2[(0,0),(1.5,0.5),(0,1),(0,0)])
+  end
 end
