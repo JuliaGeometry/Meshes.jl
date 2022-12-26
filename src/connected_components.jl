3e1027960dd2228f9e8c3a27d1b37b64444c7f6e
stla <laurent_step@yahoo.fr>
2022-12-26 22:15:57 +0100

    improved algorithm

--- src/connected_components.jl
+++ src/connected_components.jl
@@ -8,9 +8,47 @@
 Return the connected components of `mesh` as a list of meshes.
 """
 function connected_components(mesh::Mesh)
+  # get faces
+  topo = convert(HalfEdgeTopology, topology(mesh))
+  ∂₂₀ = Boundary{2,0}(topo)
+  elemsind = collect(1:nelements(topo))
+
+  # get vertices
+  points = vertices(mesh)
+  
+  # get connected components
   graph = _meshgraph(mesh)
   components = connected_components(graph)
-  [_meshcomponent(mesh, c) for c in components]
+  meshes = Vector{SimpleMesh}(undef, length(components))
+
+  # iterate over the connected components
+  for (i, component) in enumerate(components)
+    # dictionary to map old indices to new indices
+    newinds = Dict{Int,Int}()
+    for (j, index) in enumerate(component)
+      newinds[index] = j
+    end
+    # vector to store the indices of the faces we will find, 
+    # in order to delete them for the next iterations
+    todelete = Vector{Int}(undef, 0)
+    # make the connectivities of the mesh component
+    connec = Vector{Connectivity}(undef, 0)
+    for faceind in elemsind
+      faceinds = ∂₂₀(faceind) 
+      if faceinds[1] in component
+        facenewinds = [newinds[i] for i in faceinds]
+        connection = connect(tuple(facenewinds...))
+        push!(connec, connection)
+        push!(todelete, faceind)
+      end
+    end
+    # make the mesh of this component
+    meshes[i] = SimpleMesh(points[component], connec)
+    # delete the indices of the faces we found
+    setdiff!(elemsind, todelete)
+  end
+
+  meshes
 end
 
 # construct the graph associated to a mesh
@@ -28,33 +66,3 @@ function _meshgraph(mesh)
 
   graph
 end
-
-# make the mesh corresponding to a connected component
-# (a component is a vector of vertex indices)
-function _meshcomponent(mesh, component)
-  # dictionary to map old indices to new indices
-  newinds = Dict{Int,Int}()
-  for (j, index) in enumerate(component)
-    newinds[index] = j
-  end
-
-  # get faces
-  topo = convert(HalfEdgeTopology, topology(mesh))
-  ∂₂₀ = Boundary{2,0}(topo)
-
-  # make the connectivities of the mesh component
-  connec = Vector{Connectivity}(undef, 0)
-  for faceind in 1:nelements(topo)
-    faceinds = ∂₂₀(faceind) 
-    if faceinds[1] in component
-      facenewinds = [newinds[i] for i in faceinds]
-      connectivity = connect(tuple(facenewinds...))
-      push!(connec, connectivity)
-    end
-  end
-
-  SimpleMesh(vertices(mesh)[component], connec)
-end
-
-
-
