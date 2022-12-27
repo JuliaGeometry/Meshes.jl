bde86d2c2111df5a4f9684979b128416a1119057
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-12-27 09:02:10 -0300

    Small adjustments

--- src/refinement/loopsubdivision.jl
+++ src/refinement/loopsubdivision.jl
@@ -17,44 +17,46 @@ then subdividing each triangle into four triangles.
 struct LoopSubdivision <: RefinementMethod end
 
 function refine(mesh, ::LoopSubdivision)
+  # triangulate mesh if necessary
   tmesh = eltype(mesh) <: Triangle ? mesh : simplexify(mesh)
 
-  # retrieve vertices
+  # retrieve initial list of points
   points = vertices(tmesh)
-  npoints = length(points)
+
+  # initial number of points and triangles
+  np = nvertices(tmesh)
+  nt = nelements(tmesh)
 
   # convert topology to half-edge structure
   t = convert(HalfEdgeTopology, topology(tmesh))
-  ntriangles = nelements(t)
 
   # add midpoints of edges
   midpoints = Dict{Tuple{Int,Int},Int}()
   ∂₁₀ = Boundary{1,0}(t)
   for eind in 1:nfacets(t)
     i, j = sort(∂₁₀(eind))
-    midpoint = Segment(points[i], points[j])(0.5)
-    push!(points, midpoint)
-    npoints = npoints + 1
-    midpoints[(i, j)] = npoints
+    edge = Segment(points[i], points[j])
+    push!(points, center(edge))
+    midpoints[(i, j)] = (np += 1)
   end
 
   # construct subtriangles of faces
   ∂₂₀ = Boundary{2,0}(t)
-  triangles = Vector{Tuple{Int,Int,Int}}(undef, 4 * ntriangles)
-  for tind in 1:ntriangles
-    v1, v2, v3 = ∂₂₀(tind)
-    m12 = midpoints[_ordered_pair(v1, v2)]
-    m23 = midpoints[_ordered_pair(v2, v3)]
-    m31 = midpoints[_ordered_pair(v3, v1)]
-    k = 4 * (tind - 1)
-    triangles[k + 1] = (v1, m12, m31)
-    triangles[k + 2] = (v2, m23, m12)
-    triangles[k + 3] = (v3, m31, m23)
-    triangles[k + 4] = (m12, m23, m31)
+  triangles = Vector{Tuple{Int,Int,Int}}(undef, 4nt)
+  for tind in 1:nt
+    t = 4*(tind - 1)
+    i, j, k = ∂₂₀(tind)
+    m1 = midpoints[_ordered(i, j)]
+    m2 = midpoints[_ordered(j, k)]
+    m3 = midpoints[_ordered(k, i)]
+    triangles[t+1] = ( i, m1, m3)
+    triangles[t+2] = ( j, m2, m1)
+    triangles[t+3] = ( k, m3, m2)
+    triangles[t+4] = (m1, m2, m3)
   end
 
-  SimpleMesh(points, connect.(triangles, Triangle))
+  SimpleMesh(points, connect.(triangles))
 end
 
-_ordered_pair(i ,j) = i < j ? (i, j) : (j, i)
+_ordered(i ,j) = i < j ? (i, j) : (j, i)
 
