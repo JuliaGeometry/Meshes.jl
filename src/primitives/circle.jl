f865ed19b03a65dde2d1624d2923ee70b451a79b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-02-27 12:09:17 -0300

    Update src/primitives/circle.jl

--- src/primitives/circle.jl
+++ src/primitives/circle.jl
@@ -36,7 +36,6 @@ end
 Circle(p1::Tuple, p2::Tuple, p3::Tuple) =
   Circle(Point(p1), Point(p2), Point(p3))
 
-
 Circle(plane::Plane{T}, radius) where {T} = Circle(plane, T(radius))
 
 paramdim(::Type{<:Circle}) = 1
