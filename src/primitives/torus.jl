f500d22c31ea7fcfd55d8ce9505a4313c4440767
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-03-27 08:19:04 -0300

    Update src/primitives/torus.jl

--- src/primitives/torus.jl
+++ src/primitives/torus.jl
@@ -23,7 +23,7 @@ Torus(center::Tuple, normal::Tuple, major, minor) =
   Torus(Point(center), Vec(normal), major, minor)
 
 """
-    Torus(p1, p2, p3)
+    Torus(p1, p2, p3, minor)
 
 The torus whose equator passes through points `p1`, `p2` and `p3` and with
 minor radius `minor`.
