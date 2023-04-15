ae938c3d18785f6eb8a23004185334585ba4e0db
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-04-15 09:27:16 -0300

    Adjust types

--- src/angles.jl
+++ src/angles.jl
@@ -8,8 +8,9 @@
 Angle ∠ABC between rays BA and BC.
 See https://en.wikipedia.org/wiki/Angle.
 
-Vec2 angles return a value in range [-π, π].
-Vec3 angles return a value in range [0, π].
+Return a value in range [-π, π] for 2D
+angles and a value in range [0, π] for
+3D angles.
 
 ## Examples
 
@@ -17,8 +18,7 @@ Vec3 angles return a value in range [0, π].
 ∠(Point(1,0), Point(0,0), Point(0,1)) == π/2
 ```
 """
-∠(A::P, B::P, C::P) where {P<:Point{2}} = ∠(A-B, C-B)
-∠(A::P, B::P, C::P) where {P<:Point{3}} = ∠(A-B, C-B)
+∠(A::Point, B::Point, C::Point) = ∠(A-B, C-B)
 
 """
     ∠(u, v)
@@ -26,44 +26,35 @@ Vec3 angles return a value in range [0, π].
 Angle between vectors u and v.
 See https://en.wikipedia.org/wiki/Angle.
 
-Vec2 angles return a value in range [-π, π].
-Vec3 angles return a value in range [0, π].
+Return a value in range [-π, π] for 2D
+angles and a value in range [0, π] for
+3D angles.
 
 ## Examples
 
 ```julia
 ∠(Vec(1,0), Vec(0,1)) == π/2
 ```
-
-Thank you Jeffrey Sarnoff for contributing this work.
 """
-function ∠(u::V, v::V) where {V<:Vec}
-    T = eltype(u.coords)
+function ∠(u::Vec, v::Vec)
+    T = eltype(u)
 
-    u_unit = unitize(u)
-    v_unit = unitize(v)
+    uunit = unitize(u)
+    vunit = unitize(v)
 
-    y = norm2(u_unit .- v_unit)
-    x = norm2(u_unit .+ v_unit)
+    y = norm2(uunit .- vunit)
+    x = norm2(uunit .+ vunit)
 
     a = 2 * atan(y, x)
 
     a = !(signbit(a) || signbit(pi - a)) ? a : (signbit(a) ? zero(T) : (T)(pi))
 
-    ifelse(isnegangle(u_unit, v_unit), -a, a)
+    ifelse(isnegangle(uunit, vunit), -a, a)
 end
 
-@inline norm2(u::V) where {V<:Vec} = sqrt(foldl(+, abs2.(u.coords)))
-@inline norm2(u::NTuple) = sqrt(foldl(+, abs2.(u)))
+@inline norm2(u::Vec) = sqrt(foldl(+, abs2.(u)))
 
-@inline unitize(u::V) where {V<:Vec} = u.coords ./ norm2(u)
-@inline unitize(u::NTuple) = u ./ norm2(u)
+@inline unitize(u::Vec) = u ./ norm2(u)
 
-@inline isnegangle(u::V, v::V) where {V<:Vec3} = false
-@inline isnegangle(u::V, v::V) where {V<:Vec2} =
-    u.coords[1] * v.coords[2] < u.coords[2] * v.coords[1]
-
-@inline isnegangle(u::NTuple{3,T}, v::NTuple{3,T}) where {T} = false
-@inline isnegangle(u::NTuple{2,T}, v::NTuple{2,T}) where {T} =
-    u.coords[1] * v.coords[2] < u.coords[2] * v.coords[1]
-
+@inline isnegangle(u::Vec{3,T}, v::Vec{3,T}) where {T} = false
+@inline isnegangle(u::Vec{2,T}, v::Vec{2,T}) where {T} = u[1] * v[2] < u[2] * v[1]
