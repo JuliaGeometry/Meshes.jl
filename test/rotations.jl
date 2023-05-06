b344d24fa6fbc48341f4df3b5a518183b31d168a
hyrodium <hyrodium@gmail.com>
2023-05-06 22:38:21 +0900

    update test/rotations.jl to remove `ClockwiseAngle` and `CounterClockAngle`

--- test/rotations.jl
+++ test/rotations.jl
@@ -1,11 +1,11 @@
 @testset "Rotations" begin
-  cw = ClockwiseAngle(T(π/2))
-  R  = convert(DCM, cw)
+  # ClockwiseAngle
+  R = Angle2d(T(-π/2))
   @test eltype(R) == T
   @test R ≈ T[0 1; -1 0]
 
-  ccw = CounterClockwiseAngle(T(π/2))
-  R  = convert(DCM, ccw)
+  # CounterClockwiseAngle
+  R = Angle2d(T(π/2))
   @test eltype(R) == T
   @test R ≈ T[0 -1; 1 0]
 end
