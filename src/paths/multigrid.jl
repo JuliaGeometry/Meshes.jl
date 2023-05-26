719f6d795a822fac0ea5123d6de675e1752ee299
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-05-26 10:59:17 -0300

    Update src/paths/multigrid.jl

--- src/paths/multigrid.jl
+++ src/paths/multigrid.jl
@@ -12,10 +12,11 @@ struct MultiGridPath <: Path end
 function traverse(grid::Grid{Dim}, ::MultiGridPath) where {Dim}
   path = Int[]
   dims = size(grid)
+  nelem = prod(dims)
+  
   steps = dims .- 1
-  nelems = prod(dims)
   linear = LinearIndices(dims)
-  while length(path) < nelems
+  while length(path) < nelem
     ranges = ntuple(d -> 1:steps[d]:dims[d], Dim)
     cinds = CartesianIndices(ranges)
     linds = [linear[cind] for cind in cinds]
