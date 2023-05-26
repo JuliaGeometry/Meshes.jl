e422c38a2c2287d21e40941c8fb7e805344e6609
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-05-26 13:38:23 -0300

    Update docstring

--- src/paths.jl
+++ src/paths.jl
@@ -7,6 +7,11 @@
 
 A path is a sequence of indices that can be
 used to traverse a given [`Domain`](@ref).
+
+## References
+
+* Nussbaumer et al. 2017. [Which Path to Choose in Sequential Gaussian Simulation]
+  (https://link.springer.com/article/10.1007/s11004-017-9699-5)
 """
 abstract type Path end
 
