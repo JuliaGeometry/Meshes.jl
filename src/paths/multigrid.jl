7688a15a1983f34215afd6e7d6ec641072345815
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-26 10:00:51 -0300

    Apply suggestions

--- src/paths/multigrid.jl
+++ src/paths/multigrid.jl
@@ -13,12 +13,13 @@ function traverse(grid::Grid{Dim}, ::MultiGridPath) where {Dim}
   path = Int[]
   sizes = size(grid)
   steps = sizes .- 1
-  nelems = nelements(grid)
+  nelems = prod(sizes)
   linear = LinearIndices(sizes)
   while length(path) < nelems
-    cinds = CartesianIndices(ntuple(d -> 1:steps[d]:sizes[d], Dim))
-    inds = [linear[cind] for cind in cinds]
-    append!(path, setdiff(inds, path))
+    ranges = ntuple(d -> 1:steps[d]:sizes[d], Dim)
+    cinds = CartesianIndices(ranges)
+    linds = [linear[cind] for cind in cinds]
+    append!(path, setdiff(linds, path))
     steps = ceil.(Int, steps ./ 2)
   end
   path
