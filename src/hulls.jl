d7821aeaf7fee1ef24fef6fb211dbb107a17d3b1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-06-03 17:55:19 -0300

    Refactor hulls.jl

--- src/hulls.jl
+++ src/hulls.jl
@@ -25,11 +25,11 @@ hull(g::Polytope, method) = hull(vertices(g), method)
 
 hull(p::Primitive, method) = hull(boundary(p), method)
 
+hull(m::Multi, method) = hull(collect(m), method)
+
 hull(d::Domain, method) = hull(collect(d), method)
 
 hull(d::Data, method) = hull(domain(d), method)
-
-hull(g::AbstractVector{<:Geometry}, method) = hull(Multi(g), method)
 
 # ---------
 # DEFAULTS
@@ -43,11 +43,13 @@ hull(d::Domain{2}) = hull(d, GrahamScan())
 # SPECIALIZATIONS
 # ----------------
 
-hull(p::Point) = p
+hull(p::Point) = boundingbox(p)
 
 hull(b::Box) = b
 
 hull(b::Ball) = b
+
+hull(s::Sphere) = Ball(center(s), radius(s))
 
 hull(t::Triangle) = t
 
@@ -56,5 +58,10 @@ hull(g::Grid) = boundingbox(g)
 # ----------------
 # IMPLEMENTATIONS
 # ----------------
+
+function hull(geoms::AbstractVector{<:Geometry}, method)
+  verts(geom) = vertices(discretize(boundary(geom)))
+  hull(mapreduce(verts, vcat, geoms), method)
+end
 
 include("hulls/graham.jl")
--- test/hulls.jl
+++ test/hulls.jl
@@ -1,28 +1,28 @@
 @testset "Hulls" begin
   @testset "GrahamScan" begin
     # basic test
-    pset = PointSet(rand(P2, 100))
+    pset = rand(P2, 100)
     chul = hull(pset, GrahamScan())
     @test all(pset .∈ Ref(chul))
 
     # duplicated points
-    pset = PointSet([rand(P2, 100); rand(P2, 100)])
+    pset = [rand(P2, 100); rand(P2, 100)]
     chul = hull(pset, GrahamScan())
     @test all(pset .∈ Ref(chul))
 
     # corner cases
-    pset = PointSet(P2[(0, 0)])
+    pset = P2[(0, 0)]
     chul = hull(pset, GrahamScan())
     @test chul == P2(0, 0)
-    pset = PointSet(P2[(0, 1), (1, 0)])
+    pset = P2[(0, 1), (1, 0)]
     chul = hull(pset, GrahamScan())
     @test chul == Segment(P2(1, 0), P2(0, 1))
-    pset = PointSet(P2[(1, 0), (0, 0), (0, 1)])
+    pset = P2[(1, 0), (0, 0), (0, 1)]
     chul = hull(pset, GrahamScan())
     @test vertices(boundary(chul)) == P2[(0, 0), (1, 0), (0, 1)]
 
     # original point set is already in hull
-    pset = PointSet(P2[(0, 0), (1, 0), (1, 1), (0, 1), (0.5, -1)])
+    pset = P2[(0, 0), (1, 0), (1, 1), (0, 1), (0.5, -1)]
     chul = hull(pset, GrahamScan())
     verts = vertices(boundary(chul))
     @test verts == P2[(0.5, -1), (1, 0), (1, 1), (0, 1), (0, 0)]
@@ -30,9 +30,26 @@
     # random points in interior do not affect result
     p1 = P2[(0, 0), (1, 0), (1, 1), (0, 1), (0.5, -1)]
     p2 = P2[0.5 .* (rand(), rand()) .+ 0.5 for _ in 1:10]
-    pset = PointSet([p1; p2])
+    pset = [p1; p2]
     chul = hull(pset, GrahamScan())
     verts = vertices(boundary(chul))
     @test verts == P2[(0.5, -1), (1, 0), (1, 1), (0, 1), (0, 0)]
+  end
+
+  @testset "Miscelanneous" begin
+    @test hull(P2(0, 0)) == Box(P2(0, 0), P2(0, 0))
+
+    @test hull(Box(P2(0, 0), P2(1, 1))) == Box(P2(0, 0), P2(1, 1))
+
+    @test hull(Ball(P2(0, 0), T(1))) == Ball(P2(0, 0), T(1))
+    @test hull(Ball(P2(1, 1), T(1))) == Ball(P2(1, 1), T(1))
+
+    @test hull(Sphere(P2(0, 0), T(1))) == Ball(P2(0, 0), T(1))
+    @test hull(Sphere(P2(1, 1), T(1))) == Ball(P2(1, 1), T(1))
+
+    b1 = Box(P2(0, 0), P2(1, 1))
+    b2 = Box(P2(-1, -1), P2(0.5, 0.5))
+    @test hull(Multi([b1, b2])) == PolyArea(P2[(-1, -1), (0.5, -1), (1, 0), (1, 1), (0, 1), (-1, 0.5)])
+    @test hull(Collection([b1, b2])) == PolyArea(P2[(-1, -1), (0.5, -1), (1, 0), (1, 1), (0, 1), (-1, 0.5)])
   end
 end
