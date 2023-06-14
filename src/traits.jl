aa758991f2799f606be82b45f1634fc0be0aaf1b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-06-14 11:49:18 -0300

    Refactor source files

--- src/traits.jl
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------
-# Licensed under the MIT License. See LICENSE in the project root.
-# ------------------------------------------------------------------
-
-include("traits/domain.jl")
-include("traits/data.jl")
-
-# type alias for convenience
-const DomainOrData = Union{Domain,Data}
