187fad6b1f410822931e0bfaa2b292e1ce7baeeb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-06-19 17:24:46 -0300

    Update docstring of GrahamScan

--- src/hulls/graham.jl
+++ src/hulls/graham.jl
@@ -6,10 +6,10 @@
     GrahamScan
 
 Compute the convex hull of a set of points or geometries using the
-Graham's scan method. See [https://en.wikipedia.org/wiki/Graham_scan]
+Graham's scan algorithm. See [https://en.wikipedia.org/wiki/Graham_scan]
 (https://en.wikipedia.org/wiki/Graham_scan).
 
-The method has complexity `O(n*log(n))` where `n` is the number of points.
+The algorithm has complexity `O(n*log(n))` where `n` is the number of points.
 
 ## References
 
