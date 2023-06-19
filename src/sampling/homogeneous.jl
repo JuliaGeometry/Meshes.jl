6e092aebb274bdcd6e13625ccea0ce131a02598d
Erick <eral.th07@gmail.com>
2023-06-19 13:14:50 -0500

    doscstring and test for homogeneous sampling with custom weights

--- src/sampling/homogeneous.jl
+++ src/sampling/homogeneous.jl
@@ -3,10 +3,11 @@
 # ------------------------------------------------------------------
 
 """
-    HomogeneousSampling(size)
+    HomogeneousSampling(size, [weights])
 
 Generate sample of given `size` from geometric object
-according to a homogeneous density.
+according to a homogeneous density. Optionally, provide `weights`
+to specify custom sampling weights for the elements of a domain.
 """
 struct HomogeneousSampling{W} <: ContinuousSamplingMethod
   size::Int
