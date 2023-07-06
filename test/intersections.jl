d535352037042d917e74be0b65a957e0d2f80a1d
Elias Carvalho <eliascarvdev@gmail.com>
2023-07-06 13:08:03 -0300

    Fix tests

--- test/intersections.jl
+++ test/intersections.jl
@@ -1231,8 +1231,8 @@
     b2 = Box(P2(2, 2), P2(5, 5))
     @test hasintersect(b1, b2)
     @test hasintersect(b2, b1)
-    @test @elapsed(hasintersect(b1, b2)) > 8e-6
-    @test @allocated(hasintersect(b1, b2)) > 50
+    @test @elapsed(hasintersect(b1, b2)) < 8e-6
+    @test @allocated(hasintersect(b1, b2)) < 50
 
     # partial application
     points = P2[(0, 0), (1, 0), (1, 1), (0, 1)]
