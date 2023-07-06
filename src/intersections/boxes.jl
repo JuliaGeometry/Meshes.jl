9b885c785a0c563c43067a92a05b081d563960cd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-07-06 17:03:42 -0300

    More fixes for Box-Box intersection

--- src/intersections/boxes.jl
+++ src/intersections/boxes.jl
@@ -2,7 +2,13 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-function intersection(f, box₁::Box, box₂::Box)
+# The intersection type can be one of four types:
+# 
+# 1. overlap with non-zero measure (Overlapping -> Box)
+# 2. intersect at corner point (CornerTouching -> Point)
+# 3. intersect at one of the facets (Touching -> Box)
+# 4. do not overlap nor intersect (NotIntersecting -> Nothing)
+function intersection(f, box₁::Box{Dim,T}, box₂::Box{Dim,T}) where {Dim,T}
   # retrieve corner points
   m1, M1 = coordinates.(extrema(box₁))
   m2, M2 = coordinates.(extrema(box₂))
@@ -11,9 +17,18 @@ function intersection(f, box₁::Box, box₂::Box)
   u = Point(max.(m1, m2))
   v = Point(min.(M1, M2))
 
+  # auxiliary variables
+  δ = v - u
+  δ̄ = abs.(δ)
+  τ = atol(T)
+
   # branch on possible configurations
-  if u ≺ v
+  if all(>(τ), δ)
     return @IT Overlapping Box(u, v) f
+  elseif all(<(τ), δ̄)
+    return @IT CornerTouching u f
+  elseif any(<(τ), δ̄)
+    return @IT Touching (u ⪯ v ? Box(u, v) : Box(v, u)) f
   else
     return @IT NotIntersecting nothing f
   end
