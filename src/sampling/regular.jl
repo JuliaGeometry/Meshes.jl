3c32ad1245c459e3a86d0636cd16adb2b4777d80
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-07-22 20:19:36 -0300

    Refactor regular sampling

--- src/sampling/regular.jl
+++ src/sampling/regular.jl
@@ -45,6 +45,10 @@ soffset(b::Ball) = (n -> inv(n + 1), soffset(boundary(b))...)
 eoffset(b::Ball) = (n -> zero(n), eoffset(boundary(b))...)
 extrapoints(b::Ball) = (center(b),)
 
+soffset(::CylinderSurface) = (n -> zero(n), n -> zero(n))
+eoffset(::CylinderSurface) = (n -> inv(n), n -> zero(n))
+extrapoints(c::CylinderSurface) = (bottom(c)(0, 0), top(c)(0, 0))
+
 soffset(g::Geometry) = ntuple(i -> (n -> zero(n)), paramdim(g))
 eoffset(g::Geometry) = ntuple(i -> (n -> isperiodic(g)[i] ? inv(n) : zero(n)), paramdim(g))
 extrapoints(::Geometry) = ()
@@ -52,49 +56,6 @@ extrapoints(::Geometry) = ()
 # --------------
 # SPECIAL CASES
 # --------------
-
-function sample(::AbstractRNG, cylsurf::CylinderSurface{T}, method::RegularSampling) where {T}
-  V = T <: AbstractFloat ? T : Float64
-  sz = fitdims(method.sizes, paramdim(cylsurf))
-  c = center(cylsurf)
-  r = radius(cylsurf)
-  b = bottom(cylsurf)
-  t = top(cylsurf)
-  a = axis(cylsurf)
-  d = a(1) - a(0)
-  l = norm(d)
-
-  φmin, φmax = V(0), V(2π)
-  zmin, zmax = V(0), V(1)
-  δφ = (φmax - φmin) / sz[1]
-  φs = range(φmin, stop=φmax - δφ, length=sz[1])
-  zs = range(zmin, stop=zmax, length=sz[2])
-
-  # rotation to align z axis with cylinder axis
-  Q = rotation_between(d, Vec{3,T}(0, 0, 1))
-
-  # new normals of planes in new rotated system
-  nᵦ = Q * normal(b)
-  nₜ = Q * normal(t)
-
-  # given cylindrical coordinates (r*cos(φ), r*sin(φ), z) and the
-  # equation of the plane, we can solve for z and find all points
-  # along the ellipse obtained by intersection
-  zᵦ(φ) = -l / 2 - (r * cos(φ) * nᵦ[1] + r * sin(φ) * nᵦ[2]) / nᵦ[3]
-  zₜ(φ) = +l / 2 - (r * cos(φ) * nₜ[1] + r * sin(φ) * nₜ[2]) / nₜ[3]
-  cᵦ(φ) = Point(r * cos(φ), r * sin(φ), zᵦ(φ))
-  cₜ(φ) = Point(r * cos(φ), r * sin(φ), zₜ(φ))
-
-  function point(φ, z)
-    pᵦ, pₜ = cᵦ(φ), cₜ(φ)
-    p = pᵦ + z * (pₜ - pᵦ)
-    Point(Q' * coordinates(p) + coordinates(c))
-  end
-
-  iᵣ = (point(φ, z) for z in zs for φ in φs)
-  iₚ = (b(0, 0), t(0, 0))
-  Iterators.flatmap(identity, (iᵣ, iₚ))
-end
 
 function sample(rng::AbstractRNG, grid::CartesianGrid, method::RegularSampling)
   sample(rng, boundingbox(grid), method)
