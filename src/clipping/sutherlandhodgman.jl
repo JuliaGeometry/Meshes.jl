f4bebc2d176096c748f6d61aa511c6db0d10cc9b
ClaroHenrique <38709777+ClaroHenrique@users.noreply.github.com>
2023-08-14 10:33:43 -0300

    Add clipping algorithm

--- src/clipping/sutherlandhodgman.jl
+++ src/clipping/sutherlandhodgman.jl
@@ -14,6 +14,50 @@ The Sutherland-Hodgman algorithm for clipping polygons.
 """
 struct SutherlandHodgman <: ClippingMethod end
 
-function clip(poly::Polygon, other::Geometry, ::SutherlandHodgman)
-  @error "not implemented"
+
+clip(poly::T, window::Polygon, ::SutherlandHodgman) where {T <: Polygon} = T(clip(vertices(poly), segments(window)))
+
+function clip(poly::PolyArea, window::Polygon, ::SutherlandHodgman)
+  r = map(rings(poly)) do ring
+    clip(ring, window, ::SutherlandHodgman)
+  end
+  PolyArea(first(r), r[2:end])
+end
+
+# ---------------
+# IMPLEMENTATION
+# ---------------
+
+function clip(v::AbstractVector{Point}, window::AbstractVector{Segment}, ::SutherlandHodgman)
+  # clip each segment of the window one per time
+  for s in window
+    v = clip(v, s, ::SutherlandHodgman)
+  end
+  v
+end
+
+function clip(v::AbstractVector{Point}, window::Segment, ::SutherlandHodgman)
+  n = length(v)
+  new_v = []
+
+  for i in 1:n
+    p1 = v[i]
+    p2 = v[(i%n)+1]
+
+    # assuming convex clockwise window
+    p1_visible = (sideof(p1, window) != :LEFT) 
+    p2_visible = (sideof(p2, window) != :LEFT)
+
+    if p1_visible && p2_visible
+      push!(new_v, p1)
+    elseif p1_visible && !p2_visible
+      p_intersection = Segment(p1, p2) ∩ window
+      push!(new_v, p1)
+      push!(new_v, p_intersection)
+    elseif !p1_visible && p2_visible
+      p_intersection = Segment(p1, p2) ∩ window
+      push!(new_v, p_intersection)
+    end
+  end
+  new_v
 end
