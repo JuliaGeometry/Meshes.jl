c6e65837c87f9a07ec656ed113ee01de6c6add5f
ClaroHenrique <claro.sales@reserva.ink>
2023-08-15 15:36:45 -0300

    Add test

--- test/clipping.jl
+++ test/clipping.jl
@@ -1,20 +1,52 @@
 @testset "Clipping" begin
-  @test "Triangle" begin
-    poly = Triangle(P2(0,2), P2(3,5), P2(6,2))
-    other = Quadrangle(P2(0,0), P2(0,4), P2(5,4), P2(5,0))
+  @testset "Triangle" begin
+    poly = Triangle(P2(0, 2), P2(3, 5), P2(6, 2))
+    other = Quadrangle(P2(0, 0), P2(0, 4), P2(5, 4), P2(5, 0))
 
-    newpoly = clip(poly, window, SutherlandHodgman())
+    newpoly = clip(poly, other, SutherlandHodgman())
 
     @test length(rings(newpoly)) == 1
-    @test first(rings(clippedpoly)) ≈ Ring(P2[(0,2), (2,4), (4,4), (5,3), (5,2)])
+    @test first(rings(newpoly)) ≈ Ring(P2[(0, 2), (2, 4), (4, 4), (5, 3), (5, 2)])
   end
 
-  @test "Random" begin
-    poly = Ngon(rand(P2, 20)...)
-    other = Quadrangle(P2(0,0), P2(0,1/2), P2(1/2,1/2), P2(1/2,0))
+  @testset "Octagon" begin
+    poly = Octagon(
+      P2(2,-2),
+      P2(2, 1),
+      P2(4, 1),
+      P2(6, 3),
+      P2(4, 3),
+      P2(2, 5),
+      P2(8, 5),
+      P2(8,-2)
+    )
 
-    inpoints = [p for p in vertices(poly) if p ∈ other]
-    outpoints = [p for p in vertices(poly) if p ∉ other]
+    other = Quadrangle(P2(0, 0), P2(0, 4), P2(5, 4), P2(5, 0))
+    
+    expected = Ring(
+      P2(2, 0),
+      P2(2, 1),
+      P2(4, 1),
+      P2(5, 2),
+      P2(5, 3),
+      P2(4, 3),
+      P2(3, 4),
+      P2(5, 4),
+      P2(5, 0)
+    )
+
+    newpoly = clip(poly, other, SutherlandHodgman())
+
+    @test length(rings(newpoly)) == 1
+    @test first(rings(newpoly)) ≈ expected
+  end
+
+  @testset "Random" begin
+    poly = Ngon(rand(P2, 10)...)
+    other = Quadrangle(P2(0, 0), P2(0, 1/2), P2(1/2, 1/2), P2(1/2, 0))
+
+    inpoints = filter(p -> p ∈ other, vertices(poly))
+    outpoints = filter(p -> p ∉ other, vertices(poly))
 
     newpoly = clip(poly, other, SutherlandHodgman())
     v = vertices(newpoly)
