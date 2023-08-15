7983d4766663370f27730e7ca6150c0f1906f7f6
ClaroHenrique <claro.sales@reserva.ink>
2023-08-15 14:47:00 -0300

    Add random test

--- test/clipping.jl
+++ test/clipping.jl
@@ -1,8 +1,28 @@
 @testset "Clipping" begin
-  window = Ring(Point.([(0,0), (0,4), (5,4),(5,0)]))
-  poly = PolyArea(Point.([(0,2), (3,5), (6,2)]))
+  @test "Triangle" begin
+    poly = Triangle(P2(0,2), P2(3,5), P2(6,2))
+    other = Quadrangle(P2(0,0), P2(0,4), P2(5,4), P2(5,0))
 
-  clippedpoly = clip(poly, window, SutherlandHodgman())
+    newpoly = clip(poly, window, SutherlandHodgman())
 
-  @test clippedpoly ≈ PolyArea(Point.([(0,2), (2,4), (4,4), (5,3), (5,2)]))
+    @test length(rings(newpoly)) == 1
+    @test first(rings(clippedpoly)) ≈ Ring(P2[(0,2), (2,4), (4,4), (5,3), (5,2)])
+  end
+
+  @test "Random" begin
+    poly = Ngon(rand(P2, 20)...)
+    other = Quadrangle(P2(0,0), P2(0,1/2), P2(1/2,1/2), P2(1/2,0))
+
+    inpoints = [p for p in vertices(poly) if p ∈ other]
+    outpoints = [p for p in vertices(poly) if p ∉ other]
+
+    newpoly = clip(poly, other, SutherlandHodgman())
+    v = vertices(newpoly)
+
+    @test newpoly isa PolyArea
+    @test length(rings(newpoly)) == 1
+    @test v ⊆ other
+    @test inpoints ⊆ v
+    @test isempty(outpoints ∩ v)
+  end
 end
