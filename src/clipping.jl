c208ff2c1530d26ad2c09cdebafd7a492b63a4e0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-08-18 07:17:09 -0300

    Fix indentation and docstring

--- src/clipping.jl
+++ src/clipping.jl
@@ -12,19 +12,18 @@ abstract type ClippingMethod end
 """
     clip(geometry, other, method)
 """
-
 function clip(poly::Polygon, other::Geometry, method::ClippingMethod) 
-    c = [clip(ring, boundary(other), method) for ring in rings(poly)]
-    r = filter(!isnothing, c)
-  
-    if isempty(r)
-      nothing
-    elseif hasholes(poly)
-      PolyArea(r[1], r[2:end])
-    else
-      PolyArea(r[1])
-    end
+  c = [clip(ring, boundary(other), method) for ring in rings(poly)]
+  r = filter(!isnothing, c)
+
+  if isempty(r)
+    nothing
+  elseif hasholes(poly)
+    PolyArea(r[1], r[2:end])
+  else
+    PolyArea(r[1])
   end
+end
 
 # ----------------
 # IMPLEMENTATIONS
