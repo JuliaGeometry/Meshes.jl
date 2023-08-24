dc5a651516e5076ce65583b0cb9dc76c1212db72
ClaroHenrique <claro.sales@reserva.ink>
2023-08-24 15:04:20 -0300

    Refact weileratherton.jl

--- src/clipping/weileratherton.jl
+++ src/clipping/weileratherton.jl
@@ -37,54 +37,57 @@ function clip(ring::Ring{Dim,T}, other::Ring{Dim,T}, ::WeilerAtherton) where {Di
   # TODO: Corner case with colinear segments with intersections
   # TODO: Poly point on boundary
   # TODO: PolyArea with holes
-  # TODO: Optimize _pointindex
 
-  vᵣ, kindᵣ = _pairwiseintersections(ring, other)
-  vₒ, kindₒ = _pairwiseintersections(other, ring)
-  nᵣ = length(vᵣ)
-  nₒ = length(vₒ)
+  I, pᵣ, pₒ = _pairwiseintersections(ring, other)
+  nᵣ = length(pᵣ)
+  nₒ = length(pₒ)
 
-  isentering = nothing
-  # set specific kinds of intersections to kindᵣ
-  for i in eachindex(vᵣ)
-    if kindᵣ[i] == :POINT
-      isentering = (sideof(vᵣ[i], other) == OUT)
-    else
-      kindᵣ[i] = isentering ? :INTERSECTION_ENTER : :INTERSECTION_EXIT
+  vᵣ = vertices(ring)
+  vₒ = vertices(other)
+
+  #TODO: not assume that the first point is not in poly boundary
+  isentering = (sideof(vᵣ[pᵣ[1][2]], other) != IN)
+
+  for i in eachindex(pᵣ)
+    if pᵣ[i][1] == :INTERSECTION
+      kind = isentering ? :INTERSECTION_ENTER : :INTERSECTION_EXIT
+      pᵣ[i] = (kind, pᵣ[i][2])
       isentering = !isentering
     end
   end
 
-  isvisitedᵣ = fill(false, length(vᵣ))
+  isvisitedᵣ = fill(false, length(pᵣ))
   u = Ring{Dim,T}[]
 
   for i in 1:nᵣ
-    if kindᵣ[i] == :INTERSECTION_ENTER && !isvisitedᵣ[i]
+    if pᵣ[i][1] == :INTERSECTION_ENTER && !isvisitedᵣ[i]
       vᵤ = Point{Dim,T}[]
       j = i
       while(true)
         isvisitedᵣ[j] = true
-        while(kindᵣ[j] != :INTERSECTION_EXIT)
-          push!(vᵤ, vᵣ[j])
+        push!(vᵤ, I[pᵣ[j][2]])
+        j = mod1(j+1, nᵣ)
+  
+        while(pᵣ[j][1] == :POINT)
+          push!(vᵤ, vᵣ[pᵣ[j][2]])
           j = mod1(j+1, nᵣ)
         end
 
-        push!(vᵤ, vᵣ[j])
-        j = _pointindex(vᵣ[j], vₒ)
+        push!(vᵤ, I[pᵣ[j][2]])
+        j = _pointindex(pᵣ[j][2], pₒ)
         j = mod1(j+1, nₒ)
-        
-        while(kindₒ[j] != :INTERSECTION)
-          push!(vᵤ, vₒ[j])
+
+        while(pₒ[j][1] == :POINT)
+          push!(vᵤ, vₒ[pₒ[j][2]])
           j = mod1(j+1, nₒ)
         end
 
-        if vₒ[j] ≈ vᵣ[i]
+        if I[pₒ[j][2]] == I[pᵣ[i][2]]
           break
         end
-
-        j = _pointindex(vₒ[j], vᵣ)
+        j = _pointindex(pₒ[j][2], pᵣ)
       end
-      push!(u, Ring(vᵤ))
+      push!(u, Ring(unique(vᵤ)))
     end
   end
   u
@@ -101,44 +104,54 @@ function _pairwiseintersections(ring::Ring{Dim,T}, other::Ring{Dim,T}) where {Di
   vₒ = vertices(other)
   nₒ = length(vₒ)
 
-  # vector of points and intersection
-  vᵢ = Point{Dim,T}[]
-  kind = Symbol[]
-
-  # build path to poly (points and intersections)  
+  I = []
+  vᵣI = [Int[] for i=1:nᵣ]
+  vₒI = [Int[] for i=1:nₒ]
   for i in 1:nᵣ
-    p₁, p₂ = vᵣ[i], vᵣ[i+1]
-    sᵣ = Segment(p₁, p₂)
-
-    # intersections from sᵣ
-    I = []
-
     for j in 1:nₒ
+      sᵣ = Segment(vᵣ[i], vᵣ[i+1])
       sₒ = Segment(vₒ[j], vₒ[j+1])
+      sI = sᵣ ∩ sₒ
 
-      if !isnothing(sᵣ ∩ sₒ)
-        push!(I, sᵣ ∩ sₒ)
+      if !isnothing(sI)
+        push!(I, sI)
+        id = length(I)
+        push!(vᵣI[i], id)
+        push!(vₒI[j], id)
       end
-    end
-
-    # sort intersections by distance from p₁
-    sort!(I, by = p -> measure(Segment(p₁, p)))
-
-    # add points and kind
-    push!(vᵢ, p₁)
-    push!(kind, :POINT)
-    for p in I 
-      push!(vᵢ, p)
-      push!(kind, :INTERSECTION)
     end
   end
 
-  vᵢ, kind
+  for i in 1:nᵣ
+    sort!(vᵣI[i], by = j -> measure(Segment(vᵣ[i], I[j])))
+  end
+
+  for i in 1:nₒ
+    sort!(vₒI[i], by = j -> measure(Segment(vₒ[i], I[j])))
+  end
+
+  pᵣ = Tuple{Symbol,Int}[]
+  for i in 1:nᵣ
+    push!(pᵣ, (:POINT, i))
+    for j in vᵣI[i]
+      push!(pᵣ, (:INTERSECTION, j))
+    end
+  end
+
+  pₒ = Tuple{Symbol,Int}[]
+  for i in 1:nₒ
+    push!(pₒ, (:POINT, i))
+    for j in vₒI[i]
+      push!(pₒ, (:INTERSECTION, j))
+    end
+  end
+
+  I, pᵣ, pₒ
 end
 
 function _pointindex(p, points)
   for i in eachindex(points)
-    if points[i] ≈ p
+    if points[i][1] != :POINT && points[i][2] == p
       return i
     end
   end
