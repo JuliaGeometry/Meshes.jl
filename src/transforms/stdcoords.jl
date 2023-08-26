0d6487b32b06f57419c7c47be3bc94783880dfa4
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-08-26 08:16:52 -0300

    Improve StdCoords docstring

--- src/transforms/stdcoords.jl
+++ src/transforms/stdcoords.jl
@@ -7,6 +7,16 @@
 
 Standardize coordinates of all geometries
 to the interval `[-0.5, 0.5]`.
+
+## Examples
+
+```julia
+julia> CartesianGrid(10, 10) |> StdCoords()
+10×10 CartesianGrid{2,Float64}
+  minimum: Point(-0.5, -0.5)
+  maximum: Point(0.5, 0.5)
+  spacing: (0.1, 0.1)
+```
 """
 struct StdCoords <: GeometricTransform end
 
