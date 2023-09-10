4961f826bd652aa8ec16b3fad9cc3bf0f03a0e41
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-09-10 12:19:30 -0300

    Add intersection method for polygons

---
+++ src/intersections/polygons.jl
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
+function intersection(f, poly₁::Polygon, poly₂::Polygon)
+  # TODO: use Weiler-Atherton or other more general clipping method
+  clipped = clip(poly₁, poly₂, SutherlandHodgman())
+  if isnothing(clipped)
+    @IT NotIntersecting nothing f
+  else
+    @IT Intersecting clipped f
+  end
+end
