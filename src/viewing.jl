2bc5bd55ba8262e1da6775bc84a083a577350367
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-11 16:56:50 -0300

    Add method to 'indices' function with 'Grid' and 'Polygon'

--- src/viewing.jl
+++ src/viewing.jl
@@ -44,6 +44,21 @@ that intersect with the `geometry`.
 """
 indices(domain::Domain, geometry::Geometry) = findall(intersects(geometry), domain)
 
+function indices(grid::Grid{2}, poly::Polygon{2})
+  dims = size(grid)
+  mask = zeros(Int, dims)
+  cpoly = clip(poly, boundingbox(grid), SutherlandHodgman())
+
+  for (n, triangle) in enumerate(simplexify(cpoly))
+    _fill!(mask, grid, triangle, n)
+  end
+
+  # convert to linear indices
+  LinearIndices(dims)[mask .> 0]
+end
+
+indices(domain::Domain, multi::Multi) = mapreduce(geom -> indices(domain, geom), vcat, collect(multi)) |> unique
+
 function indices(grid::CartesianGrid, box::Box)
   # grid properties
   or = minimum(grid)
@@ -62,4 +77,102 @@ function indices(grid::CartesianGrid, box::Box)
 
   # convert to linear indices
   LinearIndices(sz)[range] |> vec
+end
+
+# utils
+function _fill!(mask, grid, triangle, n)
+  v = vertices(triangle)
+
+  # fill edges of triangle
+  _bresenham!(mask, grid, n, v[1], v[2])
+  _bresenham!(mask, grid, n, v[2], v[3])
+  _bresenham!(mask, grid, n, v[3], v[1])
+
+  # fill interior of triangle
+  j1 = findfirst(==(n), mask).I[2]
+  j2 = findlast(==(n), mask).I[2]
+  for j in j1:j2
+    i1 = findfirst(==(n), @view(mask[:, j]))
+    i2 = findlast(==(n), @view(mask[:, j]))
+    mask[i1:i2, j] .= n
+  end
+end
+
+# Bresenham's line algorithm: https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+function _bresenham!(mask, grid, n, p₁, p₂)
+  o = minimum(grid)
+  s = spacing(grid)
+
+  # integer coordinates
+  x₁, y₁ = ceil.(Int, (p₁ - o) ./ s)
+  x₂, y₂ = ceil.(Int, (p₂ - o) ./ s)
+
+  # fix coordinates of points that are on the grid border
+  xmax, ymax = size(grid)
+  x₁ = clamp(x₁, 1, xmax)
+  y₁ = clamp(y₁, 1, ymax)
+  x₂ = clamp(x₂, 1, xmax)
+  y₂ = clamp(y₂, 1, ymax)
+
+  if abs(y₂ - y₁) < abs(x₂ - x₁)
+    if x₁ > x₂
+      _bresenhamlow!(mask, n, x₂, y₂, x₁, y₁)
+    else
+      _bresenhamlow!(mask, n, x₁, y₁, x₂, y₂)
+    end
+  else
+    if y₁ > y₂
+      _bresenhamhigh!(mask, n, x₂, y₂, x₁, y₁)
+    else
+      _bresenhamhigh!(mask, n, x₁, y₁, x₂, y₂)
+    end
+  end
+end
+
+function _bresenhamlow!(mask, n, x₁, y₁, x₂, y₂)
+  dx = x₂ - x₁
+  dy = y₂ - y₁
+  yi = 1
+  if dy < 0
+    yi = -1
+    dy = -dy
+  end
+
+  D = 2dy - dx
+  y = y₁
+
+  for x in x₁:x₂
+    mask[x, y] = n
+
+    if D > 0
+      y = y + yi
+      D = D + 2dy - 2dx
+    else
+      D = D + 2dy
+    end
+  end
+end
+
+function _bresenhamhigh!(mask, n, x₁, y₁, x₂, y₂)
+  dx = x₂ - x₁
+  dy = y₂ - y₁
+  xi = 1
+  if dx < 0
+    xi = -1
+    dx = -dx
+  end
+
+  D = 2dx - dy
+  x = x₁
+
+  for y in y₁:y₂
+    mask[x, y] = n
+
+    if D > 0
+      x = x + xi
+      D = D + 2dx - 2dy
+    else
+      D = D + 2dx
+    end
+  end
 end
--- test/views.jl
+++ test/views.jl
@@ -1,53 +1,51 @@
 @testset "Views" begin
-  @testset "Domain" begin
-    pset = PointSet(rand(P3, 100))
-    inds = rand(1:100, 3)
-    v = view(pset, inds)
-    @test nelements(v) == 3
-    for i in 1:3
-      p = pset[inds[i]]
-      @test v[i] == p
-      @test centroid(v, i) == p
-    end
-
-    grid = CartesianGrid{T}(10, 10)
-    inds = rand(1:100, 3)
-    v = view(grid, inds)
-    @test nelements(v) == 3
-    for i in 1:3
-      e = grid[inds[i]]
-      @test v[i] == e
-      @test centroid(v, i) == centroid(e)
-    end
-
-    points = P2[(0, 0), (1, 0), (0, 1), (1, 1), (0.5, 0.5)]
-    connec = connect.([(1, 2, 5), (2, 4, 5), (4, 3, 5), (3, 1, 5)], Triangle)
-    mesh = SimpleMesh(points, connec)
-    inds = rand(1:4, 3)
-    v = view(mesh, inds)
-    @test nelements(v) == 3
-    for i in 1:3
-      e = mesh[inds[i]]
-      @test v[i] == e
-      @test centroid(v, i) == centroid(e)
-    end
-
-    # view of view stores the correct domain
-    g = CartesianGrid{T}(10, 10)
-    v = view(view(g, 11:20), 1:3)
-    @test v isa DomainView{2,T,<:CartesianGrid}
-    @test v[1] == g[11]
-    @test v[2] == g[12]
-    @test v[3] == g[13]
-
-    # centroid of view of PointSet
-    points = P2[(0, 0), (1, 0), (0, 1), (1, 1), (0.5, 0.5)]
-    pview = view(PointSet(points), 1:4)
-    @test centroid(pview) == P2(0.5, 0.5)
-
-    # measure of view
-    g = CartesianGrid{T}(10, 10)
-    v = view(g, 1:3)
-    @test measure(v) ≈ T(3)
+  pset = PointSet(rand(P3, 100))
+  inds = rand(1:100, 3)
+  v = view(pset, inds)
+  @test nelements(v) == 3
+  for i in 1:3
+    p = pset[inds[i]]
+    @test v[i] == p
+    @test centroid(v, i) == p
   end
+
+  grid = CartesianGrid{T}(10, 10)
+  inds = rand(1:100, 3)
+  v = view(grid, inds)
+  @test nelements(v) == 3
+  for i in 1:3
+    e = grid[inds[i]]
+    @test v[i] == e
+    @test centroid(v, i) == centroid(e)
+  end
+
+  points = P2[(0, 0), (1, 0), (0, 1), (1, 1), (0.5, 0.5)]
+  connec = connect.([(1, 2, 5), (2, 4, 5), (4, 3, 5), (3, 1, 5)], Triangle)
+  mesh = SimpleMesh(points, connec)
+  inds = rand(1:4, 3)
+  v = view(mesh, inds)
+  @test nelements(v) == 3
+  for i in 1:3
+    e = mesh[inds[i]]
+    @test v[i] == e
+    @test centroid(v, i) == centroid(e)
+  end
+
+  # view of view stores the correct domain
+  g = CartesianGrid{T}(10, 10)
+  v = view(view(g, 11:20), 1:3)
+  @test v isa DomainView{2,T,<:CartesianGrid}
+  @test v[1] == g[11]
+  @test v[2] == g[12]
+  @test v[3] == g[13]
+
+  # centroid of view of PointSet
+  points = P2[(0, 0), (1, 0), (0, 1), (1, 1), (0.5, 0.5)]
+  pview = view(PointSet(points), 1:4)
+  @test centroid(pview) == P2(0.5, 0.5)
+
+  # measure of view
+  g = CartesianGrid{T}(10, 10)
+  v = view(g, 1:3)
+  @test measure(v) ≈ T(3)
 end
