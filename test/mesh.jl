db648ab7b13ada7f65549b1fb5aab1bc60567809
Elias Carvalho <eliascarvdev@gmail.com>
2023-10-03 16:31:48 -0300

    Add tests

--- test/mesh.jl
+++ test/mesh.jl
@@ -155,6 +155,16 @@
     @test vertex(grid, 1) == P2(0, 0)
     @test vertex(grid, 121) == P2(10, 10)
 
+    # units
+    Q = typeof(zero(T) * u"m")
+    grid = CartesianGrid{Q}(10, 10)
+    o = minimum(grid)
+    s = spacing(grid)
+    @test unit(coordtype(o)) == u"m"
+    @test Unitful.numtype(coordtype(o)) === T
+    @test unit(eltype(s)) == u"m"
+    @test Unitful.numtype(eltype(s)) === T
+
     grid = CartesianGrid{T}(200, 100)
     if T == Float32
       @test sprint(show, MIME"text/plain"(), grid) == """
@@ -172,7 +182,7 @@
   end
 
   @testset "RectilinearGrid" begin
-    x = range(zero(T), stop=oneunit(T), length=6)
+    x = range(zero(T), stop=one(T), length=6)
     y = T[0.0, 0.1, 0.3, 0.7, 0.9, 1.0]
     grid = RectilinearGrid(x, y)
     @test embeddim(grid) == 2
