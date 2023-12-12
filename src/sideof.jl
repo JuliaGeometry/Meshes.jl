fa61546f577de42386ae47ac8a278b195f76736d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-12-12 08:32:44 -0300

    Refactor sideof

--- src/sideof.jl
+++ src/sideof.jl
@@ -6,8 +6,8 @@
     IntersectionType
 
 The different types of sides that a point may lie in relation to a
-geometry or domain. Type `SideType` in a Julia session to see the
-full list.
+boundary geometry or mesh. Type `SideType` in a Julia session to see
+the full list.
 """
 @enum SideType begin
   IN
@@ -16,6 +16,18 @@ full list.
   LEFT
   RIGHT
 end
+
+"""
+    sideof(points, object)
+
+Determines on which side the `points` are in relation to the geometric
+`object`, which can be a boundary `geometry` or `mesh`.
+"""
+function sideof end
+
+# ---------
+# GEOMETRY
+# ---------
 
 """
     sideof(point, line)
@@ -27,36 +39,44 @@ Possible results are `LEFT`, `RIGHT` or `ON` the `line`.
 
 * Assumes the orientation of `Segment(line(0), line(1))`.
 """
-function sideof(p::Point{2,T}, l::Line{2,T}) where {T}
-  area = signarea(p, l(0), l(1))
-  ifelse(area > atol(T), LEFT, ifelse(area < -atol(T), RIGHT, ON))
+function sideof(point::Point{2,T}, line::Line{2,T}) where {T}
+  a = signarea(point, line(0), line(1))
+  ifelse(a > atol(T), LEFT, ifelse(a < -atol(T), RIGHT, ON))
 end
 
 """
     sideof(point, ring)
 
-Determines on which side of the `ring` the `point` lies.
+Determines on which side the `point` is in relation to the `ring`.
 Possible results are `IN` or `OUT` the `ring`.
 """
-function sideof(p::Point{2,T}, r::Ring{2,T}) where {T}
-  w = windingnumber(p, r)
+function sideof(point::Point{2,T}, ring::Ring{2,T}) where {T}
+  w = windingnumber(point, ring)
   ifelse(isapprox(w, zero(T), atol=atol(T)), OUT, IN)
 end
+
+# fallback for iterable of points
+sideof(points, geom::Geometry) = map(point -> sideof(point, geom), points)
+
+# -----
+# MESH
+# -----
 
 """
     sideof(point, mesh)
 
-Determines on which side of the surface `mesh` the `point` lies.
+Determines on which side the `point` is in relation to the surface `mesh`.
 Possible results are `IN`, `OUT`, or `ON` the `mesh`.
-
-### Notes
-
-* Uses a ray-casting algorithm.
 """
-function sideof(point::Point{3,T}, mesh::Mesh{3,T}) where {T}
-  @assert paramdim(mesh) == 2 "sideof only defined for surface meshes"
-  (eltype(mesh) <: Triangle) || return sideof(point, simplexify(mesh))
+sideof(point::Point{3}, mesh::Mesh{3}) = sideof((point,), mesh) |> first
 
+function sideof(points, mesh::Mesh{3})
+  @assert paramdim(mesh) == 2 "sideof only defined for surface meshes"
+  (eltype(mesh) <: Triangle) || return sideof(points, simplexify(mesh))
+  map(point -> _sideof(point, mesh), points)
+end
+
+function _sideof(point::Point{3,T}, mesh::Mesh{3,T}) where {T}
   z = last.(coordinates.(extrema(mesh)))
   r = Ray(point, Vec(zero(T), zero(T), 2 * (z[2] - z[1])))
 
