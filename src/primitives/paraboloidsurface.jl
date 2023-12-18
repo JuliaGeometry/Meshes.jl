b2d73c9ef53dda4125247958887857efd29fe8a2
Maurizio Tomasi <ziotom78@gmail.com>
2023-12-18 15:09:39 +0100

    Double \ in LaTeX commands

--- src/primitives/paraboloidsurface.jl
+++ src/primitives/paraboloidsurface.jl
@@ -11,7 +11,7 @@ and passes through `point` (the apex of the paraboloid). The equation
 of the paraboloid is the following:
 
 ```math
-f(x, y) = \frac{(x - x_0)^2 + (y - y_0)^2}{4f} + z_0\qquad\text{for } x^2 + y^2 < r^2,
+f(x, y) = \\frac{(x - x_0)^2 + (y - y_0)^2}{4f} + z_0\\qquad\\text{for } x^2 + y^2 < r^2,
 ```
 
 where ``(x_0, y_0, z_0)`` is the vertex of the parabola, ``f`` is the
