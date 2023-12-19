7da67d77ce9e74888bba8820c3aa92a3e2b12b42
Elias Carvalho <eliascarvdev@gmail.com>
2023-12-19 16:03:29 -0300

    Specialize 'Translate' transform to 'RectilinearGrid' and 'StructuredGrid'

--- src/transforms/translate.jl
+++ src/transforms/translate.jl
@@ -23,3 +23,12 @@ inverse(t::Translate) = Translate(-1 .* t.offsets)
 applycoord(t::Translate, v::Vec) = v
 
 applycoord(t::Translate, p::Point) = p + Vec(t.offsets)
+
+# specializations
+apply(t::Translate{Dim}, g::RectilinearGrid{Dim}) where {Dim} =
+  RectilinearGrid(ntuple(i -> xyz(g)[i] .+ t.offsets[i], Dim)), nothing
+
+apply(t::Translate{Dim}, g::StructuredGrid{Dim}) where {Dim} =
+  StructuredGrid(ntuple(i -> XYZ(g)[i] .+ t.offsets[i], Dim)), nothing
+
+revert(t::Translate, g::Union{RectilinearGrid,StructuredGrid}, c) = first(apply(inverse(t), g))
