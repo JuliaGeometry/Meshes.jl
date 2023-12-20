b582470b7ef317d5b8ffa7d3019698f5f8c00ae7
Elias Carvalho <eliascarvdev@gmail.com>
2023-12-20 17:56:05 -0300

    Apply suggestions

--- src/mesh/transformedmesh.jl
+++ src/mesh/transformedmesh.jl
@@ -18,6 +18,8 @@ TransformedMesh(m::TransformedMesh, t::Transform) = TransformedMesh(m.mesh, m.tr
 # alias to improve readability in IO methods
 const TransformedGrid{Dim,T} = TransformedMesh{Dim,T,GridTopology{Dim}}
 
+TransformedGrid(g::Grid, t::Transform) = TransformedMesh(g, t)
+
 topology(m::TransformedMesh) = topology(m.mesh)
 
 vertex(m::TransformedMesh, ind::Int) = m.transform(vertex(m.mesh, ind))
