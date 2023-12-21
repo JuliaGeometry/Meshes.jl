7f713823b9bb42880381c9d75155421c1dcff8bc
Elias Carvalho <eliascarvdev@gmail.com>
2023-12-21 17:36:29 -0300

    Apply suggestions

--- ext/fallbacks.jl
+++ ext/fallbacks.jl
@@ -2,11 +2,9 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-Makie.plottype(::AbstractVector{<:Geometry}) = Viz{<:Tuple{AbstractVector{<:Geometry}}}
 Makie.plottype(::Geometry) = Viz{<:Tuple{Geometry}}
 Makie.plottype(::Domain) = Viz{<:Tuple{Domain}}
 
-Makie.convert_arguments(::Type{<:Viz}, geoms::AbstractVector{<:Geometry}) = (GeometrySet(geoms),)
 Makie.convert_arguments(::Type{<:Viz}, geom::Geometry) = (GeometrySet([geom]),)
 Makie.convert_arguments(::Type{<:Viz}, domain::Domain) = (GeometrySet(collect(domain)),)
 Makie.convert_arguments(::Type{<:Viz}, mesh::Mesh) = (convert(SimpleMesh, mesh),)
@@ -16,3 +14,7 @@ Makie.convert_arguments(::Type{<:Viz}, gset::GeometrySet) = (gset,)
 Makie.convert_arguments(::Type{<:Viz}, mesh::SimpleMesh) = (mesh,)
 Makie.convert_arguments(::Type{<:Viz}, grid::CartesianGrid) = (grid,)
 Makie.convert_arguments(::Type{<:Viz}, grid::SubCartesianGrid) = (grid,)
+
+# vector of geometries for convenience
+Makie.plottype(::AbstractVector{<:Geometry}) = Viz{<:Tuple{AbstractVector{<:Geometry}}}
+Makie.convert_arguments(::Type{<:Viz}, geoms::AbstractVector{<:Geometry}) = (GeometrySet(geoms),)
