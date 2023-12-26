a26b24ab5359f7b46b99b75186731923996221f5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-12-26 16:27:51 -0300

    Add more references to winding docstring

--- src/winding.jl
+++ src/winding.jl
@@ -11,9 +11,10 @@ Generalized winding number of `points` with respect to the geometric `object`.
 
 * Barill et al. 2018. [Fast winding numbers for soups and clouds]
   (https://dl.acm.org/doi/10.1145/3197517.3201337)
-
 * Jacobson et al. 2013. [Robust inside-outside segmentation using generalized winding numbers]
   (https://dl.acm.org/doi/10.1145/2461912.2461916)
+* Oosterom, A. & Strackee, J. 1983. [The Solid Angle of a Plane Triangle]
+  (https://ieeexplore.ieee.org/document/4121581)
 """
 function winding end
 
