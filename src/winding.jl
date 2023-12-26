a2d0354de6a6f1ba2237cccdfb654a720362b793
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-12-26 11:59:23 -0300

    Add generalized winding number for surface mesh

--- src/winding.jl
+++ src/winding.jl
@@ -11,8 +11,15 @@ Generalized winding number of `points` with respect to the geometric `object`.
 
 * Barill et al. 2018. [Fast winding numbers for soups and clouds]
   (https://dl.acm.org/doi/10.1145/3197517.3201337)
+
+* Jacobson et al. 2013. [Robust inside-outside segmentation using generalized winding numbers]
+  (https://dl.acm.org/doi/10.1145/2461912.2461916)
 """
 function winding end
+
+# ---------
+# GEOMETRY
+# ---------
 
 function winding(p::Point{2,T}, r::Ring{2,T}) where {T}
   v = vertices(r)
@@ -20,5 +27,32 @@ function winding(p::Point{2,T}, r::Ring{2,T}) where {T}
   sum(∠(v[i], p, v[i + 1]) for i in 1:n) / T(2π)
 end
 
-# fallback with iterable of points
-winding(points, object) = [winding(point, object) for point in points]
+# fallback for iterable of points
+winding(points, geom::Geometry) = map(point -> winding(point, geom), points)
+
+# -----
+# MESH
+# -----
+
+winding(point::Point{3}, mesh::Mesh{3}) = winding((point,), mesh) |> first
+
+# Jacobson et al 2013.
+function winding(points, mesh::Mesh{3,T}) where {T}
+  @assert paramdim(mesh) == 2 "winding number only defined for surface meshes"
+  (eltype(mesh) <: Triangle) || return winding(points, simplexify(mesh))
+  map(points) do p
+    ∑ = sum(1:nelements(mesh)) do i
+      v = vertices(mesh[i])
+      a⃗ = v[1] - p
+      b⃗ = v[2] - p
+      c⃗ = v[3] - p
+      a = norm(a⃗)
+      b = norm(b⃗)
+      c = norm(c⃗)
+      n = det([a⃗ b⃗ c⃗])
+      d = a * b * c + (a⃗ ⋅ b⃗) * c + (b⃗ ⋅ c⃗) * a + (c⃗ ⋅ a⃗) * b
+      2atan(n, d)
+    end
+    ∑ / T(4π)
+  end
+end
--- test/winding.jl
+++ test/winding.jl
@@ -10,4 +10,9 @@
   @test winding(p, c) ≈ T(2)
   @test winding(p, reverse(c)) ≈ T(-2)
   @test winding([p, p], c) ≈ T[2, 2]
+
+  m = boundary(Box(P3(0, 0, 0), P3(2, 2, 2)))
+  @test all(>(0), winding(vertices(m), m))
+  @test isapprox(winding(P3(1, 1, 1), m), T(1), atol = atol(T))
+  @test isapprox(winding(P3(3, 3, 3), m), T(0), atol = atol(T))
 end
