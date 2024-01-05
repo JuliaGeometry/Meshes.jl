ad3ba9c1df81d7ae005310427dbfdccb2bae4c56
Elias Carvalho <eliascarvdev@gmail.com>
2024-01-05 12:10:10 -0300

    Add more tests

--- test/sideof.jl
+++ test/sideof.jl
@@ -4,18 +4,24 @@
   @test sideof(p1, l) == LEFT
   @test sideof(p2, l) == RIGHT
   @test sideof(p3, l) == ON
+  pts = [p1, p2, p3]
+  @test sideof(pts, l) == [LEFT, RIGHT, ON]
 
   p1, p2, p3 = P2(0.5, 0.5), P2(1.5, 0.5), P2(1, 1)
   c = Ring(P2[(0, 0), (1, 0), (1, 1), (0, 1)])
   @test sideof(p1, c) == IN
   @test sideof(p2, c) == OUT
   @test sideof(p3, c) == IN
+  pts = [p1, p2, p3]
+  @test sideof(pts, c) == [IN, OUT, IN]
 
   points = P3[(0, 0, 0), (1, 0, 0), (0, 1, 0), (0.25, 0.25, 1)]
   connec = connect.([(1, 3, 2), (1, 2, 4), (1, 4, 3), (2, 3, 4)], Triangle)
   mesh = SimpleMesh(points, connec)
   @test sideof(P3(0.25, 0.25, 0.1), mesh) == IN
   @test sideof(P3(0.25, 0.25, -0.1), mesh) == OUT
+  pts = P3[(0.25, 0.25, 0.1), (0.25, 0.25, -0.1)]
+  @test sideof(pts, mesh) == [IN, OUT]
 
   # ray goes through vertex
   @test sideof(P3(0.25, 0.25, 0.1), mesh) == IN
