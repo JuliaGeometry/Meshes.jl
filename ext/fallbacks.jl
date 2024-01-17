b5b77f92f032517458235697398f11bf6041a785
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-01-17 14:38:36 -0300

    Add recipe for `Grid` (#689)
    * Add recipe for 'Grid'
    
    * Apply suggestions from code review
    
    * Apply suggestions
    
    * Format code
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- ext/fallbacks.jl
+++ ext/fallbacks.jl
@@ -12,9 +12,8 @@ Makie.convert_arguments(::Type{<:Viz}, mesh::Mesh) = (convert(SimpleMesh, mesh),
 # skip conversion for these types
 Makie.convert_arguments(::Type{<:Viz}, gset::GeometrySet) = (gset,)
 Makie.convert_arguments(::Type{<:Viz}, mesh::SimpleMesh) = (mesh,)
-Makie.convert_arguments(::Type{<:Viz}, grid::CartesianGrid) = (grid,)
+Makie.convert_arguments(::Type{<:Viz}, grid::Grid) = (grid,)
 Makie.convert_arguments(::Type{<:Viz}, grid::SubCartesianGrid) = (grid,)
-Makie.convert_arguments(::Type{<:Viz}, grid::RectilinearGrid) = (grid,)
 
 # vector of geometries for convenience
 Makie.plottype(::AbstractVector{<:Geometry}) = Viz{<:Tuple{AbstractVector{<:Geometry}}}
---
+++ ext/grid.jl
@@ -0,0 +1,174 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
+function Makie.plot!(plot::Viz{<:Tuple{Grid}})
+  grid = plot[:object][]
+  T = typeof(grid)
+  Dim = embeddim(grid)
+  if Dim == 1
+    vizgrid1D!(T, plot)
+  elseif Dim == 2
+    vizgrid2D!(T, plot)
+  elseif Dim == 3
+    vizgrid3D!(T, plot)
+  end
+end
+
+vizgrid1D!(::Type{<:Grid}, plot) = vizmesh1D!(plot)
+
+vizgrid2D!(::Type{<:Grid}, plot) = vizmesh2D!(plot)
+
+function vizgrid2D!(::Type{<:Union{CartesianGrid,RectilinearGrid}}, plot)
+  grid = plot[:object]
+  color = plot[:color]
+  alpha = plot[:alpha]
+  colorscheme = plot[:colorscheme]
+  segmentsize = plot[:segmentsize]
+  showfacets = plot[:showfacets]
+  facetcolor = plot[:facetcolor]
+
+  # process color spec into colorant
+  colorant = Makie.@lift process($color, $colorscheme, $alpha)
+
+  # number of vertices and colors
+  nv = Makie.@lift nvertices($grid)
+  nc = Makie.@lift $colorant isa AbstractVector ? length($colorant) : 1
+
+  # grid coordinates
+  xyz = Makie.@lift Meshes.xyz($grid)
+  xs = Makie.@lift $xyz[1]
+  ys = Makie.@lift $xyz[2]
+
+  if nc[] == 1
+    # visualize bounding box with a single
+    # color for maximum performance
+    bbox = Makie.@lift boundingbox($grid)
+    viz!(plot, bbox, color=colorant)
+  else
+    if nc[] == nv[]
+      # visualize as a simple mesh so that
+      # colors can be specified at vertices
+      vizmesh2D!(plot)
+    else
+      # visualize as built-in heatmap
+      sz = Makie.@lift size($grid)
+      C = Makie.@lift reshape($colorant, $sz)
+      Makie.heatmap!(plot, xs, ys, C)
+    end
+  end
+
+  if showfacets[]
+    tup = Makie.@lift xysegments($xs, $ys)
+    x, y = Makie.@lift($tup[1]), Makie.@lift($tup[2])
+    Makie.lines!(plot, x, y, color=facetcolor, linewidth=segmentsize)
+  end
+end
+
+function vizgrid3D!(::Type{<:Grid}, plot)
+  grid = plot[:object]
+  color = plot[:color]
+
+  # number of vertices and colors
+  nv = Makie.@lift nvertices($grid)
+  nc = Makie.@lift $color isa AbstractVector ? length($color) : 1
+
+  if nv[] == nc[]
+    error("not implemented")
+  else
+    vizmesh3D!(plot)
+  end
+end
+
+function vizgrid3D!(::Type{<:CartesianGrid}, plot)
+  # retrieve parameters
+  grid = plot[:object]
+  color = plot[:color]
+  alpha = plot[:alpha]
+  colorscheme = plot[:colorscheme]
+  segmentsize = plot[:segmentsize]
+  showfacets = plot[:showfacets]
+  facetcolor = plot[:facetcolor]
+
+  # process color spec into colorant
+  colorant = Makie.@lift process($color, $colorscheme, $alpha)
+
+  # number of vertices and colors
+  nv = Makie.@lift nvertices($grid)
+  nc = Makie.@lift $colorant isa AbstractVector ? length($colorant) : 1
+
+  # origin, spacing and size of grid
+  or = Makie.@lift coordinates(minimum($grid))
+  sp = Makie.@lift spacing($grid)
+  sz = Makie.@lift size($grid)
+
+  # coordinates of centroids
+  xyz = [Makie.@lift(range($or[i] + $sp[i] / 2, step=$sp[i], length=$sz[i])) for i in 1:3]
+
+  if nc[] == 1
+    # visualize bounding box with a single
+    # color for maximum performance
+    bbox = Makie.@lift boundingbox($grid)
+    viz!(plot, bbox, color=colorant)
+  else
+    if nc[] == nv[]
+      error("not implemented")
+    else
+      # visualize as built-in meshscatter
+      xs = Makie.@lift $(xyz[1]) .+ $sp[1] / 2
+      ys = Makie.@lift $(xyz[2]) .+ $sp[2] / 2
+      zs = Makie.@lift $(xyz[3]) .+ $sp[3] / 2
+      coords = Makie.@lift [(x, y, z) for z in $zs for y in $ys for x in $xs]
+      rect = Makie.@lift Makie.Rect3(-1 .* $sp, $sp)
+      Makie.meshscatter!(plot, coords, marker=rect, markersize=1, color=colorant)
+    end
+  end
+
+  if showfacets[]
+    tup = Makie.@lift xyzsegments(Meshes.xyz($grid)...)
+    x, y, z = Makie.@lift($tup[1]), Makie.@lift($tup[2]), Makie.@lift($tup[3])
+    Makie.lines!(plot, x, y, z, color=facetcolor, linewidth=segmentsize)
+  end
+end
+
+# helper functions to create a minimum number
+# of line segments within Cartesian/Rectilinear grid
+function xysegments(xs, ys)
+  coords = []
+  for x in xs
+    push!(coords, (x, first(ys)))
+    push!(coords, (x, last(ys)))
+    push!(coords, (NaN, NaN))
+  end
+  for y in ys
+    push!(coords, (first(xs), y))
+    push!(coords, (last(xs), y))
+    push!(coords, (NaN, NaN))
+  end
+  x = getindex.(coords, 1)
+  y = getindex.(coords, 2)
+  x, y
+end
+
+function xyzsegments(xs, ys, zs)
+  coords = []
+  for y in ys, z in zs
+    push!(coords, (first(xs), y, z))
+    push!(coords, (last(xs), y, z))
+    push!(coords, (NaN, NaN, NaN))
+  end
+  for x in xs, z in zs
+    push!(coords, (x, first(ys), z))
+    push!(coords, (x, last(ys), z))
+    push!(coords, (NaN, NaN, NaN))
+  end
+  for x in xs, y in ys
+    push!(coords, (x, y, first(zs)))
+    push!(coords, (x, y, last(zs)))
+    push!(coords, (NaN, NaN, NaN))
+  end
+  x = getindex.(coords, 1)
+  y = getindex.(coords, 2)
+  z = getindex.(coords, 3)
+  x, y, z
+end
