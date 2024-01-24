451383672f80017e2b3ee9b09875274d0ed00434
Elias Carvalho <eliascarvdev@gmail.com>
2024-01-24 11:55:11 -0300

    Refactor 'TransformedGrid' recipe

--- ext/grid/transformed.jl
+++ ext/grid/transformed.jl
@@ -2,47 +2,40 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-const RotatedGrid{Dim,T} = TransformedGrid{Dim,T,G,TR} where {G,TR<:Rotate{<:Angle2d}}
+isimplemented(::TB.Transform) = false
+isimplemented(::Rotate{<:Angle2d}) = true
+isimplemented(::Translate{2}) = true
+isimplemented(::Translate{3}) = true
 
-function vizgrid2D!(plot::Viz{<:Tuple{RotatedGrid}})
+vizgrid2D!(plot::Viz{<:Tuple{TransformedGrid}}) = transformedgrid!(plot, vizmesh2D!)
+
+vizgrid3D!(plot::Viz{<:Tuple{TransformedGrid}}) = transformedgrid!(plot, vizmesh3D!)
+
+function transformedgrid!(plot, fallback)
   tgrid = plot[:object]
-  color = plot[:color]
-  alpha = plot[:alpha]
-  colorscheme = plot[:colorscheme]
-  segmentsize = plot[:segmentsize]
-  showfacets = plot[:showfacets]
-  facetcolor = plot[:facetcolor]
-
   grid = Makie.@lift parent($tgrid)
   trans = Makie.@lift Meshes.transform($tgrid)
-  rot = Makie.@lift first(TB.parameters($trans))
-  θ = Makie.@lift first(Rotations.params($rot))
-
-  # plot and rotate the grid
-  viz!(plot, grid; color, alpha, colorscheme, segmentsize, showfacets, facetcolor)
-  Makie.rotate!(plot, θ[])
+  if isaffine(trans[]) && isimplemented(trans[])
+    color = plot[:color]
+    alpha = plot[:alpha]
+    colorscheme = plot[:colorscheme]
+    segmentsize = plot[:segmentsize]
+    showfacets = plot[:showfacets]
+    facetcolor = plot[:facetcolor]
+    viz!(plot, grid; color, alpha, colorscheme, segmentsize, showfacets, facetcolor)
+    makietransform!(plot, trans[])
+  else
+    fallback(tgrid)
+  end
 end
 
-const TranslatedGrid{Dim,T} = TransformedGrid{Dim,T,G,TR} where {G,TR<:Translate{Dim}}
+function makietransform!(plot, trans::Rotate{<:Angle2d})
+  rot = first(TB.parameters(trans))
+  θ = first(Rotations.params(rot))
+  Makie.rotate!(plot, θ)
+end
 
-vizgrid2D!(plot::Viz{<:Tuple{TranslatedGrid}}) = translatedgrid!(plot)
-
-vizgrid3D!(plot::Viz{<:Tuple{TranslatedGrid}}) = translatedgrid!(plot)
-
-function translatedgrid!(plot)
-  tgrid = plot[:object]
-  color = plot[:color]
-  alpha = plot[:alpha]
-  colorscheme = plot[:colorscheme]
-  segmentsize = plot[:segmentsize]
-  showfacets = plot[:showfacets]
-  facetcolor = plot[:facetcolor]
-
-  grid = Makie.@lift parent($tgrid)
-  trans = Makie.@lift Meshes.transform($tgrid)
-  offsets = Makie.@lift first(TB.parameters($trans))
-
-  # plot and translate the grid
-  viz!(plot, grid; color, alpha, colorscheme, segmentsize, showfacets, facetcolor)
-  Makie.translate!(plot, offsets[]...)
+function makietransform!(plot, trans::Translate)
+  offsets = first(TB.parameters(trans))
+  Makie.translate!(plot, offsets...)
 end
