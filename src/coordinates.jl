28ada1bb58b3cf393a3d086a35610bfbe9faf2ba
Elias Carvalho <eliascarvdev@gmail.com>
2024-01-29 17:49:42 -0300

    Use parametric dimension

--- src/coordinates.jl
+++ src/coordinates.jl
@@ -2,23 +2,23 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-abstract type Coordinates{Dim,T} end
+abstract type Coordinates{N,T} end
 
-struct Cartesian{Dim,T} <: Coordinates{Dim,T}
-  coords::NTuple{Dim,T}
-  Cartesian{Dim,T}(coords) where {Dim,T} = new{Dim,float(T)}(coords)
+struct Cartesian{N,T} <: Coordinates{N,T}
+  coords::NTuple{N,T}
+  Cartesian{N,T}(coords) where {N,T} = new{N,float(T)}(coords)
 end
 
-Cartesian(coords::NTuple{Dim,T}) where {Dim,T} = Coordinates{Dim,T}(coords)
+Cartesian(coords::NTuple{N,T}) where {N,T} = Coordinates{N,T}(coords)
 Cartesian(coords::Tuple) = Coordinates(promote(coords...))
 Cartesian(coords...) = Cartesian(coords)
 
-struct LatLon{T<:Quantity} <: Coordinates{3,T}
+struct LatLon{T<:Quantity} <: Coordinates{2,T}
   lat::T
   lon::T
   function LatLon{T}(lat, lon) where {T<:Quantity}
     if unit(T) ≠ u"°"
-      throw(ArgumentError("`lat` and `lon` units must be degrees"))
+      throw(ArgumentError("the units of `lat` and `lon` must be degrees"))
     end
     new{float(T)}(lat, lon)
   end
@@ -34,10 +34,10 @@ struct LatLonAlt{T<:Quantity,A<:Quantity} <: Coordinates{3,T}
   alt::A
   function LatLonAlt{T,A}(lat, lon, alt) where {T<:Quantity,A<:Quantity}
     if unit(T) ≠ u"°"
-      throw(ArgumentError("`lat` and `lon` units must be degrees"))
+      throw(ArgumentError("the units of `lat` and `lon` must be degrees"))
     end
-    if dimension(A) ≠ u"𝐋"
-      throw(ArgumentError("`alt` unit must be a length unit"))
+    if Nension(A) ≠ u"𝐋"
+      throw(ArgumentError("the unit of `alt` must be a length unit"))
     end
     new{float(T),float(A)}(lat, lon, alt)
   end
@@ -47,12 +47,12 @@ LatLonAlt(lat::T, lon::T, alt::A) where {T<:Quantity,A<:Quantity} = LatLonAlt{T,
 LatLonAlt(lat::Quantity, lon::Quantity, alt::Quantity) = LatLonAlt(promote(lat, lon)..., alt)
 LatLonAlt(lat, lon, alt) = LatLonAlt(lat * u"°", lon * u"°", alt * u"m")
 
-struct EastNorth{T<:Quantity} <: Coordinates{3,T}
+struct EastNorth{T<:Quantity} <: Coordinates{2,T}
   east::T
   north::T
   function EastNorth{T}(east, north) where {T<:Quantity}
-    if dimension(T) ≠ u"𝐋"
-      throw(ArgumentError("`east` and `north` units must be length units"))
+    if Nension(T) ≠ u"𝐋"
+      throw(ArgumentError("the units of `east` and `north` must be length units"))
     end
     new{float(T)}(east, north)
   end
@@ -62,12 +62,12 @@ EastNorth(east::T, north::T) where {T<:Quantity} = EastNorth{T}(east, north)
 EastNorth(east::Quantity, north::Quantity) = EastNorth(promote(east, north)...)
 EastNorth(east, north) = EastNorth(east * u"m", north * u"m")
 
-struct WebMercator{T<:Quantity} <: Coordinates{3,T}
+struct WebMercator{T<:Quantity} <: Coordinates{2,T}
   x::T
   y::T
   function WebMercator{T}(x, y) where {T<:Quantity}
-    if dimension(T) ≠ u"𝐋"
-      throw(ArgumentError("`x` and `y` units must be length units"))
+    if Nension(T) ≠ u"𝐋"
+      throw(ArgumentError("the units of `x` and `y` must be length units"))
     end
     new{float(T)}(x, y)
   end
