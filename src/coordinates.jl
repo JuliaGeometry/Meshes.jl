2810edb3dcd8484c2fd03e955e3bc3f4516b9086
Elias Carvalho <eliascarvdev@gmail.com>
2024-01-30 15:12:42 -0300

    Add basic conversions

--- src/coordinates.jl
+++ src/coordinates.jl
@@ -32,3 +32,28 @@ end
 
 include("coordinates/basic.jl")
 include("coordinates/gis.jl")
+
+# ------------
+# CONVERSIONS
+# ------------
+
+# Cartesian <--> Polar
+Base.convert(::Type{<:Cartesian}, (; ρ, ϕ)::Polar) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ))
+function Base.convert(::Type{<:Polar}, (; coords)::Cartesian{2})
+  x, y = coords
+  Polar(sqrt(x^2 + y^2), atan(y / x))
+end
+
+# Cartesian <--> Cylindrical
+Base.convert(::Type{<:Cartesian}, (; ρ, ϕ, z)::Cylindrical) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ), z)
+function Base.convert(::Type{<:Cylindrical}, (; coords)::Cartesian{3})
+  x, y, z = coords
+  Cylindrical(sqrt(x^2 + y^2), atan(y / x), z)
+end
+
+# Cartesian <--> Spherical
+Base.convert(::Type{<:Cartesian}, (; r, θ, ϕ)::Spherical) = Cartesian(r * sin(θ) * cos(ϕ), r * sin(θ) * sin(ϕ), r * cos(θ))
+function Base.convert(::Type{<:Spherical}, (; coords)::Cartesian{3})
+  x, y, z = coords
+  Spherical(sqrt(x^2 + y^2 + z^2), atan(sqrt(x^2 + y^2) / z), atan(y / x))
+end
