7d73d98fb659eb5a4722aa739c68764ad16aee88
Elias Carvalho <eliascarvdev@gmail.com>
2024-01-30 15:29:49 -0300

    Apply suggestions

--- src/coordinates.jl
+++ src/coordinates.jl
@@ -41,19 +41,19 @@ include("coordinates/gis.jl")
 Base.convert(::Type{<:Cartesian}, (; ρ, ϕ)::Polar) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ))
 function Base.convert(::Type{<:Polar}, (; coords)::Cartesian{2})
   x, y = coords
-  Polar(sqrt(x^2 + y^2), atan(y / x))
+  Polar(sqrt(x^2 + y^2), atan(y, x))
 end
 
 # Cartesian <--> Cylindrical
 Base.convert(::Type{<:Cartesian}, (; ρ, ϕ, z)::Cylindrical) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ), z)
 function Base.convert(::Type{<:Cylindrical}, (; coords)::Cartesian{3})
   x, y, z = coords
-  Cylindrical(sqrt(x^2 + y^2), atan(y / x), z)
+  Cylindrical(sqrt(x^2 + y^2), atan(y, x), z)
 end
 
 # Cartesian <--> Spherical
 Base.convert(::Type{<:Cartesian}, (; r, θ, ϕ)::Spherical) = Cartesian(r * sin(θ) * cos(ϕ), r * sin(θ) * sin(ϕ), r * cos(θ))
 function Base.convert(::Type{<:Spherical}, (; coords)::Cartesian{3})
   x, y, z = coords
-  Spherical(sqrt(x^2 + y^2 + z^2), atan(sqrt(x^2 + y^2) / z), atan(y / x))
+  Spherical(sqrt(x^2 + y^2 + z^2), atan(sqrt(x^2 + y^2), z), atan(y, x))
 end
