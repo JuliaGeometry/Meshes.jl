bc45a641c7f07c2726b5030c455cd9508a96d98c
Elias Carvalho <eliascarvdev@gmail.com>
2024-02-05 11:24:36 -0300

    Apply suggestions

--- src/coordinates/gis.jl
+++ src/coordinates/gis.jl
@@ -6,7 +6,7 @@ struct EPSG{Code,N,Coords} <: Coordinates{N}
   coords::Coords
 end
 
-EPSG{Code,N,Coords}(; kwargs...) where {Code,N,Coords} = EPSG{Code,N,Coords}(values(kwargs))
+EPSG{Code,N,Coords}(args...) where {Code,N,Coords} = EPSG{Code,N,Coords}(Coords(args))
 
 Base.isapprox(c₁::T, c₂::T; kwargs...) where {T<:EPSG} =
   all(isapprox(x₁, x₂; kwargs...) for (x₁, x₂) in zip(c₁.coords, c₂.coords))
@@ -38,14 +38,11 @@ LatLon((π/4)u"rad", (π/4)u"rad") # radians are converted to degrees
 LatLon(45.0u"°", 45.0u"°")
 ```
 
-## References
-
-* [Geographic coordinate system](https://en.wikipedia.org/wiki/Geographic_coordinate_system)
-* [ISO 6709:2022](https://www.iso.org/standard/75147.html)
+See [EPSG:4326](epsg.io/4326).
 """
 const LatLon{D<:Deg} = EPSG{4326,2,@NamedTuple{lat::D, lon::D}}
 
-LatLon(lat::D, lon::D) where {D<:Deg} = LatLon{float(D)}(; lat, lon)
+LatLon(lat::D, lon::D) where {D<:Deg} = LatLon{float(D)}(lat, lon)
 LatLon(lat::Deg, lon::Deg) = LatLon(promote(lat, lon)...)
 LatLon(lat::Rad, lon::Rad) = LatLon(rad2deg(lat), rad2deg(lon))
 LatLon(lat::Number, lon::Number) = LatLon(addunit(lat, u"°"), addunit(lon, u"°"))
@@ -63,13 +60,11 @@ Mercator(1u"m", 1u"m") # integers are converted converted to floats
 Mercator(1.0u"km", 1.0u"km")
 ```
 
-## References
-
-* [Mercator projection](https://en.wikipedia.org/wiki/Mercator_projection)
+See [EPSG:3395](epsg.io/3395).
 """
 const Mercator{L<:Len} = EPSG{3395,2,@NamedTuple{x::L, y::L}}
 
-Mercator(x::L, y::L) where {L<:Len} = Mercator{float(L)}(; x, y)
+Mercator(x::L, y::L) where {L<:Len} = Mercator{float(L)}(x, y)
 Mercator(x::Len, y::Len) = Mercator(promote(x, y)...)
 Mercator(x::Number, y::Number) = Mercator(addunit(x, u"m"), addunit(y, u"m"))
 
@@ -86,12 +81,10 @@ WebMercator(1u"m", 1u"m") # integers are converted converted to floats
 WebMercator(1.0u"km", 1.0u"km")
 ```
 
-## References
-
-* [Web Mercator projection](https://en.wikipedia.org/wiki/Web_Mercator_projection)
+See [EPSG:3857](epsg.io/3857).
 """
 const WebMercator{L<:Len} = EPSG{3857,2,@NamedTuple{x::L, y::L}}
 
-WebMercator(x::L, y::L) where {L<:Len} = WebMercator{float(L)}(; x, y)
+WebMercator(x::L, y::L) where {L<:Len} = WebMercator{float(L)}(x, y)
 WebMercator(x::Len, y::Len) = WebMercator(promote(x, y)...)
 WebMercator(x::Number, y::Number) = WebMercator(addunit(x, u"m"), addunit(y, u"m"))
