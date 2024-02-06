a5a027cfe3f5a4ca8a89b05a9c519b5b96d8f4ab
Elias Carvalho <eliascarvdev@gmail.com>
2024-02-06 16:13:18 -0300

    Readjust code

--- src/coordinates.jl
+++ src/coordinates.jl
@@ -2,6 +2,42 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
+# ----------
+# UTILITIES
+# ----------
+
+const Len{T} = Quantity{T,u"𝐋"}
+const Rad{T} = Quantity{T,NoDims,typeof(u"rad")}
+const Deg{T} = Quantity{T,NoDims,typeof(u"°")}
+
+# only add the unit if the argument is not a quantity
+addunit(x::Number, u) = x * u
+addunit(x::Quantity, u) = throw(ArgumentError("invalid units for coordinates, please check the documentation"))
+
+# adjust negative angles
+function atanpos(y, x)
+  α = atan(y, x)
+  ifelse(α ≥ zero(α), α, α + oftype(α, 2π))
+end
+
+# -----------
+# ELLIPSOIDS
+# -----------
+
+const wgs84 = let
+  a = 6378137.0 * u"m"
+  f⁻¹ = 298.257223563
+  f = inv(f⁻¹)
+  b = a * (1 - f)
+  e² = (2 - f) / f⁻¹
+  e = √e²
+  (; a, b, e, e², f, f⁻¹)
+end
+
+# ------------
+# COORDINATES
+# ------------
+
 """
     Coordinates{N}
 
@@ -33,8 +69,63 @@ end
 # IMPLEMENTATIONS
 # ----------------
 
-include("coordinates/utils.jl")
 include("coordinates/basic.jl")
 include("coordinates/gis.jl")
-include("coordinates/ellipsoids.jl")
-include("coordinates/conversions.jl")
+
+# ------------
+# CONVERSIONS
+# ------------
+
+# Cartesian <-> Polar
+Base.convert(::Type{Cartesian}, (; ρ, ϕ)::Polar) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ))
+function Base.convert(::Type{Polar}, (; coords)::Cartesian{2})
+  x, y = coords
+  Polar(sqrt(x^2 + y^2), atanpos(y, x) * u"rad")
+end
+
+# Cartesian <-> Cylindrical
+Base.convert(::Type{Cartesian}, (; ρ, ϕ, z)::Cylindrical) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ), z)
+function Base.convert(::Type{Cylindrical}, (; coords)::Cartesian{3})
+  x, y, z = coords
+  Cylindrical(sqrt(x^2 + y^2), atanpos(y, x) * u"rad", z)
+end
+
+# Cartesian <-> Spherical
+Base.convert(::Type{Cartesian}, (; r, θ, ϕ)::Spherical) =
+  Cartesian(r * sin(θ) * cos(ϕ), r * sin(θ) * sin(ϕ), r * cos(θ))
+function Base.convert(::Type{Spherical}, (; coords)::Cartesian{3})
+  x, y, z = coords
+  Spherical(sqrt(x^2 + y^2 + z^2), atan(sqrt(x^2 + y^2), z) * u"rad", atanpos(y, x) * u"rad")
+end
+
+# LatLon <-> Mercator
+function Base.convert(::Type{Mercator}, (; coords)::LatLon)
+  λ = deg2rad(coords.lon)
+  ϕ = deg2rad(coords.lat)
+  l = ustrip(λ)
+  a = oftype(l, ustrip(wgs84.a))
+  e = oftype(l, wgs84.e)
+  x = a * l
+  y = a * (asinh(tan(ϕ)) - e * atanh(e * sin(ϕ)))
+  Mercator(x * u"m", y * u"m")
+end
+
+# LatLon <-> WebMercator
+function Base.convert(::Type{WebMercator}, (; coords)::LatLon)
+  λ = deg2rad(coords.lon)
+  ϕ = deg2rad(coords.lat)
+  l = ustrip(λ)
+  a = oftype(l, ustrip(wgs84.a))
+  x = a * l
+  y = a * asinh(tan(ϕ))
+  WebMercator(x * u"m", y * u"m")
+end
+
+function Base.convert(::Type{LatLon}, (; coords)::WebMercator)
+  x = coords.x
+  y = coords.y
+  a = oftype(x, wgs84.a)
+  λ = x / a
+  ϕ = atan(sinh(y / a))
+  LatLon(rad2deg(ϕ) * u"°", rad2deg(λ) * u"°")
+end
