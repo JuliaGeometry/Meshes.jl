104549f38a1573e19f8d5f94ddd590d73a84cefd
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-02-06 16:27:11 -0300

    Add GIS Conversions (#732)
    * [WIP] Add GIS Conversions
    
    * Apply suggestions
    
    * Add tests
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Apply suggestions
    
    * Move helper function to 'coordinates/utils.jl'
    
    * Readjust code

--- src/coordinates.jl
+++ src/coordinates.jl
@@ -2,6 +2,42 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
+# ----------
+# UTILITIES
+# ----------
+
+const Len{T} = Quantity{T,u"𝐋"}
+const Rad{T} = Quantity{T,NoDims,typeof(u"rad")}
+const Deg{T} = Quantity{T,NoDims,typeof(u"°")}
+
+# only add the unit if the argument is not a quantity
+addunit(x::Number, u) = x * u
+addunit(x::Quantity, u) = throw(ArgumentError("invalid units for coordinates, please check the documentation"))
+
+# adjust negative angles
+function atanpos(y, x)
+  α = atan(y, x)
+  ifelse(α ≥ zero(α), α, α + oftype(α, 2π))
+end
+
+# -----------
+# ELLIPSOIDS
+# -----------
+
+const wgs84 = let
+  a = 6378137.0 * u"m"
+  f⁻¹ = 298.257223563
+  f = inv(f⁻¹)
+  b = a * (1 - f)
+  e² = (2 - f) / f⁻¹
+  e = √e²
+  (; a, b, e, e², f, f⁻¹)
+end
+
+# ------------
+# COORDINATES
+# ------------
+
 """
     Coordinates{N}
 
@@ -29,18 +65,6 @@ function Base.show(io::IO, ::MIME"text/plain", coords::Coordinates)
   printfields(io, coords)
 end
 
-# ----------
-# UTILITIES
-# ----------
-
-const Len{T} = Quantity{T,u"𝐋"}
-const Rad{T} = Quantity{T,NoDims,typeof(u"rad")}
-const Deg{T} = Quantity{T,NoDims,typeof(u"°")}
-
-# only add the unit if the argument is not a quantity
-addunit(x::Number, u) = x * u
-addunit(x::Quantity, u) = throw(ArgumentError("invalid units for coordinates, please check the documentation"))
-
 # ----------------
 # IMPLEMENTATIONS
 # ----------------
@@ -53,29 +77,55 @@ include("coordinates/gis.jl")
 # ------------
 
 # Cartesian <-> Polar
-Base.convert(::Type{<:Cartesian}, (; ρ, ϕ)::Polar) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ))
-function Base.convert(::Type{<:Polar}, (; coords)::Cartesian{2})
+Base.convert(::Type{Cartesian}, (; ρ, ϕ)::Polar) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ))
+function Base.convert(::Type{Polar}, (; coords)::Cartesian{2})
   x, y = coords
   Polar(sqrt(x^2 + y^2), atanpos(y, x) * u"rad")
 end
 
 # Cartesian <-> Cylindrical
-Base.convert(::Type{<:Cartesian}, (; ρ, ϕ, z)::Cylindrical) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ), z)
-function Base.convert(::Type{<:Cylindrical}, (; coords)::Cartesian{3})
+Base.convert(::Type{Cartesian}, (; ρ, ϕ, z)::Cylindrical) = Cartesian(ρ * cos(ϕ), ρ * sin(ϕ), z)
+function Base.convert(::Type{Cylindrical}, (; coords)::Cartesian{3})
   x, y, z = coords
   Cylindrical(sqrt(x^2 + y^2), atanpos(y, x) * u"rad", z)
 end
 
 # Cartesian <-> Spherical
-Base.convert(::Type{<:Cartesian}, (; r, θ, ϕ)::Spherical) =
+Base.convert(::Type{Cartesian}, (; r, θ, ϕ)::Spherical) =
   Cartesian(r * sin(θ) * cos(ϕ), r * sin(θ) * sin(ϕ), r * cos(θ))
-function Base.convert(::Type{<:Spherical}, (; coords)::Cartesian{3})
+function Base.convert(::Type{Spherical}, (; coords)::Cartesian{3})
   x, y, z = coords
   Spherical(sqrt(x^2 + y^2 + z^2), atan(sqrt(x^2 + y^2), z) * u"rad", atanpos(y, x) * u"rad")
 end
 
-# adjust negative angles
-function atanpos(y, x)
-  α = atan(y, x)
-  ifelse(α ≥ zero(α), α, α + oftype(α, 2π))
+# LatLon <-> Mercator
+function Base.convert(::Type{Mercator}, (; coords)::LatLon)
+  λ = deg2rad(coords.lon)
+  ϕ = deg2rad(coords.lat)
+  l = ustrip(λ)
+  a = oftype(l, ustrip(wgs84.a))
+  e = oftype(l, wgs84.e)
+  x = a * l
+  y = a * (asinh(tan(ϕ)) - e * atanh(e * sin(ϕ)))
+  Mercator(x * u"m", y * u"m")
+end
+
+# LatLon <-> WebMercator
+function Base.convert(::Type{WebMercator}, (; coords)::LatLon)
+  λ = deg2rad(coords.lon)
+  ϕ = deg2rad(coords.lat)
+  l = ustrip(λ)
+  a = oftype(l, ustrip(wgs84.a))
+  x = a * l
+  y = a * asinh(tan(ϕ))
+  WebMercator(x * u"m", y * u"m")
+end
+
+function Base.convert(::Type{LatLon}, (; coords)::WebMercator)
+  x = coords.x
+  y = coords.y
+  a = oftype(x, wgs84.a)
+  λ = x / a
+  ϕ = atan(sinh(y / a))
+  LatLon(rad2deg(ϕ) * u"°", rad2deg(λ) * u"°")
 end
