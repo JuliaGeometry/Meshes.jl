366014c7333eceec351c860d61b0a4e0eff60bff
Romeo Valentin <romeov@stanford.edu>
2024-02-09 16:17:50 -0800

    Overhaul constructors for Simplex type.

--- test/polytopes.jl
+++ test/polytopes.jl
@@ -872,6 +872,24 @@
   end
 
   @testset "Simplex" begin
+    @testset "construction" begin
+        pts = (Point(1., 2., 3.), Point(1., 2., 4.));
+        pts32 = convert.(Point{3,Float32}, pts)
+
+        @test Simplex(pts...).vertices == pts
+        @test Simplex{1}(pts...).vertices == pts
+        @test Simplex{1,3}(pts...).vertices == pts
+        @test Simplex{1,3,Float64}(pts...).vertices == pts
+        @test Simplex{1,3,Float32}(pts...).vertices == pts32
+        @test Simplex(pts32...).vertices == pts32
+
+        @test_throws MethodError Simplex(pts)
+        @test_throws MethodError Simplex(collect(pts))
+        @test_throws ArgumentError Simplex{2,3}(pts...)
+
+        @test nvertices(Simplex(pts32...)) == 2
+
+    end
     @testset "normal" begin
         splx = Simplex{3, 4}([Meshes.Point(rand(4)...) for _ in 1:4]...)
         n = normal(splx)
