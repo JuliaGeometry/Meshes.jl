35e94688f672dd4965ac5c01e56e4a966fca7d97
RomeoV <valentin.romeo@gmail.com>
2024-02-12 00:11:25 +0100

    Remove parametric constructors.
    Co-authored-by: Elias Carvalho <73039601+eliascarv@users.noreply.github.com>

--- src/polytopes/simplex.jl
+++ src/polytopes/simplex.jl
@@ -14,25 +14,14 @@ struct Simplex{K,Dim,T,N} <: Polytope{K,Dim,T}
 end
 
 
-Simplex(vertices::Vararg{Point{Dim,T},K_})           where {  Dim,T,K_   } = let
-    (K_-1)<=Dim  || throw(ArgumentError("(Rank K)==(num vertices - 1) must be less or equal to embedding dimension Dim."))
-    Simplex{K_-1,Dim,T,K_}(vertices)
+function Simplex(vertices::NTuple{N,Point{Dim,T}}) where {Dim,T,N}
+  K = N - 1
+  K ≤ Dim || throw(ArgumentError("simplex rank (number of vertices - 1) must be less or equal to embedding dimension"))
+  Simplex{K,Dim,T,N}(vertices)
 end
-Simplex{K}(vertices::Vararg{Point{Dim,T},K_})        where {K,Dim,T,K_   } = let
-    K+1 == K_ || throw(ArgumentError("Num vertices must be rank K plus one."))
-    K <= Dim || throw(ArgumentError("Rank K must be less or equal to embedding dimension Dim."))
-    Simplex{K_-1,Dim,T,K_}(vertices)
-end
-Simplex{K,Dim}(vertices::Vararg{Point{Dim,T},K_})    where {K,Dim,T,K_   } = let
-    K+1 == K_ || throw(ArgumentError("Num vertices must be rank K plus one."))
-    K<=Dim || throw(ArgumentError("Rank K must be less or equal to embedding dimension Dim."))
-    Simplex{K_-1,Dim,T,K_}(vertices)
-end
-Simplex{K,Dim,T}(vertices::Vararg{Point{Dim,T′},K_}) where {K,Dim,T,K_,T′} = let
-    K+1 == K_ || throw(ArgumentError("Num vertices must be rank K plus one"))
-    K<=Dim || throw(ArgumentError("Rank K must be less or equal to embedding dimension Dim."))
-    Simplex{K_-1,Dim,T,K_}(vertices)
-end
+
+Simplex(vertices::Point{Dim,T}...) where {Dim,T} = Simplex(vertices)
+Simplex(vertices::Tuple...) = Simplex(Point.(vertices))
 
 # ---------------------
 # HIGH-LEVEL INTERFACE
