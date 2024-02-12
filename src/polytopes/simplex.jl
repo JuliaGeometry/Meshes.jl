4680469197aadd23e6d17f5f72e9a07f9537ce8d
Romeo Valentin <romeov@stanford.edu>
2024-02-11 18:12:31 -0800

    Change NTuple constructor to Vararg constructor.

--- src/polytopes/simplex.jl
+++ src/polytopes/simplex.jl
@@ -28,9 +28,9 @@ Simplex{K}(vertices::NTuple{N}) where {K,N} = let
   N == K+1 || throw(ArgumentError("Number of vertices must be rank K plus one."))
   Simplex(vertices...)
 end
-Simplex{K}(vertices::NTuple{N}...) where {K,N} = let
+Simplex{K}(vertices::Vararg{Point,N}) where {K,N} = let
   N == K+1 || throw(ArgumentError("Number of vertices must be rank K plus one."))
-  Simplex(vertices)
+  Simplex(vertices...)
 end
 
 # ---------------------
