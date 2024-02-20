b7d409723c1bb0d528fb3eda702312854394c9c5
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-02-20 09:08:58 -0300

    Apply suggestions

--- src/polytopes/simplex.jl
+++ src/polytopes/simplex.jl
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------
 
 """
-    Simplex(p1, p2, ..., pK, pK+1)
+    Simplex(p₁, p₂, ..., pₖ, pₖ₊₁)
 
 A K-simplex is a polytope with parametric dimension K and K+1 vertices.
 It generalizes the [`Segment`](@ref), the  [`Triangle`](@ref) and the
@@ -15,15 +15,6 @@ struct Simplex{K,Dim,T,N} <: Polytope{K,Dim,T}
   vertices::NTuple{N,Point{Dim,T}}
 end
 
-function Simplex(vertices::NTuple{N,Point{Dim,T}}) where {Dim,T,N}
-  K = N - 1
-  K ≤ Dim || throw(ArgumentError("rank (number of vertices minus one) must be less or equal to embedding dimension"))
-  Simplex{K,Dim,T,N}(vertices)
-end
-
-Simplex(vertices::Point{Dim,T}...) where {Dim,T} = Simplex(vertices)
-Simplex(vertices::Tuple...) = Simplex(Point.(vertices))
-
 function Simplex{K}(vertices::NTuple{N,Point{Dim,T}}) where {K,Dim,T,N}
   N == K + 1 || throw(ArgumentError("number of vertices must be equal to rank K plus one"))
   K ≤ Dim || throw(ArgumentError("rank K must be less or equal to embedding dimension"))
@@ -32,6 +23,15 @@ end
 
 Simplex{K}(vertices::Point{Dim,T}...) where {K,Dim,T} = Simplex{K}(vertices)
 Simplex{K}(vertices::Tuple...) where {K} = Simplex{K}(Point.(vertices))
+
+function Simplex(vertices::NTuple{N,Point{Dim,T}}) where {Dim,T,N}
+  K = N - 1
+  K ≤ Dim || throw(ArgumentError("rank (number of vertices minus one) must be less or equal to embedding dimension"))
+  Simplex{K,Dim,T,N}(vertices)
+end
+
+Simplex(vertices::Point{Dim,T}...) where {Dim,T} = Simplex(vertices)
+Simplex(vertices::Tuple...) = Simplex(Point.(vertices))
 
 nvertices(::Type{<:Simplex{K}}) where {K} = K + 1
 
