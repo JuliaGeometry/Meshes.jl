b07621c4951e08bc13ad2f2d7e55daec8fcabcfc
Elias Carvalho <eliascarvdev@gmail.com>
2024-02-26 14:37:47 -0300

    Reintroduce 'vizmany!(plot, objs)'

--- ext/utils.jl
+++ ext/utils.jl
@@ -15,6 +15,20 @@ concat(obj₁, obj₂) = vcat(obj₁, obj₂)
 
 concat(mesh₁::Mesh, mesh₂::Mesh) = merge(mesh₁, mesh₂)
 
+function vizmany!(plot, objs)
+  color = plot[:color]
+  alpha = plot[:alpha]
+  colorscheme = plot[:colorscheme]
+  pointsize = plot[:pointsize]
+  segmentsize = plot[:segmentsize]
+
+  object = Makie.@lift reduce(concat, $objs)
+  colors = Makie.@lift mayberepeat($color, $objs)
+  alphas = Makie.@lift mayberepeat($alpha, $objs)
+
+  viz!(plot, object, color=colors, alpha=alphas, colorscheme=colorscheme, pointsize=pointsize, segmentsize=segmentsize)
+end
+
 function vizmany!(plot, objs, colorant)
   pointsize = plot[:pointsize]
   segmentsize = plot[:segmentsize]
