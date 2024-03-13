cd6e2cfa27bc804a01738bbb7ef151bf076d8f0c
Michael Ingold <mike.ingold@gmail.com>
2024-03-13 11:53:06 -0400

    Add Line to Line distance function

--- src/distances.jl
+++ src/distances.jl
@@ -6,6 +6,37 @@
 evaluate(d::PreMetric, g::Geometry, p::Point) = evaluate(d, p, g)
 
 """
+    evaluate(Euclidean(), line1, line2)
+
+Evaluate the minimum Euclidean distance between `line1` and `line2`.
+"""
+function evaluate(::Euclidean, line1::Line, line2::Line)
+  # Inspired by: John Alexiou, Find shortest distance between lines in 3D
+  #   URL (version 2022-11-04) https://math.stackexchange.com/q/2217845
+
+  e1 = line1(1) - line1(0)
+  e2 = line2(1) - line2(0)
+  n = e1 × e2
+  nn = n ⋅ n
+  r = line2(0) - line1(0)
+
+  # Find the Point's on each line where they are closest
+  t1 = ((e2 × n) ⋅ r) / nn
+  t2 = ((e1 × n) ⋅ r) / nn
+  p1 = line1(t1)
+  p2 = line2(t2)
+
+  if iszero(nn)
+    # lines are parallel, so pick an arbitrary Point on line1 and
+    #   find distance from it to line2
+    return evaluate(Euclidean(), line1(0), line2)
+  else
+    # find distance between closest Point's
+    return evaluate(Euclidean(), p1, p2)
+  end
+end
+
+"""
     evaluate(Euclidean(), point, line)
 
 Evaluate the Euclidean distance between `point` and `line`.
@@ -21,6 +52,6 @@ end
 """
     evaluate(::PreMetric, point1, point2)
 
-Evaluate pre-metric between coordinates of `point2` and `point2`.
+Evaluate pre-metric between coordinates of `point1` and `point2`.
 """
 evaluate(d::PreMetric, p1::Point, p2::Point) = evaluate(d, coordinates(p1), coordinates(p2))
--- test/distances.jl
+++ test/distances.jl
@@ -10,4 +10,8 @@
   p = P2(68, 259)
   l = Line(P2(68, 260), P2(69, 261))
   @test evaluate(Euclidean(), p, l) ≤ T(0.8)
+
+  line1 = Line(P3(-1,0,0), P3(1,0,0))
+  line2 = Line(P3(0,-1,1), P3(0,1,1))
+  @test evaluate(Euclidean(), line1, line2) ≈ T(1)
 end
