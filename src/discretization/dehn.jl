ad815c2bc0969735ac23f219704f731c73b371e7
cserteGT3 <cserteGT3@users.noreply.github.com>
2024-03-14 11:14:43 +0100

    Fix link in dehn's docs. (#796)

--- src/discretization/dehn.jl
+++ src/discretization/dehn.jl
@@ -9,7 +9,7 @@ Max Dehns' triangulation proved in 1899.
 
 The algorithm is described in the first chapter of Devadoss & Rourke 2011,
 and is based on a theorem derived in 1899 by the German mathematician Max Dehn.
-See https://en.wikipedia.org/wiki/Two_ears_theorem.
+See <https://en.wikipedia.org/wiki/Two_ears_theorem>.
 
 Because the algorithm relies on recursion, it is mostly appropriate for polygons
 with small number of vertices.
