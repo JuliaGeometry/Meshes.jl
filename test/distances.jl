0fd020ecf1423142b944519b6c7d07d7931cc8a7
Michael Ingold <mike.ingold@gmail.com>
2024-03-16 15:04:57 -0400

    Add relevant tests

--- test/distances.jl
+++ test/distances.jl
@@ -10,4 +10,10 @@
   p = P2(68, 259)
   l = Line(P2(68, 260), P2(69, 261))
   @test evaluate(Euclidean(), p, l) ≤ T(0.8)
+
+  line1 = Line(P3(-1,0,0), P3(1,0,0))
+  line2 = Line(P3(0,-1,1), P3(0,1,1))
+  line3 = Line(P3(-1,1,0), P3(1,1,0))
+  @test evaluate(Euclidean(), line1, line2) ≈ T(1)
+  @test evaluate(Euclidean(), line1, line3) ≈ T(1)
 end
