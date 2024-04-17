16616521aecc83df66dc934c1bc0f9e16d62ffdd
Elias Carvalho <eliascarvdev@gmail.com>
2024-04-17 14:42:29 -0300

    Remove '{Dim,T}' from 'GeometrySet'

--- src/sets.jl
+++ src/sets.jl
@@ -15,7 +15,7 @@ Set containing two balls centered at `(0.0, 0.0)` and `(1.0, 1.0)`:
 julia> GeometrySet([Ball((0.0, 0.0)), Ball((1.0, 1.0))])
 ```
 """
-struct GeometrySet{Dim,T,G<:Geometry{Dim,T}} <: Domain{Dim,T}
+struct GeometrySet{G<:Geometry} <: Domain
   geoms::Vector{G}
 end
 
@@ -37,7 +37,7 @@ Base.vcat(d1::Domain, d2::GeometrySet) = GeometrySet(vcat(collect(d1), d2.geoms)
 # SPECIAL CASE: POINT SET
 # ------------------------
 
-const PointSet{Dim,T} = GeometrySet{Dim,T,Point{Dim,T}}
+const PointSet{P<:Point} = GeometrySet{P}
 
 """
     PointSet(points)
@@ -58,7 +58,7 @@ julia> PointSet([1,2,3], [4,5,6])
 julia> PointSet([1 4; 2 5; 3 6])
 ```
 """
-PointSet(points::AbstractVector{P}) where {P<:Point} = PointSet{embeddim(P),coordtype(P)}(points)
+PointSet(points::AbstractVector{P}) where {P<:Point} = PointSet{P}(points)
 PointSet(points::Vararg{P}) where {P<:Point} = PointSet(collect(points))
 PointSet(coords::AbstractVector{TP}) where {TP<:Tuple} = PointSet(Point.(coords))
 PointSet(coords::Vararg{TP}) where {TP<:Tuple} = PointSet(collect(coords))
