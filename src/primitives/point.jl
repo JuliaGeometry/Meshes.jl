b426899796b5a7463aa6a56f785c9fa8a7bf198f
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-05-30 11:44:54 -0300

    Add a `convert` method for `Point` (#855)
    * Add a 'convert' method for 'Point'
    
    * Apply suggestions
    
    * Fix code
    
    * Fix typo

--- src/primitives/point.jl
+++ src/primitives/point.jl
@@ -41,6 +41,10 @@ end
 # convenience constructor
 Point(coords...) = Point(Cartesian(coords...))
 
+# conversions
+Base.convert(::Type{Point{Dim,CRSₜ}}, p::Point{Dim,CRSₛ}) where {Dim,CRSₜ,CRSₛ} = Point(convert(CRSₜ, p.coords))
+Base.convert(::Type{Point{Dim,CRS}}, p::Point{Dim,CRS}) where {Dim,CRS} = p
+
 paramdim(::Type{<:Point}) = 0
 
 lentype(::Type{<:Point{Dim,CRS}}) where {Dim,CRS} = lentype(CRS)
