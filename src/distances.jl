c0ede41497d6188e8cbe41a12562b6fbbc565f17
Elias Carvalho <eliascarvdev@gmail.com>
2024-05-31 11:36:16 -0300

    Refactor the implementation of the 'evaluate' function

--- src/distances.jl
+++ src/distances.jl
@@ -35,8 +35,43 @@ function evaluate(::Euclidean, line1::Line{Dim}, line2::Line{Dim}) where {Dim}
 end
 
 """
-    evaluate(::PreMetric, point1, point2)
+    evaluate(dist::PreMetric, point₁, point₂)
 
-Evaluate pre-metric between coordinates of `point1` and `point2`.
+Evaluate pre-metric `dist` between coordinates of `point₁` and `point₂`.
 """
-evaluate(d::PreMetric, p1::Point, p2::Point) = evaluate(d, to(p1), to(p2))
+function evaluate(dist::PreMetric, p₁::Point{Dim}, p₂::Point{Dim}) where {Dim}
+  u₁ = unit(Meshes.lentype(p₁))
+  u₂ = unit(Meshes.lentype(p₂))
+  u = Unitful.promote_unit(u₁, u₂)
+  v₁ = ustrip.(u, to(p₁))
+  v₂ = ustrip.(u, to(p₂))
+  evaluate(dist, v₁, v₂) * u
+end
+
+# --------------
+# SPECIAL CASES
+# --------------
+
+function evaluate(dist::Haversine, p₁::Point{Dim,<:LatLon}, p₂::Point{Dim,<:LatLon}) where {Dim}
+  uᵣ = unit(dist.radius)
+  u = uᵣ === NoUnits ? u"m" : uᵣ
+  latlon₁ = coords(p₁)
+  latlon₂ = coords(p₂)
+  v₁ = SVector(latlon₁.lon, latlon₁.lat)
+  v₂ = SVector(latlon₂.lon, latlon₂.lat)
+  evaluate(dist, v₁, v₂) * u
+end
+
+evaluate(dist::Haversine, p₁::Point{Dim}, p₂::Point{Dim}) where {Dim} =
+  evaluate(dist, Point(convert(LatLon, coords(p₁))), Point(convert(LatLon, coords(p₂))))
+
+function evaluate(dist::SphericalAngle, p₁::Point{Dim,<:LatLon}, p₂::Point{Dim,<:LatLon}) where {Dim}
+  latlon₁ = coords(p₁)
+  latlon₂ = coords(p₂)
+  v₁ = SVector(deg2rad(latlon₁.lon), deg2rad(latlon₁.lat))
+  v₂ = SVector(deg2rad(latlon₂.lon), deg2rad(latlon₂.lat))
+  evaluate(dist, v₁, v₂) * u"rad"
+end
+
+evaluate(dist::SphericalAngle, p₁::Point{Dim}, p₂::Point{Dim}) where {Dim} =
+  evaluate(dist, Point(convert(LatLon, coords(p₁))), Point(convert(LatLon, coords(p₂))))
