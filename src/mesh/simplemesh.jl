827c1bf6b8f278b5c8cc10bd7474ff77aa582a0c
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-04 11:12:02 -0300

    Fix code

--- src/mesh/simplemesh.jl
+++ src/mesh/simplemesh.jl
@@ -49,7 +49,7 @@ vertices(m::SimpleMesh) = m.vertices
 
 nvertices(m::SimpleMesh) = length(m.vertices)
 
-function Base.getindex(m::SimpleMesh{Dim,V,GridTopology{Dim}}, I::CartesianIndices{Dim}) where {Dim,V}
+function Base.getindex(m::SimpleMesh{Dim,<:Any,<:Any,GridTopology{Dim}}, I::CartesianIndices{Dim}) where {Dim}
   @boundscheck _checkbounds(m, I)
   dims = size(I)
   odims = size(m)
