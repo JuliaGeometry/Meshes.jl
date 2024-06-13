271c920551fcd71e9482e6838277045a227d9169
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-13 14:37:26 -0300

    Apply suggestions

--- src/transforms/shadow.jl
+++ src/transforms/shadow.jl
@@ -16,57 +16,60 @@ Shadow("xz")
 ```
 """
 struct Shadow{Dim} <: GeometricTransform
-  plane::NTuple{Dim,Symbol}
+  inds::SVector{Dim,Int}
+  Shadow(inds::SVector{Dim,Int}) where {Dim} = new{Dim}(sort(inds))
 end
 
-Shadow(plane::AbstractString) = Shadow(Tuple(Symbol.(sort(split(plane, "")))))
+Shadow(inds::NTuple{Dim,Int}) where {Dim} = Shadow(SVector(inds))
+
+Shadow(inds::Int...) = Shadow(inds)
+
+Shadow(plane::AbstractString) = Shadow(Tuple(_index(Val(Symbol(s))) for s in eachsplit(plane, "")))
+
 Shadow(plane::Symbol) = Shadow(string(plane))
 
-parameters(t::Shadow) = (; plane=t.plane)
+parameters(t::Shadow) = (; inds=t.inds)
 
-apply(t::Shadow, v::Vec) = _shadow(t, v), nothing
+apply(t::Shadow, v::Vec) = _shadow(v, t.inds), nothing
 
-apply(t::Shadow, g::GeometryOrDomain) = _shadow(t, g), nothing
+apply(t::Shadow, g::GeometryOrDomain) = _shadow(g, t.inds), nothing
 
 _index(::Val{:x}) = 1
 _index(::Val{:y}) = 2
 _index(::Val{:z}) = 3
 
-function _shadow(t::Shadow{Dim}, g) where {Dim}
-  inds = SVector(ntuple(i -> _index(Val(t.plane[i])), Dim))
-  _shadowinds(g, inds)
-end
+_shadow(v::Vec, inds) = v[inds]
 
-_shadowinds(v::Vec, inds) = v[inds]
+_shadow(p::Point, inds) = withdatum(p, to(p)[inds])
 
-_shadowinds(p::Point, inds) = withdatum(p, _shadowinds(to(p), inds))
-
-function _shadowinds(g::CartesianGrid, inds)
+function _shadow(g::CartesianGrid, inds)
   sz = size(g)[inds]
-  or = _shadowinds(minimum(g), inds)
+  or = _shadow(minimum(g), inds)
   sp = spacing(g)[inds]
   of = offset(g)[inds]
   CartesianGrid(sz, or, sp, of)
 end
 
-_shadowinds(g::RectilinearGrid, inds) = RectilinearGrid{datum(crs(g))}(xyz(g)[inds])
+_shadow(g::RectilinearGrid, inds) = RectilinearGrid{datum(crs(g))}(xyz(g)[inds])
 
-_dropdims(X, inds) = X[ntuple(i -> ifelse(i ∈ inds, :, 1), ndims(X))...]
-
-_shadowinds(g::StructuredGrid, inds) = StructuredGrid{datum(crs(g))}(map(X -> _dropdims(X, inds), XYZ(g)[inds]))
+function _shadow(g::StructuredGrid, inds)
+  ndims = length(size(g))
+  slices = ntuple(i -> ifelse(i ∈ inds, :, 1), ndims)
+  StructuredGrid{datum(crs(g))}(map(X -> X[slices...], XYZ(g)[inds]))
+end
 
 # apply shadow transform recursively
-@generated function _shadowinds(g::G, inds) where {G<:GeometryOrDomain}
+@generated function _shadow(g::G, inds) where {G<:GeometryOrDomain}
   ctor = constructor(G)
   names = fieldnames(G)
-  exprs = (:(_shadowinds(g.$name, inds)) for name in names)
+  exprs = (:(_shadow(g.$name, inds)) for name in names)
   :($ctor($(exprs...)))
 end
 
 # stop recursion at non-geometric types
-_shadowinds(x, _) = x
+_shadow(x, _) = x
 
 # special treatment for lists of geometries
-_shadowinds(g::NTuple{<:Any,<:Geometry}, inds) = map(gᵢ -> _shadowinds(gᵢ, inds), g)
-_shadowinds(g::AbstractVector{<:Geometry}, inds) = tcollect(_shadowinds(gᵢ, inds) for gᵢ in g)
-_shadowinds(g::CircularVector{<:Geometry}, inds) = CircularVector(tcollect(_shadowinds(gᵢ, inds) for gᵢ in g))
+_shadow(g::NTuple{<:Any,<:Geometry}, inds) = map(gᵢ -> _shadow(gᵢ, inds), g)
+_shadow(g::AbstractVector{<:Geometry}, inds) = tcollect(_shadow(gᵢ, inds) for gᵢ in g)
+_shadow(g::CircularVector{<:Geometry}, inds) = CircularVector(tcollect(_shadow(gᵢ, inds) for gᵢ in g))
--- test/transforms.jl
+++ test/transforms.jl
@@ -1301,10 +1301,10 @@
     @test !isaffine(Shadow(:xy))
     @test !TB.isrevertible(Shadow(:xy))
     @test !TB.isinvertible(Shadow(:xy))
-    @test TB.parameters(Shadow(:xy)) == (; plane=(:x, :y))
-    @test TB.parameters(Shadow(:yx)) == (; plane=(:x, :y))
-    @test TB.parameters(Shadow(:xz)) == (; plane=(:x, :z))
-    @test TB.parameters(Shadow(:yz)) == (; plane=(:y, :z))
+    @test TB.parameters(Shadow(:xy)) == (; inds=SVector(1, 2))
+    @test TB.parameters(Shadow(:yx)) == (; inds=SVector(1, 2))
+    @test TB.parameters(Shadow(:xz)) == (; inds=SVector(1, 3))
+    @test TB.parameters(Shadow(:yz)) == (; inds=SVector(2, 3))
 
     # ----
     # VEC
