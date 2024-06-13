a91a4fa17913ba49e5a4defa49d84f237c7e50e4
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-13 11:14:39 -0300

    Apply suggestions

--- src/transforms/shadow.jl
+++ src/transforms/shadow.jl
@@ -34,39 +34,39 @@ _index(::Val{:z}) = 3
 
 function _shadow(t::Shadow{Dim}, g) where {Dim}
   inds = SVector(ntuple(i -> _index(Val(t.plane[i])), Dim))
-  _shadow(g, inds)
+  _shadowinds(g, inds)
 end
 
-_shadow(v::Vec, inds) = v[inds]
+_shadowinds(v::Vec, inds) = v[inds]
 
-_shadow(p::Point, inds) = withdatum(p, _shadow(to(p), inds))
+_shadowinds(p::Point, inds) = withdatum(p, _shadowinds(to(p), inds))
 
-function _shadow(g::CartesianGrid, inds)
+function _shadowinds(g::CartesianGrid, inds)
   sz = size(g)[inds]
-  or = _shadow(minimum(g), inds)
+  or = _shadowinds(minimum(g), inds)
   sp = spacing(g)[inds]
   of = offset(g)[inds]
   CartesianGrid(sz, or, sp, of)
 end
 
-_shadow(g::RectilinearGrid, inds) = RectilinearGrid{datum(crs(g))}(xyz(g)[inds])
+_shadowinds(g::RectilinearGrid, inds) = RectilinearGrid{datum(crs(g))}(xyz(g)[inds])
 
 _dropdims(X, inds) = X[ntuple(i -> ifelse(i ∈ inds, :, 1), ndims(X))...]
 
-_shadow(g::StructuredGrid, inds) = StructuredGrid{datum(crs(g))}(map(X -> _dropdims(X, inds), XYZ(g)[inds]))
+_shadowinds(g::StructuredGrid, inds) = StructuredGrid{datum(crs(g))}(map(X -> _dropdims(X, inds), XYZ(g)[inds]))
 
 # apply shadow transform recursively
-@generated function _shadow(g::G, inds) where {G<:GeometryOrDomain}
+@generated function _shadowinds(g::G, inds) where {G<:GeometryOrDomain}
   ctor = constructor(G)
   names = fieldnames(G)
-  exprs = (:(_shadow(g.$name, inds)) for name in names)
+  exprs = (:(_shadowinds(g.$name, inds)) for name in names)
   :($ctor($(exprs...)))
 end
 
 # stop recursion at non-geometric types
-_shadow(x, _) = x
+_shadowinds(x, _) = x
 
 # special treatment for lists of geometries
-_shadow(g::NTuple{<:Any,<:Geometry}, inds) = map(gᵢ -> _shadow(gᵢ, inds), g)
-_shadow(g::AbstractVector{<:Geometry}, inds) = tcollect(_shadow(gᵢ, inds) for gᵢ in g)
-_shadow(g::CircularVector{<:Geometry}, inds) = CircularVector(tcollect(_shadow(gᵢ, inds) for gᵢ in g))
+_shadowinds(g::NTuple{<:Any,<:Geometry}, inds) = map(gᵢ -> _shadowinds(gᵢ, inds), g)
+_shadowinds(g::AbstractVector{<:Geometry}, inds) = tcollect(_shadowinds(gᵢ, inds) for gᵢ in g)
+_shadowinds(g::CircularVector{<:Geometry}, inds) = CircularVector(tcollect(_shadowinds(gᵢ, inds) for gᵢ in g))
