d699a1727b447505a133235ad173dd0e8c413f82
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-13 11:00:52 -0300

    Add tests

--- src/transforms/shadow.jl
+++ src/transforms/shadow.jl
@@ -24,25 +24,22 @@ Shadow(plane::Symbol) = Shadow(string(plane))
 
 parameters(t::Shadow) = (; plane=t.plane)
 
-function apply(t::Shadow{Dim}, g::GeometryOrDomain) where {Dim}
-  inds = SVector(ntuple(i -> _index(Val(t.plane[i])), Dim))
-  _shadow(g, inds), nothing
-end
+apply(t::Shadow, v::Vec) = _shadow(t, v), nothing
+
+apply(t::Shadow, g::GeometryOrDomain) = _shadow(t, g), nothing
 
 _index(::Val{:x}) = 1
 _index(::Val{:y}) = 2
 _index(::Val{:z}) = 3
 
+function _shadow(t::Shadow{Dim}, g) where {Dim}
+  inds = SVector(ntuple(i -> _index(Val(t.plane[i])), Dim))
+  _shadow(g, inds)
+end
+
 _shadow(v::Vec, inds) = v[inds]
 
 _shadow(p::Point, inds) = withdatum(p, _shadow(to(p), inds))
-
-@generated function _shadow(g::G, inds) where {G<:GeometryOrDomain}
-  ctor = constructor(G)
-  names = fieldnames(G)
-  exprs = (:(_shadow(g.$name, inds)) for name in names)
-  :($ctor($(exprs...)))
-end
 
 function _shadow(g::CartesianGrid, inds)
   sz = size(g)[inds]
@@ -57,3 +54,18 @@ _shadow(g::RectilinearGrid, inds) = RectilinearGrid{datum(crs(g))}(xyz(g)[inds])
 _dropdims(X, inds) = X[ntuple(i -> ifelse(i ∈ inds, :, 1), ndims(X))...]
 
 _shadow(g::StructuredGrid, inds) = StructuredGrid{datum(crs(g))}(map(X -> _dropdims(X, inds), XYZ(g)[inds]))
+
+# apply shadow transform recursively
+@generated function _shadow(g::G, inds) where {G<:GeometryOrDomain}
+  ctor = constructor(G)
+  names = fieldnames(G)
+  exprs = (:(_shadow(g.$name, inds)) for name in names)
+  :($ctor($(exprs...)))
+end
+
+# stop recursion at non-geometric types
+_shadow(x, _) = x
+
+_shadow(g::NTuple{<:Any,<:Geometry}, inds) = map(gᵢ -> _shadow(gᵢ, inds), g)
+_shadow(g::AbstractVector{<:Geometry}, inds) = tcollect(_shadow(gᵢ, inds) for gᵢ in g)
+_shadow(g::CircularVector{<:Geometry}, inds) = CircularVector(tcollect(_shadow(gᵢ, inds) for gᵢ in g))
