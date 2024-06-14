be2e5d13eda890426acde65f0c227df3aca23ddd
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-14 15:41:56 -0300

    Fix the implementation of the 'LengthUnit' transform

--- src/transforms/lengthunit.jl
+++ src/transforms/lengthunit.jl
@@ -55,6 +55,10 @@ _lenunit(c, _) = throw(ArgumentError("the length unit of $(prettyname(c)) cannot
 # SPECIAL CASES
 # --------------
 
+applycoord(t::LengthUnit, radius::Len) = uconvert(t.unit, radius)
+
+applycoord(t::LengthUnit, radii::NTuple{Dim,Len}) where {Dim} = uconvert.(t.unit, radii)
+
 applycoord(t::LengthUnit, g::RectilinearGrid) = RectilinearGrid{datum(crs(g))}(map(x -> uconvert.(t.unit, x), xyz(g)))
 
 applycoord(t::LengthUnit, g::StructuredGrid) = StructuredGrid{datum(crs(g))}(map(X -> uconvert.(t.unit, X), XYZ(g)))
