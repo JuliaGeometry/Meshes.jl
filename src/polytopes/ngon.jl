d725c950cbb53ddd7415ce50f5e045ea65975fcc
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-20 08:45:59 -0300

    Apply suggestions

--- src/polytopes/ngon.jl
+++ src/polytopes/ngon.jl
@@ -54,10 +54,8 @@ nvertices(::Type{<:Ngon{N}}) where {N} = N
 
 ==(p₁::Ngon, p₂::Ngon) = p₁.vertices == p₂.vertices
 
-function Base.isapprox(p₁::Ngon, p₂::Ngon; atol=atol(lentype(p₁)), kwargs...)
-  nvertices(p₁) ≠ nvertices(p₂) && return false
-  all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(p₁.vertices, p₂.vertices))
-end
+Base.isapprox(p₁::Ngon, p₂::Ngon; atol=atol(lentype(p₁)), kwargs...) =
+  nvertices(p₁) == nvertices(p₂) && all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(p₁.vertices, p₂.vertices))
 
 rings(ngon::Ngon) = [Ring(pointify(ngon))]
 
