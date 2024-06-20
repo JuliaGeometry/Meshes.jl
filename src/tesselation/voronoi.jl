4780740fd9a83982e049aa729b7dd11d91ba25ab
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-20 14:06:58 -0300

    Fix 'VoronoiTesselation' implementation

--- src/tesselation/voronoi.jl
+++ src/tesselation/voronoi.jl
@@ -20,13 +20,19 @@ end
 VoronoiTesselation(rng=Random.default_rng()) = VoronoiTesselation(rng)
 
 function tesselate(pset::PointSet{2}, method::VoronoiTesselation)
+  d = datum(crs(pset))
+  u = unit(lentype(pset))
+
   # perform tesselation with raw coordinates
   coords = map(p -> ustrip.(to(p)), pset)
   triang = triangulate(coords, rng=method.rng)
   vorono = voronoi(triang, clip=true)
 
   # mesh with all (possibly unused) points
-  points = get_polygon_points(vorono)
+  points = map(get_polygon_points(vorono)) do (x, y)
+    coords = Cartesian{d}(x * u, y * u)
+    Point(coords)
+  end
   polygs = each_polygon(vorono)
   tuples = [Tuple(inds[begin:(end - 1)]) for inds in polygs]
   connec = connect.(tuples)
--- test/tesselation.jl
+++ test/tesselation.jl
@@ -5,6 +5,13 @@
     mesh1 = tesselate(pts, DelaunayTesselation(StableRNG(2024)))
     mesh2 = tesselate(pset, DelaunayTesselation(StableRNG(2024)))
     @test mesh1 == mesh2
+
+    # datum and unit propagation
+    tuples = [(rand(T) * u"km", rand(T) * u"km") for _ in 1:100]
+    pset = PointSet(Point.(Cartesian{WGS84Latest}.(tuples)))
+    mesh = tesselate(pset, DelaunayTesselation(StableRNG(2024)))
+    @test datum(Meshes.crs(mesh)) === WGS84Latest
+    @test unit(Meshes.lentype(mesh)) == u"km"
   end
 
   @testset "Voronoi" begin
@@ -13,5 +20,12 @@
     mesh1 = tesselate(pts, VoronoiTesselation(StableRNG(2024)))
     mesh2 = tesselate(pset, VoronoiTesselation(StableRNG(2024)))
     @test mesh1 == mesh2
+
+    # datum and unit propagation
+    tuples = [(rand(T) * u"km", rand(T) * u"km") for _ in 1:100]
+    pset = PointSet(Point.(Cartesian{WGS84Latest}.(tuples)))
+    mesh = tesselate(pset, VoronoiTesselation(StableRNG(2024)))
+    @test datum(Meshes.crs(mesh)) === WGS84Latest
+    @test unit(Meshes.lentype(mesh)) == u"km"
   end
 end
