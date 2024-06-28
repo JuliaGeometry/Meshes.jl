45529e0c498f3256d8c7d1e5b3158f2a06308bcb
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-28 17:43:13 -0300

    Apply suggestions

--- src/transforms/flatcoords.jl
+++ src/transforms/flatcoords.jl
@@ -13,10 +13,15 @@ applycoord(::FlatCoords, v::Vec) = v
 
 applycoord(::FlatCoords, p::Point) = Point(_flatcoords(coords(p)))
 
-_flatcoords(::CRS) = throw(ArgumentError("points must have 2 coordinates"))
-_flatcoords(coords::Cartesian{Datum,2}) where {Datum} = coords
-_flatcoords(coords::Polar) = convert(Cartesian, coords)
+function _flatcoords(coords::CRS) 
+  if CoordRefSystems.ncoords(coords) ≠ 2
+    throw(ArgumentError("points must have 2 coordinates"))
+  end
+  convert(Cartesian, coords)
+end
+
 _flatcoords(coords::LatLon) = Cartesian{datum(coords)}(CoordRefSystems.rawvalues(coords))
+
 _flatcoords(coords::GeocentricLatLon) = Cartesian{datum(coords)}(CoordRefSystems.rawvalues(coords))
+
 _flatcoords(coords::AuthalicLatLon) = Cartesian{datum(coords)}(CoordRefSystems.rawvalues(coords))
-_flatcoords(coords::CoordRefSystems.Projected) = convert(Cartesian, coords)
