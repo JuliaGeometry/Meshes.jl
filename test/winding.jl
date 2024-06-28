003b48770ed758cca1cefdf6f9bd6b25f19d8219
Elias Carvalho <eliascarvdev@gmail.com>
2024-06-28 15:29:21 -0300

    Add tests

--- test/winding.jl
+++ test/winding.jl
@@ -11,6 +11,12 @@
   @test winding(p, reverse(c)) ≈ T(-2)
   @test winding([p, p], c) ≈ T[2, 2]
 
+  p = Point(LatLon(T(0.5), T(0.5)))
+  c = Ring(Point.([LatLon(T(0), T(0)), LatLon(T(1), T(0)), LatLon(T(1), T(1)), LatLon(T(0), T(1))]))
+  @test winding(p, c) ≈ T(1)
+  @test winding(p, reverse(c)) ≈ T(-1)
+  @test winding([p, p], c) ≈ T[1, 1]
+
   m = boundary(Box(point(0, 0, 0), point(2, 2, 2)))
   @test all(>(0), winding(vertices(m), m))
   @test isapprox(winding(point(1, 1, 1), m), T(1), atol=atol(T))
