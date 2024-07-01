dd9e27dc8071678f6dbd32b23ac333d41dee5980
Elias Carvalho <eliascarvdev@gmail.com>
2024-07-01 15:02:23 -0300

    Add docstring

--- src/transforms/within.jl
+++ src/transforms/within.jl
@@ -2,6 +2,23 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
+"""
+    Within(x=(xmin, xmax), y=(ymin, ymax), z=(zmin, zmax))
+
+Retain the domain geometries that are within x bounds [`xmax`,`xmax`],
+y bounds [`ymax`,`ymax`] and z bounds [`zmin`,`zmax`],
+all in length units (default to meters).
+
+All bounds are optional and only applicable bounds will be used.
+
+## Examples
+
+```julia
+Within(x=(2, 4))
+Within(x=(1u"km", 3u"km"))
+Within(y=(1.2, 1.8), z=(2.4, 3.0))
+```
+"""
 struct Within{X,Y,Z} <: GeometricTransform
   x::X
   y::Y
@@ -9,9 +26,9 @@ struct Within{X,Y,Z} <: GeometricTransform
 end
 
 Within(; x=nothing, y=nothing, z=nothing) = Within(
-  isnothing(x) ? x : addunit.(x, u"m"),
-  isnothing(y) ? y : addunit.(y, u"m"),
-  isnothing(z) ? z : addunit.(z, u"m")
+  isnothing(x) ? x : _aslen.(x, u"m"),
+  isnothing(y) ? y : _aslen.(y, u"m"),
+  isnothing(z) ? z : _aslen.(z, u"m")
 )
 
 function preprocess(t::Within, d::Domain)
@@ -28,16 +45,20 @@ function apply(t::Within, d::Domain)
   n, nothing
 end
 
-function _within(dim, bound, box)
-  Dim = embeddim(box)
+_aslen(x::Len) = x
+_aslen(x::Number) = x * u"m"
+_aslen(x::Quantity) = throw(ArgumentError("invalid units, please check the documentation"))
+
+function _within(dim, bound, bbox)
+  Dim = embeddim(bbox)
   if Dim < dim || isnothing(bound)
-    box
+    bbox
   else
     bmin, bmax = bound
-    min = to(minimum(box))
-    max = to(maximum(box))
+    min = to(minimum(bbox))
+    max = to(maximum(bbox))
     nmin = Vec(ntuple(i -> i == dim ? bmin : min[i], Dim))
     nmax = Vec(ntuple(i -> i == dim ? bmax : max[i], Dim))
-    Box(withdatum(box, nmin), withdatum(box, nmax))
+    Box(withdatum(bbox, nmin), withdatum(bbox, nmax))
   end
 end
