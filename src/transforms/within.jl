07273c252abaf8f469d70bf911bdf906d2ea65b3
Elias Carvalho <eliascarvdev@gmail.com>
2024-07-01 14:38:52 -0300

    Apply suggestions

--- src/transforms/within.jl
+++ src/transforms/within.jl
@@ -2,16 +2,33 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-struct Within{T} <: GeometricTransform
-  bounds::T
+struct Within{X,Y,Z} <: GeometricTransform
+  x::X
+  y::Y
+  z::Z
 end
 
-_addunit(x::Nothing) = x
-_addunit(x) = addunit.(x, u"m")
+Within(; x=nothing, y=nothing, z=nothing) = Within(
+  isnothing(x) ? x : addunit.(x, u"m"),
+  isnothing(y) ? y : addunit.(y, u"m"),
+  isnothing(z) ? z : addunit.(z, u"m")
+)
 
-Within(; x=nothing, y=nothing, z=nothing) = Within((_addunit(x), _addunit(y), _addunit(z)))
+function preprocess(t::Within, d::Domain)
+  bbox = boundingbox(d)
+  bbox₁ = _within(1, t.x, bbox)
+  bbox₂ = _within(2, t.y, bbox₁)
+  bbox₃ = _within(3, t.z, bbox₂)
+  indices(d, bbox₃)
+end
 
-function _applybound(dim, bound, box)
+function apply(t::Within, d::Domain)
+  inds = preprocess(t, d)
+  n = view(d, inds)
+  n, nothing
+end
+
+function _within(dim, bound, box)
   Dim = embeddim(box)
   if Dim < dim || isnothing(bound)
     box
@@ -23,18 +40,4 @@ function _applybound(dim, bound, box)
     nmax = Vec(ntuple(i -> i == dim ? bmax : max[i], Dim))
     Box(withdatum(box, nmin), withdatum(box, nmax))
   end
-end
-
-function preprocess(t::Within, d::Domain)
-  bbox = boundingbox(d)
-  for (dim, bound) in enumerate(t.bounds)
-    bbox = _applybound(dim, bound, bbox)
-  end
-  indices(d, bbox)
-end
-
-function apply(t::Within, d::Domain)
-  inds = preprocess(t, d)
-  n = view(d, inds)
-  n, nothing
 end
