d50ed47d477bf26f271f57fe9cfc354ee92278be
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-07-28 11:47:16 -0400

    use ScopedValues instead of Preferences

--- src/tolerances.jl
+++ src/tolerances.jl
@@ -2,8 +2,8 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-const ATOL_FLOAT64 = @load_preference("atol_float64", 1.0e-10)
-const ATOL_FLOAT32 = Float32(@load_preference("atol_float32", 1.0f-5))
+ATOL64 = ScopedValue(1e-10)
+ATOL32 = ScopedValue(1f-5)
 
 """
     atol(T)
@@ -18,8 +18,8 @@ isapprox(a::T, b::T, atol=atol(T))
 ```
 """
 atol(x) = atol(typeof(x))
-atol(::Type{Float64}) = ATOL_FLOAT64
-atol(::Type{Float32}) = ATOL_FLOAT32
+atol(::Type{Float64}) = ATOL64[]
+atol(::Type{Float32}) = ATOL32[]
 atol(ℒ::Type{<:Len}) = atol(numtype(ℒ)) * unit(ℒ)
 atol(𝒜::Type{<:Area}) = atol(numtype(𝒜))^2 * unit(𝒜)
 atol(𝒱::Type{<:Vol}) = atol(numtype(𝒱))^3 * unit(𝒱)
