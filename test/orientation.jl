8cb14ceecba065d440c1dfb06dac0b30a52ed8f7
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-07-31 15:44:48 -0300

    Fix orientation with LatLon coords (#970)

--- test/orientation.jl
+++ test/orientation.jl
@@ -9,4 +9,10 @@
   r1 = Ring(cart.([(0, 0), (1, 0), (1, 1), (0, 1)]))
   r2 = Ring(cart.([(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)]))
   @test orientation(r1) == orientation(r2)
+
+  # orientation of rings with LatLon coordinates
+  r = Ring(latlon.([(0, 0), (0, 90), (90, 0)]))
+  @test orientation(r) == CCW
+  r = Ring(latlon.([(0, 0), (90, 0), (0, 90)]))
+  @test orientation(r) == CW
 end
