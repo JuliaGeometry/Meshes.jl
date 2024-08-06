b2c8093a90618863bccc10e6a317e6a09fde2b4f
Alberto Mengali <disberd@gmail.com>
2024-08-06 08:52:34 +0200

    fix typo

--- test/tesselation.jl
+++ test/tesselation.jl
@@ -48,7 +48,7 @@
 
     # Test polygon order is the same as input points order
     pts = randpoint2(10)
-    mesh = tesselate(pts, VoronoiTesselation(StableRNG(2024))
+    mesh = tesselate(pts, VoronoiTesselation(StableRNG(2024)))
     @test all(zip(pts, mesh)) do (pt, poly)
       pt in poly || pt ∉ mesh # The pt ∉ mesh is because some points are on the border of the respective polygon and return false from `pt in poly` due to floating point precision. So if that happens we check that the point is not in any other polygon
     end
