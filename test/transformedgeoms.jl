35688bbc66e441df64ea840f7481a98656024008
Elias Carvalho <eliascarvdev@gmail.com>
2024-08-13 15:45:50 -0300

    Add more tests

--- test/transformedgeoms.jl
+++ test/transformedgeoms.jl
@@ -10,6 +10,7 @@
   @test paramdim(tb) == paramdim(b)
   @test centroid(tb) == t(centroid(b))
   @test discretize(tb) == t(discretize(b))
+  @test tb(T(0.5), T(0.5)) == t(b(T(0.5), T(0.5)))
   t3 = Scale(T(2), T(2))
   tb3 = Meshes.TransformedGeometry(b, t3)
   @test measure(tb3) == 4 * measure(b)
