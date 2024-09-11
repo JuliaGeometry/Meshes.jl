2f0cf15a83f656834c0c9729914fc4a265a84809
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-09-11 13:25:46 -0300

    Refactor viz of grids (#1055)
    * Refactor viz of grids
    
    * Fix code

--- ext/grid.jl
+++ ext/grid.jl
@@ -14,7 +14,7 @@ function vizgrid!(plot, ::Type{<:🌐}, pdim::Val, edim::Val)
   vizgrid!(plot, 𝔼, pdim, edim)
 end
 
-vizgrid!(plot, M::Type{<:𝔼}, pdim::Val, edim::Val) = vizmesh!(plot, M, pdim, edim)
+vizgrid!(plot, M::Type{<:𝔼}, pdim::Val, edim::Val) = vizgridfallback!(plot, M, pdim, edim)
 
 # ----------------
 # SPECIALIZATIONS
@@ -24,6 +24,56 @@ include("grid/cartesian.jl")
 include("grid/rectilinear.jl")
 include("grid/structured.jl")
 include("grid/transformed.jl")
+
+# -----------------
+# HELPER FUNCTIONS
+# -----------------
+
+function vizgridfallback!(plot, M, pdim, edim)
+  grid = plot[:object]
+  color = plot[:color]
+  alpha = plot[:alpha]
+  colormap = plot[:colormap]
+  colorrange = plot[:colorrange]
+
+  if pdim == Val(2) # visualize quadrangle mesh with texture using uv coords
+    # decide whether or not to reverse connectivity list
+    rfunc = Makie.@lift _reverse(crs($grid))
+
+    verts = Makie.@lift map(asmakie, vertices($grid))
+    quads = Makie.@lift [GB.QuadFace($rfunc(indices(e))) for e in elements(topology($grid))]
+
+    colorant = Makie.@lift process($color, $colormap, $colorrange, $alpha)
+
+    nverts = Makie.@lift length($verts)
+    nquads = Makie.@lift length($quads)
+    ncolor = Makie.@lift $colorant isa AbstractVector ? length($colorant) : 1
+
+    dims = Makie.@lift size($grid)
+    texture = if ncolor[] == 1
+      Makie.@lift fill($colorant, $dims)
+    elseif ncolor[] == nquads[]
+      Makie.@lift reshape($colorant, $dims)
+    elseif ncolor[] == nverts[]
+      Makie.@lift reshape($colorant, $dims .+ 1)
+    else
+      throw(ArgumentError("invalid number of colors"))
+    end
+
+    uv = Makie.@lift [Makie.Vec2f(v, 1 - u) for v in range(0, 1, $dims[2] + 1) for u in range(0, 1, $dims[1] + 1)]
+
+    mesh = Makie.@lift GB.Mesh(Makie.meta($verts, uv=$uv), $quads)
+
+    shading = edim == Val(3) ? Makie.FastShading : Makie.NoShading
+
+    Makie.mesh!(plot, mesh, color=texture, shading=shading)
+  else # fallback to triangle mesh visualization
+    vizmesh!(plot, M, pdim, edim)
+  end
+end
+
+_reverse(::Type{<:CRS}) = identity
+_reverse(::Type{<:LatLon}) = reverse
 
 # helper functions to create a minimum number
 # of line segments within Cartesian/Rectilinear grid
