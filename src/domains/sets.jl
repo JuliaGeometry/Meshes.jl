d5a2fc379d74affe341468958e68f4e33b6e502f
Elias Carvalho <eliascarvdev@gmail.com>
2024-09-12 10:16:40 -0300

    Update docstrings

--- src/domains/sets.jl
+++ src/domains/sets.jl
@@ -14,6 +14,10 @@ Set containing two balls centered at `(0.0, 0.0)` and `(1.0, 1.0)`:
 ```julia
 julia> GeometrySet([Ball((0.0, 0.0)), Ball((1.0, 1.0))])
 ```
+
+### Notes
+
+* Geometries with different CRS will be projected to the CRS of the first geometry.
 """
 struct GeometrySet{M<:Manifold,C<:CRS,G<:Geometry{M,C}} <: Domain{M,C}
   geoms::Vector{G}
