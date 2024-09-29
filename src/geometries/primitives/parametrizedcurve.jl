615753e86ace598bf1fb6075d96366bd08e554aa
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-09-29 12:05:52 +0200

    format

--- src/geometries/primitives/parametrizedcurve.jl
+++ src/geometries/primitives/parametrizedcurve.jl
@@ -15,16 +15,16 @@ ParametrizedCurve(0.0, 1.0, t -> Point(cospi(2t), sinpi(2t)))
 ```
 """
 struct ParametrizedCurve{M<:Meshes.Manifold,C<:Meshes.CRS,T<:Real,F<:Function} <: Primitive{M,C}
-    a::T
-    b::T
-    func::F
+  a::T
+  b::T
+  func::F
 
-    function ParametrizedCurve(a::T, b::T, func) where {T<:Real}
-        p = func(a)
-        M = manifold(p)
-        C = crs(p)
-        new{M, C, T, typeof(func)}(a, b, func)
-    end
+  function ParametrizedCurve(a::T, b::T, func) where {T<:Real}
+    p = func(a)
+    M = manifold(p)
+    C = crs(p)
+    new{M,C,T,typeof(func)}(a, b, func)
+  end
 end
 
 paramdim(::ParametrizedCurve) = 1
@@ -34,8 +34,8 @@ end_parameter(curve::ParametrizedCurve) = curve.b
 end_point(curve::ParametrizedCurve) = curve(curve.b)
 
 function (curve::ParametrizedCurve)(t)
-    if t < start_parameter(curve) || t > end_parameter(curve)
-        throw(DomainError(t, "c(t) is not defined for t outside [$(start_parameter(curve)), $(end_parameter(curve))]."))
-    end
-    curve.func(t)
+  if t < start_parameter(curve) || t > end_parameter(curve)
+    throw(DomainError(t, "c(t) is not defined for t outside [$(start_parameter(curve)), $(end_parameter(curve))]."))
+  end
+  curve.func(t)
 end
