2b074500bf9acbcba4173c93de1327b219d5cc74
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-10-03 06:16:48 -0700

    Update src/sampling/fibonacci.jl

--- src/sampling/fibonacci.jl
+++ src/sampling/fibonacci.jl
@@ -8,7 +8,7 @@
 Generate `n` Fibonacci points with parameter `ϕ`.
 
 The golden ratio is used as the default value of `ϕ`,
-but other irrational values can be used.
+but other irrational numbers can be used.
 
 See <https://observablehq.com/@meetamit/fibonacci-lattices>
 and <https://www.johndcook.com/blog/2023/08/12/fibonacci-lattice>.
