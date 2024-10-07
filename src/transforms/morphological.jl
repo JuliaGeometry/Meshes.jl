5da5a5c132aee229e5c87a8e7de131af85a246db
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-07 18:06:13 +0200

    use f: CRS->CRS

--- src/transforms/morphological.jl
+++ src/transforms/morphological.jl
@@ -11,20 +11,14 @@ Morphological transform given by a function `func`.
 
 ```julia
 TODO
-# with `Cartesian`:
-# f(c) = Point(cospi(2*Meshes.ustrip(c.x)), sinpi(2*Meshes.ustrip(c.x)))
-# with `Vec`:
-f(v) = cospi(2*v[1]), sinpi(2*v[1])
+f(c) = Cartesian(cospi(2*Meshes.ustrip(c.x)), sinpi(2*Meshes.ustrip(c.x)))
 box = Box((0.0,), (1.0,))
 t = Morphological(f)
 tr = TransformedGeometry(box, t)
 
 box = Box((-pi,), (pi,))
 f(theta) = 1 - 1/3 * sin(2*theta)^2
-# with `Cartesian`:
-# tr = TransformedGeometry(box, Morphological(c -> Point(Polar(f(Meshes.ustrip(c.x)), Meshes.ustrip(c.x)))))
-# with `Vec:
-tr = TransformedGeometry(box, Morphological(v -> Polar(f(v[1]), v[1])))
+tr = TransformedGeometry(box, Morphological(c -> Point(Polar(f(Meshes.ustrip(c.x)), Meshes.ustrip(c.x)))))
 ```
 """
 struct Morphological{F<:Function} <: CoordinateTransform
@@ -34,7 +28,5 @@ struct Morphological{F<:Function} <: CoordinateTransform
   end
 end
 
-# applycoord(t::Morphological, p::Point) = t.func(coords(p))
-# applycoord(t::Morphological, v::Vec) = t.func(Point(v...))
-applycoord(t::Morphological, p::Point) = Point(t.func(ustrip(to(p))))
-applycoord(t::Morphological, v::Vec) = Point(t.func(v))
+applycoord(t::Morphological, p::Point) = Point(t.func(coords(p)))
+applycoord(t::Morphological, v::Vec) = Point(t.func(Point(v...)))
