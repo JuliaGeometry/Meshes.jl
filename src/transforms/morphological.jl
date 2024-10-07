59ae0255e218f54ef7cc281959d794b66f0e9e7d
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-07 18:16:20 +0200

    adjust docstring to how it should work

--- src/transforms/morphological.jl
+++ src/transforms/morphological.jl
@@ -11,14 +11,14 @@ Morphological transform given by a function `func`.
 
 ```julia
 TODO
-f(c) = Cartesian(cospi(2*Meshes.ustrip(c.x)), sinpi(2*Meshes.ustrip(c.x)))
+f(c) = Cartesian(cospi(2*c.x), sinpi(2*c.x))
 box = Box((0.0,), (1.0,))
 t = Morphological(f)
 tr = TransformedGeometry(box, t)
 
 box = Box((-pi,), (pi,))
 f(theta) = 1 - 1/3 * sin(2*theta)^2
-tr = TransformedGeometry(box, Morphological(c -> Point(Polar(f(Meshes.ustrip(c.x)), Meshes.ustrip(c.x)))))
+tr = TransformedGeometry(box, Morphological(c -> Polar(f(c.x), c.x)))
 ```
 """
 struct Morphological{F<:Function} <: CoordinateTransform
