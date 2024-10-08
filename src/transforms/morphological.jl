ca4301a17053ec960af35621a48a54c1b12d740c
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-08 17:55:05 +0200

    remove applycoord for Vec

--- src/transforms/morphological.jl
+++ src/transforms/morphological.jl
@@ -11,14 +11,8 @@ Morphological transform given by a function `func`.
 
 ```julia
 TODO
-f(c) = Cartesian(cospi(2*c.x), sinpi(2*c.x))
-box = Box((0.0,), (1.0,))
-t = Morphological(f)
-tr = TransformedGeometry(box, t)
-
-box = Box((-pi,), (pi,))
-f(theta) = 1 - 1/3 * sin(2*theta)^2
-tr = TransformedGeometry(box, Morphological(c -> Polar(f(c.x), c.x)))
+ball = Ball((0.0, 0.0), 1)
+tr = TransformedGeometry(ball, Morphological(c -> Cartesian(c.x + c.y, c.y, c.x - c.y)))
 ```
 """
 struct Morphological{F<:Function} <: CoordinateTransform
@@ -29,4 +23,3 @@ struct Morphological{F<:Function} <: CoordinateTransform
 end
 
 applycoord(t::Morphological, p::Point) = Point(t.func(coords(p)))
-applycoord(t::Morphological, v::Vec) = Point(t.func(Point(v...)))
