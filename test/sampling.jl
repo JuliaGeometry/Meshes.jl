34f95eadee998afafe7406264b56a4d69c986663
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-10-09 15:45:14 +0200

    format

--- test/sampling.jl
+++ test/sampling.jl
@@ -84,8 +84,7 @@ end
 
   c = ParametrizedCurve(t -> cart(cos(t), sin(t)), (0, 2π))
   ps = sample(c, RegularSampling(4))
-  ts =
-    cart.([(1.0, 0.0), (-0.5, 0.8660254037844387), (-0.5, -0.8660254037844385), (1.0, 0.0)])
+  ts = cart.([(1.0, 0.0), (-0.5, 0.8660254037844387), (-0.5, -0.8660254037844385), (1.0, 0.0)])
   for (p, t) in zip(ps, ts)
     @test p ≈ t
   end
