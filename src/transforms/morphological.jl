2b2401989aa20f93b9ddbf2b3a48b705f44c5713
Elias Carvalho <eliascarvdev@gmail.com>
2024-10-11 15:18:47 -0300

    Update implementation

--- src/transforms/morphological.jl
+++ src/transforms/morphological.jl
@@ -3,23 +3,35 @@
 # ------------------------------------------------------------------
 
 """
-    Morphological(func)
+    Morphological(fun)
 
-Morphological transform given by a function `func`.
+Morphological transform given by a function `fun`
+that maps the coordinates of a geometry or a domain
+to new coordinates (`coords -> newcoords`).
 
 # Examples
 
 ```julia
-TODO
-ball = Ball((0.0, 0.0), 1)
-tr = TransformedGeometry(ball, Morphological(c -> Cartesian(c.x + c.y, c.y, c.x - c.y)))
+ball = Ball((0, 0), 1)
+ball |> Morphological(c -> Cartesian(c.x + c.y, c.y, c.x - c.y))
 ```
 """
 struct Morphological{F<:Function} <: CoordinateTransform
-  func::F
-  function Morphological(func::F) where {F}
-    new{typeof(func)}(func)
-  end
+  fun::F
 end
 
-applycoord(t::Morphological, p::Point) = Point(t.func(coords(p)))
+applycoord(t::Morphological, p::Point) = Point(t.fun(coords(p)))
+
+applycoord(::Morphological, v::Vec) = v
+
+# --------------
+# SPECIAL CASES
+# --------------
+
+applycoord(t::Morphological, g::Primitive) = TransformedGeometry(g, t)
+
+applycoord(t::Morphological, g::RegularGrid) = TransformedGrid(g, t)
+
+applycoord(t::Morphological, g::RectilinearGrid) = TransformedGrid(g, t)
+
+applycoord(t::Morphological, g::StructuredGrid) = TransformedGrid(g, t)
