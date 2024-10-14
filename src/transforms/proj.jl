644cbb97986286fd4e0ad6e05ccf2e419ebbf957
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-10-14 15:19:50 -0300

    Fix `Proj` methods & Add `MaxLengthDiscretization` method for `TransformedGeometry` (#1111)
    * Fix 'Proj' methods & Add 'MaxLengthDiscretization' method for 'TransformedGeometry'
    
    * Fix ambiguities
    
    * Fix typo
    
    * Fix code
    
    * Update tests
    
    * Fix more tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/transforms/proj.jl
+++ src/transforms/proj.jl
@@ -51,9 +51,13 @@ applycoord(::Proj, v::Vec) = v
 # SPECIAL CASES
 # --------------
 
-applycoord(t::Proj{<:Projected}, g::Primitive{<:🌐}) = TransformedGeometry(g, t)
+applycoord(t::Proj{<:Projected}, g::Geometry{<:🌐}) = TransformedGeometry(g, t)
 
-applycoord(t::Proj{<:Geographic}, g::Primitive{<:𝔼}) = TransformedGeometry(g, t)
+applycoord(t::Proj{<:Geographic}, g::Geometry{<:𝔼}) = TransformedGeometry(g, t)
+
+# methods to fix ambiguities
+applycoord(t::Proj{<:Projected}, g::TransformedGeometry{<:🌐}) = TransformedGeometry(g, t)
+applycoord(t::Proj{<:Geographic}, g::TransformedGeometry{<:𝔼}) = TransformedGeometry(g, t)
 
 applycoord(t::Proj, g::RegularGrid) = TransformedGrid(g, t)
 
