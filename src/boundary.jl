4e0c79703a24552876134a725e9fcf070aa3d7e4
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-10-15 13:18:29 -0300

    Update src/boundary.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/boundary.jl
+++ src/boundary.jl
@@ -129,5 +129,8 @@ function boundary(m::Multi)
   isempty(valid) ? nothing : reduce(merge, valid)
 end
 
-boundary(g::TransformedGeometry) =
-  hasdistortedboundary(g) ? TransformedGeometry(boundary(parent(g)), transform(g)) : transform(g)(boundary(parent(g)))
+function boundary(g::TransformedGeometry)
+  b = boundary(parent(g))
+  t = transform(g)
+  hasdistortedboundary(g) ? TransformedGeometry(b, t) : t(b)
+end
