476df0492fdb8cf2fce2263bade1ad90be272673
Michael Ingold <mike.ingold@gmail.com>
2025-01-20 12:37:49 -0500

    Add parametrization for Pyramid

--- src/geometries/polytopes/pyramid.jl
+++ src/geometries/polytopes/pyramid.jl
@@ -15,3 +15,15 @@ nvertices(::Type{<:Pyramid}) = 5
 
 Base.isapprox(p₁::Pyramid, p₂::Pyramid; atol=atol(lentype(p₁)), kwargs...) =
   all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(p₁.vertices, p₂.vertices))
+
+function (pyramid::Pyramid)(u, v, w)
+  ℒ = lentype(pyramid)
+  T = numtype(ℒ)
+  if (u < 0 || u > 1) || (v < 0 || v > 1) || (w < 0 || w > 1)
+    throw(DomainError((u, v, w), "pyramid(u, v, w) is not defined for u, v, w outside [0, 1]³."))
+  end
+  a, b, c, d, vertex = vertices(pyramid)
+  base = Quadrangle(a, b, c, d)
+  s = Segment(base(T(u), T(v)), vertex)
+  s(T(w))
+end
