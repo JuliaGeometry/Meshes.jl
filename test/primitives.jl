cba7a2a129f5cb4f9deef1bcda12a09194da8eb6
Michael Ingold <mike.ingold@gmail.com>
2025-01-20 14:33:07 -0500

    Add a test_throws

--- test/primitives.jl
+++ test/primitives.jl
@@ -1252,6 +1252,7 @@ end
   @test norm(f(T(1), T(0), T(0)) - f(T(0), T(0), T(0))) ≈ radius(db)
   @test f(T(0), T(0), T(1)) == center(dt)
   @test norm(f(T(1), T(0), T(1)) - f(T(0), T(0), T(1))) ≈ radius(db)
+  @test_throws DomainError f(T(0), T(0), T(1.5))
 
   @test_throws AssertionError Frustum(db, db)
 
