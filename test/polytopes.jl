73972844f721d71070434c89fa764519541d8af6
Michael Ingold <mike.ingold@gmail.com>
2025-01-20 12:16:45 -0500

    Update error type

--- test/polytopes.jl
+++ test/polytopes.jl
@@ -979,7 +979,7 @@ end
   @test Meshes.lentype(w) == ℳ
   @test volume(w) ≈ T(1 / 2) * u"m^3"
   @test w(T(0), T(0), T(1)) == vertices(w)[4]
-  @test_throws ArgumentError w(T(0), T(0), T(1.5))
+  @test_throws DomainError w(T(0), T(0), T(1.5))
   m = boundary(w)
   @test m isa Mesh
   @test nelements(m) == 5
