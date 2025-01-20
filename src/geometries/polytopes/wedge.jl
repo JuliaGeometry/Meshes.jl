7fcd6c7c11d6d3c5a5c1160b7bc549a1c28dceb8
Michael Ingold <mike.ingold@gmail.com>
2025-01-20 11:46:12 -0500

    Change naming

--- src/geometries/polytopes/wedge.jl
+++ src/geometries/polytopes/wedge.jl
@@ -16,13 +16,13 @@ nvertices(::Type{<:Wedge}) = 6
 Base.isapprox(t₁::Wedge, t₂::Wedge; atol=atol(lentype(t₁)), kwargs...) =
   all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(t₁.vertices, t₂.vertices))
 
-function (w::Wedge)(u, v, w)
-  ℒ = lentype(w)
+function (wedge::Wedge)(u, v, w)
+  ℒ = lentype(wedge)
   T = numtype(ℒ)
   if (u < 0 || u > 1) || (v < 0 || v > 1) || (w < 0 || w > 1)
-    throw(DomainError((u, v, w), "w(u, v, w) is not defined for u, v, w outside [0, 1]³."))
+    throw(DomainError((u, v, w), "wedge(u, v, w) is not defined for u, v, w outside [0, 1]³."))
   end
-  a1, a2, a3, b1, b2, b3 = w.vertices
+  a1, a2, a3, b1, b2, b3 = wedge.vertices
   a = Triangle(a1, a2, a3)
   b = Triangle(b1, b2, b3)
   s = Segment(a(T(u), T(v)), b(T(u), T(v)))
