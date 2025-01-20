05bb3eac39d79f940cf37bb7146c69d1b8a3d6a8
Michael Ingold <mike.ingold@gmail.com>
2025-01-20 09:46:41 -0500

    Add draft parametrization function

--- src/geometries/polytopes/wedge.jl
+++ src/geometries/polytopes/wedge.jl
@@ -15,3 +15,13 @@ nvertices(::Type{<:Wedge}) = 6
 
 Base.isapprox(t₁::Wedge, t₂::Wedge; atol=atol(lentype(t₁)), kwargs...) =
   all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(t₁.vertices, t₂.vertices))
+
+function (w::Wedge)(u, v, w)
+  if t < 0 || t > 1
+    throw(DomainError(t, "w(u, v, w) is not defined for coordinates outside [0, 1]."))
+  end
+  a1, a2, a3, b1, b2, b3 = w.vertices
+  a = Triangle(a1, a2, a3)
+  b = Triangle(b1, b2, b3)
+  Segment(a(u, v), b(u, v))(w)
+end
