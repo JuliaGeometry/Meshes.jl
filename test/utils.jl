33f2fba39b903131823c6f520f532420e1f51fa9
souma4 <jeffrey.chandler435@gmail.com>
2025-03-19 16:44:02 -0600

    hopefully fixed failing test on Float32 values

--- test/utils.jl
+++ test/utils.jl
@@ -65,7 +65,7 @@ end
 
 @testitem "Bentley-Ottmann" setup = [Setup] begin
   # basic check with a small number of segments
-  digits = -Int(log10(atol(T)))
+  precision = -Int(log10(atol(T)))
   segs =
     Segment.([
       (cart(0, 0), cart(1.1, 1.1)),
@@ -75,11 +75,12 @@ end
       (cart(0, 1), cart(1, 1)),
       (cart(1, 0), cart(1, 1))
     ])
-  points, seginds = Meshes.bentleyottmann(segs; digits=digits)
+  points, seginds = Meshes.bentleyottmann(segs; digits=precision)
   @test all(points .≈ [cart(0, 0), cart(0, 1), cart(0.5, 0.5), cart(1, 0), cart(1, 1), cart(1.1, 1.1)])
   @test length(points) == 6
   @test length(seginds) == 6
   @test seginds == [[1, 3, 4], [2, 5, 3], [1, 2], [6, 2, 4], [5, 6, 1], [1]]
+  @inferred Meshes.bentleyottmann(segs; digits=precision)
 
   segs =
     Segment.([
@@ -92,7 +93,7 @@ end
       (cart(10, 4), cart(11, -1)),
       (cart(10, 3), cart(10, 5))
     ])
-  points, seginds = Meshes.bentleyottmann(segs; digits=digits)
+  points, seginds = Meshes.bentleyottmann(segs; digits=precision)
   @test length(points) == 17
   @test length(seginds) == 17
   @test Set(reduce(vcat, seginds)) == Set(1:8)
@@ -102,7 +103,7 @@ end
 
   # finds all intersections in a grid
   segs = facets(cartgrid(10, 10))
-  points, seginds = Meshes.bentleyottmann(segs; digits=digits)
+  points, seginds = Meshes.bentleyottmann(segs; digits=precision)
   @test length(points) == 121
   @test length(seginds) == 121
   @test Set(length.(seginds)) == Set([2, 3, 4])
@@ -110,7 +111,7 @@ end
   # result is invariant under rotations
   segs = collect(segs)
   for θ in T(π / 6):T(π / 6):T(2π - π / 6)
-    θpoints, θseginds = Meshes.bentleyottmann(segs |> Rotate(θ); digits=digits)
+    θpoints, θseginds = Meshes.bentleyottmann(segs |> Rotate(θ); digits=precision)
     @test length(θpoints) == 121
     @test length(θseginds) == 121
     @test Set(length.(θseginds)) == Set([2, 3, 4])
@@ -118,5 +119,5 @@ end
 
   # inference test
   segs = facets(cartgrid(10, 10))
-  @inferred Meshes.bentleyottmann(segs; digits=10)
+  @inferred Meshes.bentleyottmann(segs; digits=precision)
 end
