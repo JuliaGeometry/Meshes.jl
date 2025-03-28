8345d1ee7e80ad292f8a2a6eb8685b663b19278d
souma4 <jeffrey.chandler435@gmail.com>
2025-03-28 14:28:20 -0600

    WIP, With what little time I've had and will have for a bit, here's as far as I got with De Berg implementation. All tests fail. I think this is occuring between the status structure isn't reflecting the order where the plane crosses, but the total order in x and y (so a line with a low x will always be the minimum even if where it intersects the plane is the maximum). I tried having a sort method to maybe get it but that didn't work. I'm sort of at a loss outside of computing intersections to the plane, which I can't think of an efficient way of doing that right now. Maybe y'all will have some ideas

--- src/utils/intersect.jl
+++ src/utils/intersect.jl
@@ -44,99 +44,174 @@ function bentleyottmann(segments; digits=_digits(segments))
   end
 
   # sweep line algorithm
-  counter = 1
   points = Vector{P}()
   seginds = Vector{Vector{Int}}()
-  visited = Dict{P,Int}()
   while !BinaryTrees.isempty(𝒬)
     # current point (or event)
     p = BinaryTrees.key(BinaryTrees.minnode(𝒬))
 
     # delete point from event queue
     BinaryTrees.delete!(𝒬, p)
-
     # handle event, i.e. update 𝒬, ℛ and ℳ
     ℬₚ = get(ℬ, p, S[]) # segments with p at the begin
     ℰₚ = get(ℰ, p, S[]) # segments with p at the end
     ℳₚ = get(ℳ, p, S[]) # segments with p at the middle
-    _handlebeg!(ℬₚ, 𝒬, ℛ, ℳ, digits)
-    _handleend!(ℰₚ, 𝒬, ℛ, ℳ, digits)
-    _handlemid!(ℳₚ, 𝒬, ℛ, ℳ, digits)
+    # handle status line
+    _handlestatus!(ℛ, ℬₚ, ℳₚ, ℰₚ)
 
-    # report intersection point and segment indices
-    inds = [lookup[s] for s in ℳₚ]
-    if !isempty(inds)
-      if p ∈ keys(visited)
-        seginds[visited[p]] = inds
-      else
-        push!(points, p)
-        push!(seginds, inds)
-        push!(visited, p => counter)
-        counter += 1
+    if length(ℳₚ) > 0
+      push!(points, p)
+      inds = [lookup[s] for s in ℳₚ]
+      push!(seginds, inds)
+    end
+
+    activesegs = ℬₚ ∪ ℳₚ
+
+    if isempty(activesegs)
+      for s in ℰₚ
+        sₗ, sᵣ = BinaryTrees.prevnext(ℛ, s)
+        isnothing(sₗ) || isnothing(sᵣ) || Meshes._newevent!(𝒬, ℳ, p, BinaryTrees.key(sₗ), BinaryTrees.key(sᵣ), digits)
       end
+    else
+      _handlebottom!(activesegs[1], ℛ, 𝒬, ℳ, p, digits)
+
+      _handletop!(activesegs[end], ℛ, 𝒬, ℳ, p, digits)
     end
   end
 
   points, seginds
 end
 
-function _handlebeg!(ℬₚ, 𝒬, ℛ, ℳ, digits)
-  for s in ℬₚ
-    BinaryTrees.insert!(ℛ, s)
-    prev, next = BinaryTrees.prevnext(ℛ, s)
-    isnothing(prev) || _newevent!(𝒬, ℳ, BinaryTrees.key(prev), s, digits)
-    isnothing(next) || _newevent!(𝒬, ℳ, s, BinaryTrees.key(next), digits)
-    isnothing(prev) || isnothing(next) || _rmevent!(𝒬, s, s, digits)
-  end
-end
-
-function _handleend!(ℰₚ, 𝒬, ℛ, ℳ, digits)
+function _handlestatus!(ℛ, ℬₚ, ℳₚ, ℰₚ)
   for s in ℰₚ
-    prev, next = BinaryTrees.prevnext(ℛ, s)
-    isnothing(prev) || isnothing(next) || _newevent!(𝒬, ℳ, BinaryTrees.key(prev), BinaryTrees.key(next), digits)
-    BinaryTrees.delete!(ℛ, s)
+    !isnothing(BinaryTrees.search(ℛ, s)) || deleteat!(ℰₚ, findfirst(isequal(s), ℰₚ)) || BinaryTrees.delete!(ℛ, s)
   end
-end
 
-function _handlemid!(ℳₚ, 𝒬, ℛ, ℳ, digits)
   for s in ℳₚ
-    prev, next = BinaryTrees.prevnext(ℛ, s)
-    r = !isnothing(prev) ? BinaryTrees.key(prev) : nothing
-    t = !isnothing(next) ? BinaryTrees.key(next) : nothing
-    if !isnothing(r)
-      _newevent!(𝒬, ℳ, r, s, digits)
-      if !isnothing(t)
-        _newevent!(𝒬, ℳ, r, t, digits)
-      end
-    end
-    if !isnothing(t)
-      _, next = BinaryTrees.prevnext(ℛ, BinaryTrees.key(next))
-      u = !isnothing(next) ? BinaryTrees.key(next) : nothing
-      if !isnothing(u)
-        _newevent!(𝒬, ℳ, t, u, digits)
-        if !isnothing(r)
-          _newevent!(𝒬, ℳ, r, u, digits)
-        end
-      end
+    !isnothing(BinaryTrees.search(ℛ, s)) || deleteat!(ℳₚ, findfirst(isequal(s), ℳₚ)) || BinaryTrees.delete!(ℛ, s)
+  end
+
+  for s in ℬₚ ∪ ℳₚ
+    BinaryTrees.insert!(ℛ, s)
+  end
+end
+
+function _handlebottom!(s, ℛ, 𝒬, ℳ, p, digits)
+  s′ = BinaryTrees.key(BinaryTrees.search(ℛ, s))
+
+  sₗ, _ = BinaryTrees.prevnext(ℛ, s′)
+  if !isnothing(sₗ)
+    _newevent!(𝒬, ℳ, p, BinaryTrees.key(sₗ), s′, digits)
+  end
+end
+
+function _handletop!(s, ℛ, 𝒬, ℳ, p, digits)
+  s′′ = BinaryTrees.search(ℛ, s) |> BinaryTrees.key
+
+  if !isnothing(s′′)
+    _, sᵣ = BinaryTrees.prevnext(ℛ, s′′)
+    if !isnothing(sᵣ)
+      _newevent!(𝒬, ℳ, p, s′′, BinaryTrees.key(sᵣ), digits)
     end
   end
 end
 
-function _newevent!(𝒬, ℳ, s₁, s₂, digits)
+# #sort by where segment intersects plane then check to ℛ? #doesn't seem to work
+# function _sort(segs, p)
+#   segs = copy(segs)
+#   if isempty(segs)
+#     return segs
+#   end
+#   _sort!(segs, p)
+# end
+# function _sort!(segs, p)
+#   T = lentype(eltype(first(segs)))
+#   ys = Vector{T}()
+#   pₓ, _ = coords(p) |> CoordRefSystems.values
+#   for s in segs
+#     a, b = s
+#     x₁, y₁ = coords(a) |> CoordRefSystems.values
+#     x₂, y₂ = coords(b) |> CoordRefSystems.values
+#     if x₁ == x₂
+#       push!(ys, y₂)  # Vertical segment, use y₂ directly to place at end
+#     else
+#       t = (pₓ - x₁) / (x₂ - x₁)
+#       c = y₁ + t * (y₂ - y₁)
+#       push!(ys, c)
+#     end
+#   end
+#   segs[sortperm(ys)]
+# end
+
+# _handlebeg!(ℬₚ, 𝒬, ℛ, ℳ, digits)
+# _handleend!(ℰₚ, 𝒬, ℛ, ℳ, digits)
+# _handlemid!(ℳₚ, 𝒬, ℛ, ℳ, digits)
+
+# report intersection point and segment indices
+# inds = [lookup[s] for s in ℳₚ]
+# if !isempty(inds)
+#   if p ∈ keys(visited)
+#     seginds[visited[p]] = inds
+#   else
+#     push!(points, p)
+#     push!(seginds, inds)
+#     push!(visited, p => counter)
+#     counter += 1
+#   end
+# end
+# function _handlebeg!(ℬₚ, 𝒬, ℛ, ℳ, digits)
+#   for s in ℬₚ
+#     BinaryTrees.insert!(ℛ, s)
+#     prev, next = BinaryTrees.prevnext(ℛ, s)
+#     isnothing(prev) || _newevent!(𝒬, ℳ, BinaryTrees.key(prev), s, digits)
+#     isnothing(next) || _newevent!(𝒬, ℳ, s, BinaryTrees.key(next), digits)
+#     isnothing(prev) || isnothing(next) || _rmevent!(𝒬, s, s, digits)
+#   end
+# end
+
+# function _handleend!(ℰₚ, 𝒬, ℛ, ℳ, digits)
+#   for s in ℰₚ
+#     prev, next = BinaryTrees.prevnext(ℛ, s)
+#     isnothing(prev) || isnothing(next) || _newevent!(𝒬, ℳ, BinaryTrees.key(prev), BinaryTrees.key(next), digits)
+#     BinaryTrees.delete!(ℛ, s)
+#   end
+# end
+
+# function _handlemid!(ℳₚ, 𝒬, ℛ, ℳ, digits)
+#   for s in ℳₚ
+#     prev, next = BinaryTrees.prevnext(ℛ, s)
+#     r = !isnothing(prev) ? BinaryTrees.key(prev) : nothing
+#     t = !isnothing(next) ? BinaryTrees.key(next) : nothing
+#     if !isnothing(r)
+#       _newevent!(𝒬, ℳ, r, s, digits)
+#       if !isnothing(t)
+#         _newevent!(𝒬, ℳ, r, t, digits)
+#       end
+#     end
+#     if !isnothing(t)
+#       _, next = BinaryTrees.prevnext(ℛ, BinaryTrees.key(next))
+#       u = !isnothing(next) ? BinaryTrees.key(next) : nothing
+#       if !isnothing(u)
+#         _newevent!(𝒬, ℳ, t, u, digits)
+#         if !isnothing(r)
+#           _newevent!(𝒬, ℳ, r, u, digits)
+#         end
+#       end
+#     end
+#   end
+# end
+
+function _newevent!(𝒬, ℳ, p, s₁, s₂, digits)
   intersection(Segment(s₁), Segment(s₂)) do I
     if type(I) == Crossing || type(I) == EdgeTouching
-      p = roundcoords(get(I); digits)
-      if haskey(ℳ, p)
-        if s₁ ∉ ℳ[p]
-          push!(ℳ[p], s₁)
-          BinaryTrees.insert!(𝒬, p)
+      p′ = roundcoords(get(I); digits)
+      if p′ > p
+        !isnothing(BinaryTrees.search(𝒬, p′)) || BinaryTrees.insert!(𝒬, p′)
+        if haskey(ℳ, p′)
+          push!(ℳ[p′], s₁, s₂)
+        else
+          ℳ[p′] = [s₁, s₂]
         end
-        if s₂ ∉ ℳ[p]
-          push!(ℳ[p], s₂)
-        end
-      else
-        ℳ[p] = [s₁, s₂]
-        BinaryTrees.insert!(𝒬, p)
       end
     end
     nothing
