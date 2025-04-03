669680a6d48baf19f33bfa15d52fb0835f064ed4
Allen Hill <halleysfifthinc@users.noreply.github.com>
2025-04-03 12:17:53 -0700

    Document partition order for `BisectPointPartition` (#1182)
    * Document partition order for `BisectPointPartition`
    
    * Improve docstring
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/partitioning/bisectpoint.jl
+++ src/partitioning/bisectpoint.jl
@@ -7,6 +7,10 @@
 
 A method for partitioning objects into two half spaces
 defined by a `normal` direction and a reference `point`.
+
+The first half space contains elements to the "left" of
+the bisecting plane, while the second half space contains
+elements to the "right" of the bisecting plane.
 """
 struct BisectPointPartition{V<:Vec,P<:Point} <: PartitionMethod
   normal::V
