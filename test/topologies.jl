5a0c047a20e5f5953ee38b7799f8cca9ce57b6a1
Allen Hill <halleysfifthinc@users.noreply.github.com>
2025-05-14 09:13:45 -0700

    Update test/topologies.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- test/topologies.jl
+++ test/topologies.jl
@@ -504,7 +504,7 @@ end
   n = collect(elements(t))
   @test n == connect.([(5, 4, 1), (6, 2, 4), (6, 5, 3), (4, 5, 6)])
 
-  e = connect.([(1, 2, 3), (1, 3, 4), (2, 5, 3), (5, 4, 6), (3, 5, 4)], Triangle)
+  e = connect.([(1, 2, 3), (1, 3, 4), (2, 5, 3), (5, 4, 6), (3, 5, 4)])
   t = HalfEdgeTopology(e)
   test_halfedge(e, t)
 
