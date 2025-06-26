56b746ce3aef45fbe7d2589c7f0608cfa1b3f365
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-06-26 12:04:23 -0300

    Refactor intersectparameters utility

--- src/utils/misc.jl
+++ src/utils/misc.jl
@@ -78,23 +78,21 @@ calculated in order to identify the intersection type:
   - No intersection, skew lines: r == 2, rₐ == 3
 """
 function intersectparameters(a::Point, b::Point, c::Point, d::Point)
+  # build linear system
   A = ustrip.([(b - a) (c - d)])
   y = ustrip.(c - a)
+
+  # numerical tolerance
   T = eltype(A)
-
-  # calculate the rank of the augmented matrix by checking
-  # the zero entries of the diagonal of R
-  _, R = qr([A y])
-
-  # for Dim == 2 one has to check the L1 norm of rows as 
-  # there are more columns than rows
   τ = atol(T)
-  rₐ = sum(>(τ), sum(abs, R, dims=2))
 
-  # calculate the rank of the rectangular matrix
+  # calculate the ranks by checking the zero entries of
+  # the diagonal of R in the QR matrix factorization
+  _, R = qr([A y])
+  rₐ = sum(>(τ), sum(abs, R, dims=2))
   r = sum(>(τ), sum(abs, view(R, :, 1:2), dims=2))
 
-  # calculate parameters of intersection or closest point
+  # calculate parameters of intersection
   if r ≥ 2
     λ = A \ y
     λ₁, λ₂ = λ[1], λ[2]
