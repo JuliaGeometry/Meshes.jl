4a79d404a905aba02665ad24e9f7d4226d803717
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-29 16:15:23 -0300

    Fix discretization test

--- test/discretization.jl
+++ test/discretization.jl
@@ -582,9 +582,9 @@ end
   box = Box(latlon(0, 0), latlon(45, 45))
   tbox = TransformedGeometry(box, Proj(Mercator))
   mesh = discretize(tbox)
-  @test nvertices(mesh) == 49
-  @test nelements(mesh) == 36
-  @test eltype(mesh) <: Quadrangle
+  @test nvertices(mesh) == 81 
+  @test nelements(mesh) == 128
+  @test eltype(mesh) <: Triangle
 
   grid = CartesianGrid(10)
   @test discretize(grid) == grid
