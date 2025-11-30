4a05396469b7026d7194aec76412fc4375a9466d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-30 15:17:33 -0300

    Unexport atol utility function

--- test/tolerances.jl
+++ test/tolerances.jl
@@ -3,18 +3,18 @@
   𝒜 = typeof(zero(ℳ)^2)
   𝒱 = typeof(zero(ℳ)^3)
 
-  @test atol(T) == eps(T)^(3 // 4)
-  @test atol(ℒ) == atol(T) * u"m"
-  @test atol(𝒜) == atol(T)^2 * u"m^2"
-  @test atol(𝒱) == atol(T)^3 * u"m^3"
+  @test Meshes.atol(T) == eps(T)^(3 // 4)
+  @test Meshes.atol(ℒ) == Meshes.atol(T) * u"m"
+  @test Meshes.atol(𝒜) == Meshes.atol(T)^2 * u"m^2"
+  @test Meshes.atol(𝒱) == Meshes.atol(T)^3 * u"m^3"
 
-  @test atol(zero(T)) == atol(T)
-  @test atol(zero(ℒ)) == atol(ℒ)
-  @test atol(zero(𝒜)) == atol(𝒜)
-  @test atol(zero(𝒱)) == atol(𝒱)
+  @test Meshes.atol(zero(T)) == Meshes.atol(T)
+  @test Meshes.atol(zero(ℒ)) == Meshes.atol(ℒ)
+  @test Meshes.atol(zero(𝒜)) == Meshes.atol(𝒜)
+  @test Meshes.atol(zero(𝒱)) == Meshes.atol(𝒱)
 
-  @inferred atol(T)
-  @inferred atol(ℒ)
-  @inferred atol(𝒜)
-  @inferred atol(𝒱)
+  @inferred Meshes.atol(T)
+  @inferred Meshes.atol(ℒ)
+  @inferred Meshes.atol(𝒜)
+  @inferred Meshes.atol(𝒱)
 end
