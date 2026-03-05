e5795f09803fba5f86d85396e2da0c534074e7e4
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-03-05 08:40:55 -0300

    Add more special cases

--- src/integration.jl
+++ src/integration.jl
@@ -24,20 +24,34 @@ Polynomials of degree up to `2n-1` are integrated exactly.
 
 See also [`integral`](@ref).
 """
-localintegral(fun, geom; n=3) = _uvwintegral(fun, geom; n)
+localintegral(fun, geom; n=3) = _uvwintegral(fun, geom, n)
 
 # --------------
 # SPECIAL CASES
 # --------------
 
+# the integral of a (bounded) function over a single point is always zero
+localintegral(fun, point::Point; n=3) = zero(lentype(point))
+
+# cylinder surface is the union of the lateral surface and the top and bottom disks
+localintegral(fun, cylsurf::CylinderSurface; n=3) =
+  _uvwintegral(fun, cylsurf, n) + _uvwintegral(fun, top(cylsurf), n) + _uvwintegral(fun, bottom(cylsurf), n)
+
 # cone surface is the union of the lateral surface and the base disk
-localintegral(fun, conesurf::ConeSurface; n=3) = _uvwintegral(fun, conesurf; n) + _uvwintegral(fun, base(conesurf); n)
+localintegral(fun, conesurf::ConeSurface; n=3) = _uvwintegral(fun, conesurf, n) + _uvwintegral(fun, base(conesurf), n)
+
+# frustum surface is the union of the lateral surface and the top and bottom disks
+localintegral(fun, frustumsurf::FrustumSurface; n=3) =
+  _uvwintegral(fun, frustumsurf, n) + _uvwintegral(fun, top(frustumsurf), n) + _uvwintegral(fun, bottom(frustumsurf), n)
+
+# chain is the union of its segments
+localintegral(fun, chain::Chain; n=3) = sum(_uvwinteral(fun, seg, n) for seg in segments(chain))
 
 # -----------------
 # HELPER FUNCTIONS
 # -----------------
 
-function _uvwintegral(fun, geom; n=3)
+function _uvwintegral(fun, geom, n)
   # parametric dimension and number type
   N = paramdim(geom)
   T = numtype(lentype(geom))
