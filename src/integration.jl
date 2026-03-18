ca6c7bac10bf488fe657b7a7eeb256f2ff14e09d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-03-18 18:47:22 -0300

    Strip units from integrand to help backends

--- src/integration.jl
+++ src/integration.jl
@@ -69,8 +69,13 @@ function localintegral(fun, geom::Geometry, method=GAUSSLEGENDRE)
   # domain of integration for the given geometry
   domain = ∫domain(geom)
 
+  # strip units to help integration methods
+  f(uvw...) = ustrip(integrand(uvw...))
+  o = ntuple(_ -> 0, paramdim(geom))
+  u = unit(integrand(o...))
+
   # perform numerical integration
-  II.integral(integrand, domain; backend=method)
+  II.integral(f, domain; backend=method) * u
 end
 
 function ∫domain(geom::Geometry)
