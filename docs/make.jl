98450cadae3edbf41957693fb45ebcd95da4bc11
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2026-04-03 12:12:48 +0200

    Update docs/make.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -47,8 +47,9 @@ makedocs(
 repo = "github.com/JuliaGeometry/MeshesDocs.git"
 
 withenv("GITHUB_REPOSITORY" => repo) do
-  deploydocs(; repo, versions=["stable" => "v^", "dev" => "dev"],
-             push_preview=all(!isempty,
-                              (get(ENV, "GITHUB_TOKEN", ""),
-                               get(ENV, "DOCUMENTER_KEY", ""))))
+  deploydocs(;
+    repo,
+    versions=["stable" => "v^", "dev" => "dev"],
+    push_preview=all(!isempty, (get(ENV, "GITHUB_TOKEN", ""), get(ENV, "DOCUMENTER_KEY", "")))
+  )
 end
