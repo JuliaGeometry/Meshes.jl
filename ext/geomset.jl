9596feee594c975fd95555c2d00553adbc3385ea
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-04-09 19:28:32 -0300

    Refactor comment

--- ext/geomset.jl
+++ ext/geomset.jl
@@ -56,9 +56,9 @@ function vizgset!(plot, M::Type, pdim::Val, ::Val, geoms::ObservableVector{<:Geo
   # they satisfy the maximum length criterion
   mayberefine = M === 🌐 ? refinemaxlen : identity
 
-  # make sure the geometries are discretized
-  # with efficient "grid-like" methods before
-  # turning the quadrangles into triangles
+  # make sure the geometries are refined with
+  # efficient "grid-like" methods before turning
+  # quadrangles/hexahedra into triangles/tetrahedra
   triangulate = simplexify ∘ mayberefine ∘ discretize
 
   if pdim === Val(1)
