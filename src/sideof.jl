c92b02f37858d3b298556254e98f66f14a29f953
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-04-29 16:25:39 -0300

    Fix typo in docstring

--- src/sideof.jl
+++ src/sideof.jl
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------
 
 """
-    IntersectionType
+    SideType
 
 The different types of sides that a point may lie in relation to a
 boundary geometry or mesh. Type `SideType` in a Julia session to see
