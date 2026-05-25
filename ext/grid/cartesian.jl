3b1e9c647e50182924e1cc118c11a64678883753
Al Nejati <alireza.n.j@gmail.com>
2026-05-26 05:51:42 +1200

    Make face order match src/boundary.jl

--- ext/grid/cartesian.jl
+++ ext/grid/cartesian.jl
@@ -67,17 +67,17 @@ function vizgrid!(plot::Viz{<:Tuple{CartesianGrid}}, ::Type{<:𝔼}, ::Val{3}, :
     # visualize as a quadrangle mesh so that
     # colors can be specified at vertices
     verts = Makie.@lift map(asmakie, eachvertex($grid))
-    quads = Makie.@lift mapreduce(vcat, elements(topology($grid))) do elem
+    quads = Makie.@lift reduce(vcat, map(elements(topology($grid))) do elem
       i1, i2, i3, i4, i5, i6, i7, i8 = indices(elem)
       [
-        GB.QuadFace(i1, i2, i3, i4),
-        GB.QuadFace(i5, i6, i7, i8),
-        GB.QuadFace(i1, i2, i6, i5),
-        GB.QuadFace(i2, i3, i7, i6),
-        GB.QuadFace(i3, i4, i8, i7),
-        GB.QuadFace(i4, i1, i5, i8)
+        GB.QuadFace(i4, i3, i2, i1),
+        GB.QuadFace(i6, i5, i1, i2),
+        GB.QuadFace(i3, i7, i6, i2),
+        GB.QuadFace(i4, i8, i7, i3),
+        GB.QuadFace(i1, i5, i8, i4),
+        GB.QuadFace(i6, i7, i8, i5)
       ]
-    end
+    end)
     mesh = Makie.@lift GB.Mesh($verts, $quads)
     Makie.mesh!(plot, mesh, color=colorant, shading=true)
   else
