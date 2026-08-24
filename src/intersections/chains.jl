a85756360915919c3b78643d85c630d51486f944
Pedro H. Chaves Maia <68562195+phchavesmaia@users.noreply.github.com>
2026-08-24 17:10:19 -0300

    Add intersection function for chain and polygon
    Implement intersection logic between a chain and a polygon, classifying the types of intersections.

--- src/intersections/chains.jl
+++ src/intersections/chains.jl
@@ -4,3 +4,43 @@
 
 intersection(f, chain₁::Chain, chain₂::Chain) =
   intersection(f, GeometrySet(segments(chain₁)), GeometrySet(segments(chain₂)))
+
+# intersection between a chain and a polygon
+# 1. intersect at inner points (Intersecting)
+# 2. intersect at boundary points (Touching)
+# 3. overlaps at the polygon edges (EdgeTouching)
+# 4. no intersection (NotIntersecting)
+function intersection(f, chain::Chain{𝔼{2}}, poly::Polygon{𝔼{2}})
+  # assess intersections 
+  pieces = Geometry[]
+  hasintersection = false
+  onlytouching = true
+  onlyedgetouching = true
+  for seg in segments(chain)
+    I = intersection(seg, poly)
+    type(I) == NotIntersecting && continue
+    hasintersection = true
+    onlytouching &= (type(I) == Touching) || (type(I) == CornerTouching)
+    onlyedgetouching &= (type(I) == EdgeTouching)
+    geom = get(I)
+    if geom isa Multi
+      append!(pieces, parent(geom))
+    else
+      push!(pieces, geom)
+    end
+  end
+
+  unique!(pieces)
+  glue(pieces) = length(pieces) == 1 ? only(pieces) : GeometrySet(pieces)
+
+  # classifying intersections
+  if !hasintersection
+    return @IT NotIntersecting nothing f
+  elseif onlytouching
+    return @IT Touching glue(pieces) f
+  elseif onlyedgetouching
+    return @IT EdgeTouching glue(pieces) f
+  else
+    return @IT Intersecting glue(pieces) f
+  end
+end
