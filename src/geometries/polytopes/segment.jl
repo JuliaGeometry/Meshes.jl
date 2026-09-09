6f84ab640e6f7eca1219dd8d4c15d1b93306b196
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-09-09 09:10:41 -0300

    More fixes

--- src/geometries/polytopes/segment.jl
+++ src/geometries/polytopes/segment.jl
@@ -5,44 +5,47 @@
 """
     Segment(p1, p2)
 
-An oriented line segment from point `p1` to point `p2`.
+An oriented, geodesic line segment from point `p1` to point `p2`.
+
+## Examples
+
+```julia
+# segment in Euclidean manifold
+Segment((0, 0), (1, 1))
+
+# segment in ellipsoid manifold
+Segment(Point(LatLon(0, 0)), Point(LatLon(45, 90)))
+```
 
 See also [`Rope`](@ref), [`Ring`](@ref), [`Line`](@ref).
 """
-struct Segment{M<:Manifold,C<:CRS} <: Chain{M,C}
-  a::Point{M,C}
-  b::Point{M,C}
-end
-
-Segment(a::Tuple, b::Tuple) = Segment(Point(a), Point(b))
-
-Segment(ab::AbstractVector) = Segment(ab...)
-
-Segment(ab::Tuple) = Segment(ab...)
+@polytope Segment 1 2
 
 nvertices(::Type{<:Segment}) = 2
 
-vertices(s::Segment) = SVector(s.a, s.b)
+Base.minimum(s::Segment) = s.vertices[1]
 
-Base.minimum(s::Segment) = s.a
+Base.maximum(s::Segment) = s.vertices[2]
 
-Base.maximum(s::Segment) = s.b
-
-Base.extrema(s::Segment) = s.a, s.b
+Base.extrema(s::Segment) = s.vertices[1], s.vertices[2]
 
 center(s::Segment{<:𝔼}) = s(1 // 2)
 
-==(s₁::Segment, s₂::Segment) = s₁.a == s₂.a && s₁.b == s₂.b
+==(s₁::Segment, s₂::Segment) = s₁.vertices == s₂.vertices
 
 Base.isapprox(s₁::Segment, s₂::Segment; atol=atol(lentype(s₁)), kwargs...) =
-  isapprox(s₁.a, s₂.a; atol=atol, kwargs...) && isapprox(s₁.b, s₂.b; atol=atol, kwargs...)
+  all(isapprox(v₁, v₂; atol, kwargs...) for (v₁, v₂) in zip(s₁.vertices, s₂.vertices))
 
-(s::Segment{<:𝔼})(t) = s.a + t * (s.b - s.a)
-
-function (s::Segment{🌐})(t)
-  d = GeodesicDistance()
-  ϕ = geodesicbwd(s.a, s.b)
-  geodesicfwd(s.a, ϕ, t * d(s.a, s.b))
+function (s::Segment{<:𝔼})(t)
+  a, b = s.vertices
+  a + t * (b - a)
 end
 
-Base.reverse(s::Segment) = Segment(s.b, s.a)
+function (s::Segment{🌐})(t)
+  a, b = s.vertices
+  d = GeodesicDistance()
+  ϕ = geodesicbwd(a, b)
+  geodesicfwd(a, ϕ, t * d(a, b))
+end
+
+Base.reverse(s::Segment) = Segment(reverse(s.vertices))
